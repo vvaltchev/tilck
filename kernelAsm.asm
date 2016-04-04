a5f3232df104d2e4e493c809e0df635aa9af4504
Vladislav Valtchev <vladi32@gmail.com>
2016-04-05 02:36:11 +0300

    implementing user-mode + sys call to kernel, and it works.

--- kernelAsm.asm
+++ kernelAsm.asm
@@ -8,6 +8,7 @@ extern _gdt_pointer
 
 global _gdt_load
 global _idt_load
+global _tss_flush
 
 section .text
 
@@ -21,6 +22,14 @@ _gdt_load:
 _idt_load:
     lidt [_idtp]
     ret
+    
+_tss_flush:
+   mov ax, 0x2B      ; Load the index of our TSS structure - The index is
+                     ; 0x28, as it is the 5th selector and each is 8 bytes
+                     ; long, but we set the bottom two bits (making 0x2B)
+                     ; so that it has an RPL of 3, not zero.
+   ltr ax            ; Load 0x2B into the task state register.
+   ret
 
 
 ; In just a few pages in this tutorial, we will add our Interrupt
