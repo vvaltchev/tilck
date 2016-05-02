81d64efdad446fc9af1f2ab9c94378f3caefcb13
Vladislav Valtchev <vladi32@gmail.com>
2016-05-02 17:39:15 +0300

    term still work without cyclic buffer

--- include/stringUtil.h
+++ include/stringUtil.h
@@ -8,30 +8,65 @@ typedef unsigned char *va_list;
 #define va_arg(list, type)    (*(type *)((list += sizeof(type)) - sizeof(type)))
 
 
-void *memset(void *ptr, int value, size_t num);
-void *memcpy(void *dest, void *src, size_t num);
-size_t strlen(const char *str);
+// TODO: optimize
+static ALWAYS_INLINE void *memset(void *ptr, int value, size_t num)
+{
+   for (size_t i = 0; i < num; ++i)
+      ((char*)ptr)[i] = value;
+
+   return ptr;
+}
+
+// TODO: optimize
+static ALWAYS_INLINE size_t strlen(const char *str)
+{
+   size_t c = 0;
+   while (*str++) { ++c; }
+   return c;
+}
+
+
+// Dest and src can overlap
+// TODO: optimize
+static ALWAYS_INLINE void *memmove(volatile void *dest, volatile void *src, size_t num)
+{
+   volatile char *dst = (volatile char *)dest;
+   volatile char *s = (volatile char *)src;
+
+   for (size_t i = 0; i < num; i++) {
+      *dst++ = *s++;
+   }
+
+   return dest;
+}
+
+// Dest and src cannot overlap
+// TODO: optimize
+static ALWAYS_INLINE void *memcpy(volatile void *dest, volatile void *src, size_t num)
+{
+   return memmove(dest, src, num);
+}
 
 void itoa(int value, char *destBuf);
 void uitoa(unsigned value, char *destBuf, unsigned base);
 
-static inline bool isalpha_lower(char c) {
+static ALWAYS_INLINE bool isalpha_lower(char c) {
    return (c >= 'a' && c <= 'z');
 }
 
-static inline bool isalpha_upper(char c) {
+static ALWAYS_INLINE bool isalpha_upper(char c) {
    return (c >= 'a' && c <= 'z');
 }
 
-static inline bool isalpha(char c) {
+static ALWAYS_INLINE bool isalpha(char c) {
    return isalpha_lower(c) || isalpha_upper(c);
 }
 
-static inline char lower(char c) {
+static ALWAYS_INLINE char lower(char c) {
    return isalpha_upper(c) ? c + 27 : c;
 }
 
-static inline char upper(char c) {
+static ALWAYS_INLINE char upper(char c) {
    return isalpha_lower(c) ? c - 27 : c;
 }
 
