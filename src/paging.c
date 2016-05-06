82ed97580794b5aed0156ce86d0cb088b4e6a0bc
Vladislav Valtchev <vladi32@gmail.com>
2016-05-06 23:08:24 +0300

    the problem was related with the two 4KB buffers in paging.c. Replacing them with dynamic allocation. Switch to usermode with paging works.

--- src/paging.c
+++ src/paging.c
@@ -2,9 +2,10 @@
 #include <paging.h>
 #include <irq.h>
 #include <stringUtil.h>
+#include <kmalloc.h>
 
-page_directory_t kernel_page_dir __attribute__((aligned(4096)));
-page_table_t kernel_page_table __attribute__((aligned(4096)));
+page_directory_t *kernel_page_dir;
+page_table_t *kernel_page_table;
 
 void handle_page_fault(struct regs *r)
 {
@@ -23,33 +24,36 @@ void set_page_directory(page_directory_t *dir)
 
 void init_paging()
 {
+   kernel_page_dir = alloc_phys_page();
+   kernel_page_table = alloc_phys_page();
+
    set_fault_handler(FAULT_PAGE_FAULT, handle_page_fault);
 
-   kernel_page_dir.physical_address = &kernel_page_dir;
+   kernel_page_dir->physical_address = kernel_page_dir;
 
-   for (int i = 0; i < 512; i++) {
+   for (int i = 0; i < 1024; i++) {
 
       page_t page;
       memset(&page, 0, sizeof(page));
 
       page.present = 1;
       page.rw = 1;
-      page.us = 0;
+      page.us = 1; // temporary allowing user code to see these pages
 
       page.pageAddr = (0x1000 * i) >> 12;
 
-      kernel_page_table.pages[i] = page;
+      kernel_page_table->pages[i] = page;
    }
 
-   page_dir_entry_t kernel_2mb;
-   memset(&kernel_2mb, 0, sizeof(kernel_2mb));
+   page_dir_entry_t kernel_dir_entry;
+   memset(&kernel_dir_entry, 0, sizeof(kernel_dir_entry));
 
-   kernel_2mb.present = 1;
-   kernel_2mb.rw = 1;
-   kernel_2mb.us = 0;
-   kernel_2mb.pageTableAddr = ((uint32_t)&kernel_page_table) >> 12;
+   kernel_dir_entry.present = 1;
+   kernel_dir_entry.rw = 1;
+   kernel_dir_entry.us = 1;  // temporary allowing user code to see these pages
+   kernel_dir_entry.pageTableAddr = ((uint32_t)kernel_page_table) >> 12;
 
-   kernel_page_dir.entries[0] = kernel_2mb;
+   kernel_page_dir->entries[0] = kernel_dir_entry;
 
    page_dir_entry_t not_present;
    memset(&not_present, 0, sizeof(not_present));
@@ -59,9 +63,9 @@ void init_paging()
    not_present.us = 0;
 
    for (int i = 1; i < 1024; i++) {
-      kernel_page_dir.entries[i] = not_present;
+      kernel_page_dir->entries[i] = not_present;
    }
 
    //magic_debug_break();
-   set_page_directory(&kernel_page_dir);
+   set_page_directory(kernel_page_dir);
 }
