c74f2c7dd128492188eddf823c694736a50c7245
Vladislav Valtchev <vladi32@gmail.com>
2016-05-07 21:47:02 +0300

    map_page works with bochs and vm workstation but not with qemu

--- src/paging.c
+++ src/paging.c
@@ -27,17 +27,92 @@ void set_page_directory(page_directory_t *dir)
    asmVolatile("mov %eax, %cr0");
 }
 
+static void initialize_empty_page_table(page_table_t *t)
+{
+   for (int i = 0; i < 1024; i++) {
+      t->pages[i].present = 0;
+      t->pages[i].pageAddr = 0;
+   }
+}
+
+static void initialize_page_directory(page_directory_t *pdir,
+                                      void *physical_addr,
+                                      bool us)
+{
+   pdir->physical_address = physical_addr;
+
+   page_dir_entry_t not_present = {0};
+
+   not_present.present = 0;
+   not_present.rw = 0;
+   not_present.us = us;
+   not_present.pageTableAddr = 0;
+
+   for (int i = 0; i < 1024; i++) {
+      pdir->entries[i] = not_present;
+      pdir->page_tables[i] = NULL;
+   }
+}
+
+void map_page(page_directory_t *pdir,
+              uint32_t vaddr,
+              uint32_t paddr,
+              bool us,
+              bool rw)
+{
+   printk("Mapping paddr %p to vaddr %p..\n", paddr, vaddr);
+
+   uint32_t page_table_index = (vaddr >> 12) & 0x3FF;
+   uint32_t page_dir_index = (vaddr >> 22) & 0x3FF;
+
+   page_table_t *ptable = NULL;
+
+   printk("pag dir index: %p\n", page_dir_index);
+   printk("pag table index: %p\n", page_table_index);
+
+
+   if (pdir->page_tables[page_dir_index] == NULL) {
+
+      printk("No page table at that index, we have to create a page table\n");
+
+      // we have to create a page table for mapping 'vaddr'
+
+      ptable = alloc_phys_page();
+      initialize_empty_page_table(ptable);
+
+      page_dir_entry_t e = {0};
+      e.present = 1;
+      e.rw = 1;
+      e.pageTableAddr = ((uint32_t)ptable) >> 12; // physaddr = virtualaddr
+
+      pdir->page_tables[page_dir_index] = ptable;
+      pdir->entries[page_dir_index] = e;
+   }
+
+   ptable = pdir->page_tables[page_dir_index];
+
+   ASSERT(ptable->pages[page_table_index].present == 0);
+
+   page_t p = {0};
+   p.present = 1;
+   p.us = us;
+   p.rw = rw;
+
+   p.pageAddr = paddr >> 12;
+
+   ptable->pages[page_table_index] = p;
+}
+
 void init_paging()
 {
    set_fault_handler(FAULT_PAGE_FAULT, handle_page_fault);
    set_fault_handler(FAULT_GENERAL_PROTECTION, handle_general_protection_fault);
 
-   kernel_page_dir.physical_address = &kernel_page_dir;
+   initialize_page_directory(&kernel_page_dir, &kernel_page_dir, true);
 
    for (int i = 0; i < 1024; i++) {
 
-      page_t page;
-      memset(&page, 0, sizeof(page));
+      page_t page = {0};
 
       page.present = 1;
       page.rw = 1;
@@ -58,19 +133,24 @@ void init_paging()
 
    kernel_page_dir.entries[0] = kernel_dir_entry;
 
-   page_dir_entry_t not_present;
-   memset(&not_present, 0, sizeof(not_present));
 
-   not_present.present = 0;
-   not_present.rw = 1;
-   not_present.us = 0;
+   const char *str = "hello world in 3MB";
+   memcpy((void*) 0x300000, str, strlen(str) + 1);
 
-   for (int i = 1; i < 1024; i++) {
-      kernel_page_dir.entries[i] = not_present;
-   }
+   printk("String at (phys) 0x300000: %s\n", (const char *)0x300000);
 
-   //magic_debug_break();
+
+   map_page(&kernel_page_dir, 0x900000, 0x300000, true, true);
+
    set_page_directory(&kernel_page_dir);
 
    printk("Pagination is ON\n");
+
+   printk("[pagination test] string at 0x900000: %s\n", (const char *)0x900000);
+
+   for (int i = 0; i < 100; i++) {
+      printk("%x, ", ((char *)0x900000)[i]);
+   }
+
+   printk("\n\n");
 }
