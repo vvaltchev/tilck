b80c79b0d55447586630f2be856f1079a6d0600b
Vladislav Valtchev <vladi32@gmail.com>
2016-08-28 00:00:53 +0300

    removing the phy. addr member from the page_directory_t

--- include/paging.h
+++ include/paging.h
@@ -57,7 +57,6 @@ typedef struct {
 
    page_dir_entry_t entries[1024];  // actual entries used by the CPU
    page_table_t *page_tables[1024]; // pointers to the tables (virtual addreses)
-   void *physical_address;          // physical address of this struct
 
 } page_directory_t;
 
