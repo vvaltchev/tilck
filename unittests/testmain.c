32a99e1c507b7cd7ff5d4e23b9461d9caef01828
Vladislav Valtchev <vladi32@gmail.com>
2016-11-03 12:54:00 +0200

    making the unit test to work with kernel code

--- unittests/testmain.c
+++ unittests/testmain.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+void *_kmalloc(size_t);
+
 int main(int argc, char **argv) {
 
    printf("hello from the kernel unit tests!\n");
+
+   printf("kmalloc(10) returns: %p\n", _kmalloc(10));
 
    return 0;
 }
