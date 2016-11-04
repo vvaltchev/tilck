b317b629e61b38454c35d8a263f6fa117563b4c7
Vladislav Valtchev <vladi32@gmail.com>
2016-11-04 16:56:12 +0200

    fixing unittests

--- unittests/testmain.c
+++ unittests/testmain.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void *_kmalloc(size_t);
+void *kmalloc(size_t);
 
 int main(int argc, char **argv) {
 
    printf("hello from the kernel unit tests!\n");
 
-   printf("kmalloc(10) returns: %p\n", _kmalloc(10));
+   printf("kmalloc(10) returns: %p\n", kmalloc(10));
 
    return 0;
 }
