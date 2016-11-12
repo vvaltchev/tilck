c5411353578c68b1f59a9a406dba34e9220e162e
Vladislav Valtchev <vladi32@gmail.com>
2016-11-12 22:45:26 +0200

    minor changes in kmalloc_util

--- src/kmalloc_util.c
+++ src/kmalloc_util.c
@@ -6,7 +6,6 @@ bool kbasic_virtual_alloc(uintptr_t vaddr, int pageCount)
 {
    ASSERT(!(vaddr & 4095)); // the vaddr must be page-aligned
 
-
    page_directory_t *pdir = get_kernel_page_dir();
 
    // Ensure that we have enough physical memory.
@@ -14,19 +13,12 @@ bool kbasic_virtual_alloc(uintptr_t vaddr, int pageCount)
       return false;
    }
 
-#ifdef DEBUG
-   for (int i = 0; i < pageCount; i++) {
-      if (is_mapped(pdir, vaddr + (i << 12))) {
-         return false;
-      }
-   }
-#endif
-
    for (int i = 0; i < pageCount; i++) {
 
       void *paddr = alloc_phys_page();
       ASSERT(paddr != NULL);
 
+      ASSERT(!is_mapped(pdir, vaddr + (i << 12)));
       map_page(pdir, vaddr + (i << 12), (uintptr_t)paddr, false, true);
    }
 
@@ -39,20 +31,11 @@ bool kbasic_virtual_free(uintptr_t vaddr, int pageCount)
 
    page_directory_t *pdir = get_kernel_page_dir();
 
-   //printk("Free %i pages at %p\n", pageCount, vaddr);
-
-#ifdef DEBUG
-   for (int i = 0; i < pageCount; i++) {
-      if (!is_mapped(pdir, vaddr + (i << 12))) {
-         return false;
-      }
-   }
-#endif
-
    for (int i = 0; i < pageCount; i++) {
 
       uintptr_t va = vaddr + (i << 12);
 
+      // get_mapping ASSERTs that 'va' is mapped.
       void *paddr = get_mapping(pdir, va);
 
       // un-map the virtual address.
