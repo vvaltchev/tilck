dcc1faf8601865185de3fd7a9d50939a737a94db
darthvader88 <vladi32@gmail.com>
2016-11-20 17:09:35 +0200

    Working on bootloader (#9)
    Improving the stage1 of the bootloader and making possible to boot using also a hard drive, after fixing a nasty problem in the build system. (exos.img was less than 2880 sectors, due to a previous unlucky change in the main Makefile.)
    
    The exos.img file has been tested on a real PC by booting using an usbstick (seen as drive 0), as well.

--- bootloader/boot_stage1.asm
+++ bootloader/boot_stage1.asm
@@ -4,11 +4,10 @@
 [ORG 0x0000]
 
 %define BASE_LOAD_SEG 0x07C0
-%define SECTORS_TO_READ_AT_TIME 1
 %define DEST_DATA_SEGMENT 0x2000
 
 start:
-   
+
    mov ax, BASE_LOAD_SEG
    add ax, (8192 / 16)         ; 8K buffer
    cli                         ; Disable interrupts while changing stack
@@ -26,15 +25,71 @@ start:
    mov al, 0x3 ; 80x25 mode
    int 0x10
 
-   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;   
+
+   mov [current_device], dl
+
+   mov ah, 0x00  ; reset device
+   int 0x13
+   jc end
    
-   mov word [currSectorNum], 1 ; sector 1 (512 bytes after this bootloader)
+   .reset_ok:
    
-   .big_load_loop:
-   
+   mov si, dev
+   call print_string
+   mov ax, [current_device]
+   call print_num
+
+   xor ax, ax
+   mov es, ax
+   mov di, ax
+
+   mov dl, [current_device]
+   mov ah, 0x8 ; read drive parameters
+   int 0x13
+
+   jnc after_read_params_ok
+
+   mov si, read_params_failed
+   call print_string
+   jmp end
+
+   after_read_params_ok:
+
+   xor ax, ax
+   mov al, dh
+   inc al
+
+   mov [HeadsPerCylinder], ax
+
+   mov ax, cx
+   and ax, 63   ; last 6 bits
+   mov [SectorsPerTrack], ax
+
+   xor ax, ax
+   mov al, ch  ; higher 8 bits of CX = lower bits for cyclinders count
+   and cx, 192 ; bits 6 and 7 of CX = higher 2 bits for cyclinders count
+   shl cx, 8
+   or ax, cx
+   inc ax
+   mov [CylindersCount], ax
+
+
+   ; mov ax, [CylindersCount]  ; we have already the value in ax
+   call print_num
+
+   mov ax, [HeadsPerCylinder]
+   call print_num
+
+   mov ax, [SectorsPerTrack]
+   call print_num
+
+
+
+   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
    .load_loop:
 
-   ; xchg bx, bx ; magic break
+   ;xchg bx, bx ; magic break
 
    mov ax, [currSectorNum]
    call lba_to_chs
@@ -42,64 +97,129 @@ start:
    mov ax, [currDataSeg]
    mov es, ax        ; Store currDataSeg in ES, the destination address of the sectors read
                      ; (AX is used since we cannot store directly IMM value in ES)
-                     
+
    mov bx, [currSectorNum]
-   shl bx, 9         ; Sectors read from floppy are stored in ES:BX
+   shl bx, 9         ; Sectors read are stored in ES:BX
                      ; bx = 512 * counter * bx
-   
+
    ; 20-bit address in 8086 (real mode)
    ; SEG:OFF
    ; ADDR20 = (SEG << 4) | OFF
 
-   mov ah, 2         ; Params for int 13h: read floppy sectors
-   mov al, SECTORS_TO_READ_AT_TIME
+   mov ah, 0x02      ; Params for int 13h: read sectors
+   mov al, 1         ; Read just 1 sector at time
+
+   ; save the CHS parameters for error messages
+   mov [saved_cx], cx
+   mov [saved_dx], dx
 
    int 13h
-   
+
+   ;call print_chs
    ;xchg bx, bx ; magic break
-   
+
    jc .load_error
 
    mov ax, [currSectorNum]
-   add ax, SECTORS_TO_READ_AT_TIME
+   inc ax                    ; we read just 1 sector at time
    mov [currSectorNum], ax
-   
-   sub ax, 1
+
+   ; If the current sector num have the bits 0-7 unset,
+   ; we loaded 128 sectors * 512 bytes = 64K.
+   ; We have to increase the segment.
+
+   dec ax
    and ax, 0x7F
-   cmp ax, 0
-   je .end_small_load_loop 
-   jmp .load_loop
+   test ax, ax
+   jne .load_loop ; JMP if ax != 0
 
-   .end_small_load_loop:
-
-   ; xchg bx, bx ; magic break
-
-   mov ax, [currDataSeg] 
+   mov ax, [currDataSeg]
    cmp ax, 0x9FE0 ; so, we'd have 0x20000 - 0x9FFFF for the kernel (512 KB)
    je .load_OK
-   
+
+   ; Increment the segment by 4K => 64K in plain address
    add ax, 0x1000
    mov [currDataSeg], ax
-   jmp .big_load_loop
-   
+   jmp .load_loop
+
 .load_error:
 
-   push err1
+   ; The load failed for some reason
+
+   mov ah, 0x01 ; Get Status of Last Drive Operation
+   mov dl, [current_device]
+   int 0x13
+
+   jnc .los_ok
+   
+   mov si, los_failed
    call print_string
-   add sp, 2
-   jmp end
+   
+   .los_ok:
+   
+   ; We have now in AH the last error
+   shr ax, 8 ; move AH in AL and make AH=0
+   mov si, last_op_status
+   call print_string
+   call print_num
+   
+   
+   ;print the sector number
+   mov si, load_failed
+   call print_string
+   mov ax, [currSectorNum]
+   call print_num
+
+   call print_chs
+
+   ; continue to boot anyway since with hdd we fail after loading 270 sectors
+   ; which are enough for the moment, but that's not OK of course.
+
+   ; xchg bx, bx
+
+   ; burn some cycles to wait before booting
+
+   xor eax, eax
+
+   .loop:
+   dec eax
+   test eax, eax
+   jne .loop      ; JMP if EAX != 0
 
 .load_OK:
 
-   push str1
-   call print_string
-   add sp, 2 
-
-   ; xchg bx, bx ; magic break   
+   ; xchg bx, bx ; magic break
    jmp DEST_DATA_SEGMENT:0x0000
-   
+
 end:
    jmp end
+
+print_chs:
+   pusha
+
+   ; print the cylinder param
+   mov si, cyl_param
+   call print_string
+   mov ax, [saved_cx]
+   shr ax, 8     ; put 'ch' in al
+   call print_num
+
+   ; print the head param
+   mov si, head_param
+   call print_string
+   mov ax, [saved_dx]
+   shr ax, 8    ; put 'dh' in al
+   call print_num
+
+   ; print the sector param
+   mov si, sector_param
+   call print_string
+   mov ax, [saved_cx]
+   xor ah, ah
+   call print_num
+
+   popa
+   ret
 
 lba_to_chs:         ; Calculate head, track and sector settings for int 13h
                     ; IN: logical sector in AX, OUT: correct registers for int 13h
@@ -108,15 +228,24 @@ lba_to_chs:         ; Calculate head, track and sector settings for int 13h
 
    mov bx, ax        ; Save logical sector
 
-   mov dx, 0         ; First the sector
-   div word [SectorsPerTrack]
-   add dl, 01h       ; Physical sectors start at 1
-   mov cl, dl        ; Sectors belong in CL for int 13h
-   mov ax, bx
+   ; DIV {ARG}
+   ; divides DX:AX by {ARG}
+   ; quotient => AX
+   ; reminder => DX
 
-   mov dx, 0         ; Now calculate the head
+
+   xor dx, dx        ; First the sector
    div word [SectorsPerTrack]
-   mov dx, 0
+   inc dl            ; Physical sectors start at 1
+   mov cl, dl        ; Sectors belong in CL for int 13h
+   and cl, 63        ; Make sure the upper two bits of CL are unset
+
+
+   mov ax, bx        ; reload the LBA sector in AX
+
+   xor dx, dx        ; reset DX and calculate the head
+   div word [SectorsPerTrack]
+   xor dx, dx
    div word [HeadsPerCylinder]
    mov dh, dl        ; Head
    mov ch, al        ; Cylinder
@@ -124,41 +253,130 @@ lba_to_chs:         ; Calculate head, track and sector settings for int 13h
    pop ax
    pop bx
 
-   mov dl, 0      ; Set correct device
+   mov dl, [current_device]      ; Set correct device
 
    ret
 
-print_string:      ; Routine: output string in SI to screen
-   push bp
-   mov bp, sp
+print_num:
+
+   push strBuf
+   push ax ; the input number
+   call itoa
+   add sp, 4
+
+   mov si, strBuf
+   call print_string
+
+   mov si, newline
+   call print_string
+
+   ret
+
+
+print_string:
+
+   push ax         ; save AX for the caller
    
-   mov si, [bp+4]  ; bp+4 is the first argument
-   mov ah, 0Eh     ; int 10h 'print char' function
+   mov ah, 0x0E    ; int 10h 'print char' function
 
 .repeat:
    lodsb           ; Get character from string
-   cmp al, 0
+   test al, al
    je .done        ; If char is zero, end of string
    int 10h         ; Otherwise, print it
    jmp .repeat
 
 .done:
+   pop ax
+   ret
+
+itoa: ; convert 16-bit integer to string
+
+;  USAGE:
+;  push destbuffer
+;  push number
+;
+;  call itoa
+;  add sp, 4
+
+   push bp
+   mov bp, sp
+   sub sp, 24
+
+   mov [bp-2], bp
+   sub word [bp-2], 4
+
+   .loop:
+
+   xor dx, dx
+   mov ax, [bp+4]
+   mov bx, 10
+   div bx
+   mov [bp+4], ax
+
+   mov bx, [bp-2]
+   add dl, 48
+   mov [bx], dl
+   add word [bp-2], 1
+
+   mov cx, [bp+4]
+   test cx, cx
+   jne .loop   ; JMP if cx != 0
+
+   mov bx, [bp-2]
+   sub bx, 1
+
+
+   mov di, [bp+6]
+
+   .l2:
+
+   mov ax, [bx]
+   mov [di], ax
+
+   dec bx
+   inc di
+
+   mov ax, bp
+   sub ax, 4
+   cmp bx, ax
+   jge .l2
+
+   mov byte [di], 0
+
    leave
    ret
-   
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-SectorsPerTrack      dw 18    ; Sectors per track (36/cylinder)
-HeadsPerCylinder     dw 2
-str1                 db 'This is my bootloader!', 10, 13, 0
-err1                 db 'ERROR while loading kernel!', 10, 13, 0
+SectorsPerTrack      dw 0
+HeadsPerCylinder     dw 0
+CylindersCount       dw 0
+
+saved_cx             dw 0
+saved_dx             dw 0
+
 newline              db 10, 13, 0
-currSectorNum        dw 0
+dev                  db 'Dev:', 0
+load_failed          db 'LBA:', 0
+cyl_param            db 'C:', 0
+head_param           db 'H:', 0
+sector_param         db 'S:', 0
+last_op_status       db 'LOS:', 0
+read_params_failed   db 'F1', 10, 13, 0
+los_failed           db 'F2', 10, 13, 0
+
+current_device       dw 0
+
+currSectorNum        dw 1
 
                      ; Hack the destination segment in a way to avoid
                      ; special calculations in order to skip the first sector
                      ; of the floppy.
 currDataSeg          dw (DEST_DATA_SEGMENT - 512/16)
+
+strBuf               times 8 db 0
 
 times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
 dw 0xAA55               ; The standard PC boot signature
