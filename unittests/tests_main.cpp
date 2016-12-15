5ec4524360c54a30e6c1ef4590cc76bab9dfc7f1
Vladislav Valtchev <vladi32@gmail.com>
2016-12-15 17:40:15 +0200

    refactoring tests

--- unittests/tests_main.cpp
+++ unittests/tests_main.cpp
@@ -18,46 +18,7 @@ void kmalloc_perf_test();
 
 using namespace std;
 
-extern "C" {
 
-
-void *kernel_heap_base = nullptr;
-
-
-unordered_map<uptr, uptr> mappings;
-
-bool __wrap_is_mapped(void *pdir, uptr vaddr)
-{
-   return mappings[vaddr & PAGE_MASK] != 0;
-}
-
-bool __wrap_kbasic_virtual_alloc(uptr vaddr, int pageCount)
-{
-   assert((vaddr & (PAGE_SIZE - 1)) == 0);
-
-   for (int i = 0; i < pageCount; i++) {
-      void *p = alloc_pageframe();
-      mappings[vaddr + i * PAGE_SIZE] = (uptr)p;
-   }
-
-   return true;
-}
-
-bool __wrap_kbasic_virtual_free(uptr vaddr, int pageCount)
-{
-   assert((vaddr & (PAGE_SIZE - 1)) == 0);
-
-   for (int i = 0; i < pageCount; i++) {
-      
-      uptr phys_addr = mappings[vaddr + i * PAGE_SIZE];
-      free_pageframe((void *)phys_addr);
-      mappings[vaddr + i * PAGE_SIZE] = 0;
-   }
-
-   return true;
-}
-
-}
 
 void init_test_kmalloc()
 {
