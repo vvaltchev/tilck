4b676c5100116656a3a7fca628ba2374e83a4db6
Vladislav Valtchev <vladi32@gmail.com>
2016-12-31 15:07:45 +0200

    moving syscall table under an #ifdef

--- kernel/syscalls.c
+++ kernel/syscalls.c
@@ -7,19 +7,15 @@
 
 typedef sptr (*syscall_type)();
 
-// 0
 sptr sys_setup()
 {
    return 0;
 }
 
-// 1
 sptr sys_exit(int code);
 
-// 2
 sptr sys_fork();
 
-// 3
 sptr sys_read(int fd, void *buf, size_t count)
 {
    //printk("sys_read(fd = %i, count = %u)\n", fd, count);
@@ -37,7 +33,6 @@ sptr sys_write(int fd, const void *buf, size_t count)
    return 0;
 }
 
-// 5
 sptr sys_open(const char *pathname, int flags, int mode)
 {
    printk("sys_open(filename = '%s', "
@@ -45,122 +40,109 @@ sptr sys_open(const char *pathname, int flags, int mode)
    return 825;
 }
 
-// 6
 sptr sys_close(int fd)
 {
    printk("sys_close(fd = %d)\n", fd);
    return 0;
 }
 
-// 7
 sptr sys_waitpid()
 {
    return 0;
 }
 
-// 8
 sptr sys_creat()
 {
    return 0;
 }
 
-// 9
 sptr sys_link()
 {
    return 0;
 }
 
-// 10
 sptr sys_unlink()
 {
    return 0;
 }
 
-// 11
 sptr sys_execve()
 {
    return 0;
 }
 
-// 12
 sptr sys_chdir()
 {
    return 0;
 }
 
-// 13
 sptr sys_time()
 {
    return 0;
 }
 
-// 14
 sptr sys_mknod()
 {
    return 0;
 }
 
-// 15
 sptr sys_chmod()
 {
    return 0;
 }
 
-// 16
 sptr sys_lchown()
 {
    return 0;
 }
 
-// 17
 sptr sys_break()
 {
    return 0;
 }
 
-// 18
 sptr sys_oldstat()
 {
    return 0;
 }
 
-// 19
 sptr sys_lseek()
 {
    return 0;
 }
 
-// 20
 sptr sys_getpid();
 
+#ifdef __i386__
+
+// The syscall numbers are ARCH-dependent
 syscall_type syscalls_pointers[] =
 {
-   sys_setup,
-   sys_exit,
-   sys_fork,
-   sys_read,
-   sys_write,
-   sys_open,
-   sys_close,
-   sys_waitpid,
-   sys_creat,
-   sys_link,
-   sys_unlink,
-   sys_execve,
-   sys_chdir,
-   sys_time,
-   sys_mknod,
-   sys_chmod,
-   sys_lchown,
-   sys_break,
-   sys_oldstat,
-   sys_lseek,
-   sys_getpid
+   sys_setup,    //  0
+   sys_exit,     //  1
+   sys_fork,     //  2
+   sys_read,     //  3
+   sys_write,    //  4
+   sys_open,     //  5
+   sys_close,    //  6
+   sys_waitpid,  //  7
+   sys_creat,    //  8
+   sys_link,     //  9
+   sys_unlink,   // 10
+   sys_execve,   // 11
+   sys_chdir,    // 12
+   sys_time,     // 13
+   sys_mknod,    // 14
+   sys_chmod,    // 15
+   sys_lchown,   // 16
+   sys_break,    // 17
+   sys_oldstat,  // 18
+   sys_lseek,    // 19
+   sys_getpid    // 20
 };
 
 const ssize_t syscall_count = ARRAY_SIZE(syscalls_pointers);
 
-#ifdef __i386__
 
 #include <arch/i386/arch_utils.h>
 
