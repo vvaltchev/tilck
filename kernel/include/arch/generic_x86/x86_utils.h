8360567cd31c93c542ec4442d78bba97f11476f6
Vladislav Valtchev <vladi32@gmail.com>
2017-05-17 10:12:58 +0300

    adding a native python system test

--- kernel/include/arch/generic_x86/x86_utils.h
+++ kernel/include/arch/generic_x86/x86_utils.h
@@ -164,6 +164,16 @@ static ALWAYS_INLINE uptr get_eflags()
 
 #endif
 
+
+/*
+ * For the moment, keep those defines always defined.
+ * Unless qemu is run with -device isa-debug-exit,iobase=0xf4,iosize=0x04
+ * debug_qemu_turn_off_machine() won't turn off the VM.
+ */
+
+#define DEBUG_QEMU_EXIT_ON_INIT_EXIT
+#define DEBUG_QEMU_EXIT_ON_PANIC
+
 void debug_qemu_turn_off_machine();
 
 // Reboot the system
