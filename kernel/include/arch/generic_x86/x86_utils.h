dc55d2c8d295fbdcb5efc09b73073b7165f081aa
Vladislav Valtchev <vladi32@gmail.com>
2017-05-24 12:38:02 +0300

    fixing the nastly bug in disable_interrupts()

--- kernel/include/arch/generic_x86/x86_utils.h
+++ kernel/include/arch/generic_x86/x86_utils.h
@@ -15,6 +15,13 @@
 #define X86_PC_ACPI_IRQ        9
 #define X86_PC_PS2_MOUSE_IRQ  12
 
+#define X86_EFLAGS_CF (1 << 0) // carry flag
+#define X86_EFLAGS_ZF (1 << 6) // zero flag
+#define X86_EFLAGS_SF (1 << 7) // sign flag
+#define X86_EFLAGS_IF (1 << 9) // interrupts enabled flag
+#define X86_EFLAGS_DF (1 << 10) // direction flag
+#define X86_EFLAGS_OF (1 << 11) // overflow flag
+
 
 static ALWAYS_INLINE u64 RDTSC()
 {
@@ -56,33 +63,6 @@ static ALWAYS_INLINE void halt()
    asmVolatile("hlt");
 }
 
-extern volatile int disable_interrupts_count;
-
-
-
-static ALWAYS_INLINE void enable_interrupts()
-{
-   ASSERT(disable_interrupts_count > 0);
-
-   if (--disable_interrupts_count == 0) {
-
-#if !defined(TESTING) && !defined(KERNEL_TEST)
-      asmVolatile("sti");
-#endif
-
-   }
-}
-
-static ALWAYS_INLINE void enable_interrupts_forced()
-{
-   disable_interrupts_count = 0;
-
-#if !defined(TESTING) && !defined(KERNEL_TEST)
-   asmVolatile("sti");
-#endif
-}
-
-
 static ALWAYS_INLINE void wrmsr(u32 msr_id, u64 msr_value)
 {
    asmVolatile( "wrmsr" : : "c" (msr_id), "A" (msr_value) );
@@ -95,76 +75,8 @@ static ALWAYS_INLINE u64 rdmsr(u32 msr_id)
    return msr_value;
 }
 
-extern volatile bool in_panic;
 
-static inline bool are_interrupts_enabled_int(const char *file, int line)
-{
-   uptr flags;
-   asmVolatile("pushf\n\t"
-               "pop %0"
-               : "=g"(flags) );
-
-   bool interrupts_on = flags & (1 << 9);
-
-#ifdef DEBUG
-
-   if (interrupts_on != (disable_interrupts_count == 0)) {
-      if (!in_panic) {
-         panic("FAILED interrupts check.\nFile: %s on line %i.\n"
-               "interrupts_on: %s\ndisable_interrupts_count: %i",
-               file, line, interrupts_on ? "TRUE" : "FALSE",
-               disable_interrupts_count);
-      }
-   }
-
-#endif
-
-   return interrupts_on;
-}
-
-#define are_interrupts_enabled() are_interrupts_enabled_int(__FILE__, __LINE__)
-
-static ALWAYS_INLINE void disable_interrupts()
-{
-   ASSERT(disable_interrupts_count >= 0);
-
-   if (++disable_interrupts_count == 1) {
-
-#if !defined(TESTING) && !defined(KERNEL_TEST)
-      asmVolatile("cli");
-#endif
-   }
-
-#if !defined(TESTING) && !defined(KERNEL_TEST)
-   ASSERT(!are_interrupts_enabled());
-#endif
-}
-
-static ALWAYS_INLINE void disable_interrupts_forced()
-{
-   disable_interrupts_count = 1;
-
-#if !defined(TESTING) && !defined(KERNEL_TEST)
-   asmVolatile("cli");
-   ASSERT(!are_interrupts_enabled());
-#endif
-}
-
-
-static ALWAYS_INLINE void cpuid(int code, u32 *a, u32 *d)
-{
-    asmVolatile( "cpuid" : "=a"(*a), "=d"(*d) : "0"(code) : "ebx", "ecx" );
-}
-
-/*
- * Invalidates the TLB entry used for resolving the page containing 'vaddr'.
- */
-static ALWAYS_INLINE void invalidate_page(uptr vaddr)
-{
-   asmVolatile("invlpg (%0)" ::"r" (vaddr) : "memory");
-}
-
-#ifdef BITS32
+#if defined(BITS32) && !defined(UNIT_TEST_ENVIRONMENT)
 
 static ALWAYS_INLINE uptr get_eflags()
 {
@@ -184,6 +96,125 @@ static ALWAYS_INLINE uptr get_eflags()
 }
 
 #endif
+
+
+
+#if !defined(TESTING) && !defined(KERNEL_TEST)
+#  define HW_disable_interrupts() asmVolatile("cli")
+#  define HW_enable_interrupts() asmVolatile("sti")
+#else
+#  define HW_disable_interrupts()
+#  define HW_enable_interrupts()
+#endif
+
+
+extern volatile bool in_panic;
+extern volatile int disable_interrupts_count;
+
+static inline bool are_interrupts_enabled_int(const char *file, int line)
+{
+   uptr flags;
+   asmVolatile("pushf\n\t"
+               "pop %0"
+               : "=g"(flags) );
+
+   bool interrupts_on = !!(flags & X86_EFLAGS_IF);
+
+#ifdef DEBUG
+
+   if (interrupts_on) {
+      // If the interrupts are ON, we have to disable them just in order to
+      // check the value of disable_interrupts_count.
+      HW_disable_interrupts();
+   }
+
+   if (interrupts_on != (disable_interrupts_count == 0)) {
+      if (!in_panic) {
+         panic("FAILED interrupts check.\nFile: %s on line %i.\n"
+               "interrupts_on: %s\ndisable_interrupts_count: %i",
+               file, line, interrupts_on ? "TRUE" : "FALSE",
+               disable_interrupts_count);
+      }
+   }
+
+   if (interrupts_on) {
+      HW_enable_interrupts();
+   }
+
+#endif
+
+   return interrupts_on;
+}
+
+#define are_interrupts_enabled() are_interrupts_enabled_int(__FILE__, __LINE__)
+
+static ALWAYS_INLINE void enable_interrupts_forced()
+{
+   disable_interrupts_count = 0;
+   HW_enable_interrupts();
+}
+
+static ALWAYS_INLINE void disable_interrupts_forced()
+{
+   HW_disable_interrupts();
+   disable_interrupts_count = 1;
+}
+
+#ifndef UNIT_TEST_ENVIRONMENT
+
+static ALWAYS_INLINE void enable_interrupts()
+{
+#if !defined(TESTING) && !defined(KERNEL_TEST)
+   ASSERT(!are_interrupts_enabled());
+#endif
+
+   ASSERT(disable_interrupts_count > 0);
+
+   if (--disable_interrupts_count == 0) {
+      HW_enable_interrupts();
+   }
+}
+
+static ALWAYS_INLINE void disable_interrupts()
+{
+   uptr eflags = get_eflags();
+
+   if (eflags & X86_EFLAGS_IF) {
+
+      // interrupts are enabled: disable them first.
+      HW_disable_interrupts();
+
+      ASSERT(disable_interrupts_count == 0);
+
+   } else {
+
+      // interrupts are already disabled: just increase the counter.
+      ASSERT(disable_interrupts_count > 0);
+   }
+
+   ++disable_interrupts_count;
+}
+
+#else
+
+#define enable_interrupts()
+#define disable_interrupts()
+
+#endif // ifndef UNIT_TEST_ENVIRONMENT
+
+static ALWAYS_INLINE void cpuid(int code, u32 *a, u32 *d)
+{
+    asmVolatile( "cpuid" : "=a"(*a), "=d"(*d) : "0"(code) : "ebx", "ecx" );
+}
+
+/*
+ * Invalidates the TLB entry used for resolving the page containing 'vaddr'.
+ */
+static ALWAYS_INLINE void invalidate_page(uptr vaddr)
+{
+   asmVolatile("invlpg (%0)" ::"r" (vaddr) : "memory");
+}
+
 
 void validate_stack_pointer_int(const char *file, int line);
 
