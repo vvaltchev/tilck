949c9c37e86b5915194d64a3b36eb3b0f69565d7
Vladislav Valtchev <vladi32@gmail.com>
2017-07-26 12:54:41 +0300

    adding a priority queue impl

---
+++ kernel/include/pq.h
@@ -0,0 +1,23 @@
+
+#include <common_defs.h>
+
+typedef struct pqueue_elem pqueue_elem;
+typedef struct pqueue pqueue;
+
+struct pqueue {
+
+   size_t size;
+   size_t capacity;
+   pqueue_elem *elems;
+};
+
+void pqueue_init(pqueue *pq, size_t capacity);
+void pqueue_destroy(pqueue *pq);
+
+void pqueue_push(pqueue *pq, void *data, int priority);
+void *pqueue_pop(pqueue *pq);
+
+static inline bool pqueue_is_empty(pqueue *pq)
+{
+   return pq->size == 0;
+}
---
+++ kernel/pq.c
@@ -0,0 +1,106 @@
+
+
+#include <common_defs.h>
+#include <kmalloc.h>
+#include <pq.h>
+
+#define LEFT(i) (2*(i)+1)
+#define RIGHT(i) (2*(i)+2)
+#define PARENT(i) ((i - 1)/2)
+
+struct pqueue_elem {
+
+   void *data;
+   int priority;
+};
+
+void pqueue_init(pqueue *pq, size_t capacity)
+{
+   pq->size = 0;
+   pq->capacity = capacity;
+   pq->elems = kmalloc(capacity * sizeof(pqueue_elem));
+}
+
+void pqueue_destroy(pqueue *pq)
+{
+   kfree(pq->elems, pq->capacity);
+   bzero(pq, sizeof(pqueue_elem));
+}
+
+static inline void pq_swap(pqueue *pq, int i, int j)
+{
+   pqueue_elem tmp = pq->elems[i];
+   pq->elems[i] = pq->elems[j];
+   pq->elems[j] = tmp;
+}
+
+void pqueue_push(pqueue *pq, void *data, int p)
+{
+   ASSERT(pq->size < pq->capacity);
+
+   pq->elems[pq->size].data = data;
+   pq->elems[pq->size].priority = p;
+
+   pq->size++;
+
+   if (pq->size <= 1)
+      return;
+
+   // heap-up operation
+   for (int i = pq->size - 1;
+        pq->elems[i].priority < pq->elems[PARENT(i)].priority; i = PARENT(i)) {
+      pq_swap(pq, i, PARENT(i));
+   }
+}
+
+
+static void heap_down(pqueue *pq)
+{
+   pqueue_elem *heap = pq->elems;
+   int len = pq->size;
+
+   if (len <= 1) return;
+
+   int i = 0;
+
+   while (LEFT(i) < len) {
+
+      int e = heap[i].priority;
+      int l = heap[LEFT(i)].priority;
+
+      if (RIGHT(i) < len) {
+
+         int r = heap[RIGHT(i)].priority;
+
+         if (e < l && e < r)
+            break;
+
+         /* index of the smallest child */
+         int si = l < r ? LEFT(i) : RIGHT(i);
+
+         pq_swap(pq, i, si);
+         i = si;
+
+      } else if (l < e) {
+
+         pq_swap(pq, i, LEFT(i));
+         i = LEFT(i);
+
+      } else {
+         break;
+      }
+   }
+}
+
+
+void *pqueue_pop(pqueue *pq)
+{
+   ASSERT(pq->size > 0);
+   void *res = pq->elems[0].data;
+
+   pq_swap(pq, 0, pq->size - 1);
+   pq->size--;
+
+   heap_down(pq);
+   return res;
+}
