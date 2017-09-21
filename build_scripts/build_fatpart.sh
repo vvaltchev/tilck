03c1281dd341e575b0319e9416222ca15034fc98
Vladislav Valtchev <vladi32@gmail.com>
2017-09-22 00:35:02 +0300

    Introducing partial support for FAT16 and FAT32 (#42)
    This change introduces in the kernel a partial read-only support for FAT16 and FAT32.
    The kernel now it is able to walk through the directory-tree of a FAT file-system and read a whole file.
    The walk-through across cluster chains is fully supported, both for directory entries and files.
    Only the "Long Directory Entries" feature has not been implemented yet, but it certainly will. Since this will be the FS used by the system, clearly the 11 chars length-limit is not acceptable.
    
    Intentionally not implemented: handling of bad clusters and fall-back to other FATs different than the first one. That's clearly because this code has been designed to work only with FAT volumes in ramdisks, where bad clusters simply do not exist.

--- build_scripts/build_fatpart.sh
+++ build_scripts/build_fatpart.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Args: <build dir> <dest img file>
 
@@ -6,10 +6,26 @@ bdir=$1
 dest=$1/$2
 
 if [ ! -f $1 ]; then
-	# If the file does not already exist
-	dd status=none if=/dev/zero of=$dest bs=1M count=16
-	mformat -i $dest -T 32256 -h 16 -s 63 ::
+   # If the file does not already exist
+   dd status=none if=/dev/zero of=$dest bs=1M count=16
+   mformat -i $dest -T 32256 -h 16 -s 63 ::
+   mlabel -i $dest ::EXOS
 fi
 
-mcopy -i $dest $bdir/init ::/
+rm -rf $bdir/sysroot
+cp -r $bdir/../sysroot $bdir/
 
+cd $bdir/sysroot
+
+# hard-link init to sysroot/sbin
+ln $bdir/init sbin/
+
+# first, create the directories
+for f in $(find * -type d); do
+   mmd -i $dest $f
+done
+
+# then, copy all the files in sysroot
+for f in $(find * -type f); do
+   mcopy -i $dest $f ::/$f
+done
