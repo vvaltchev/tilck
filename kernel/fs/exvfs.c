3150d5dafb385af03e712eab00369bbb4a978dbb
Vladislav Valtchev <vladi32@gmail.com>
2017-12-29 16:46:04 +0200

    [FS] Working on mounting a filesystem

--- kernel/fs/exvfs.c
+++ kernel/fs/exvfs.c
@@ -1,1 +1,37 @@
 
+#include <fs/exvfs.h>
+#include <string_util.h>
+#include <kmalloc.h>
+
+/* exOS is small: supporting 16 mount points seems more than enough. */
+mountpoint *mps[16] = {0};
+
+void mountpoint_add(filesystem *fs, const char *path)
+{
+   u32 i;
+
+   for (i = 0; i < ARRAY_SIZE(mps); i++)
+      if (!mps[i])
+         break;
+
+   VERIFY(i < ARRAY_SIZE(mps));
+
+   size_t path_len = strlen(path);
+   mountpoint *mp = kmalloc(sizeof(mountpoint) + path_len + 1);
+   mp->fs = fs;
+   memcpy(mp->path, path, path_len + 1);
+   mps[i] = mp;
+}
+
+void mountpoint_remove(filesystem *fs)
+{
+   for (u32 i = 0; i < ARRAY_SIZE(mps); i++) {
+      if (mps[i] && mps[i]->fs == fs) {
+         kfree(mps, sizeof(mountpoint) + strlen(mps[i]->path) + 1);
+         mps[i] = NULL;
+         break;
+      }
+   }
+
+   NOT_REACHED();
+}
--- kernel/include/fs/exvfs.h
+++ kernel/include/fs/exvfs.h
@@ -1,29 +1,47 @@
 
+#pragma once
 
 /*
  * exOS's toy virtual file system
  *
  * As this project's goals are by far different from the Linux ones, this
- * layer won't provide anything even as close as what Linux's VFS does.
- * Its purpose is to provide the MINIMUM NECESSARY to allow basic operations
- * like open, read, write, close to work both on FAT32 and on character devices
- * like /dev/tty0 (when it will implemented).
+ * layer won't provide anything close to the Linux's VFS. Its purpose is to
+ * provide the MINIMUM NECESSARY to allow basic operations like open, read,
+ * write, close to work both on FAT32 and on character devices like /dev/tty0
+ * (when it will implemented). In particular:
+ *
+ *    - No real disk I/O will be supported
+ *    - No disk cache
+ *    - No access control: single user (root) system
+ *    - No SMP
+ *    - Only the simplest Linux syscalls will be supported
  *
  */
 
-typedef void *fs_file_handle;
+#include <common_defs.h>
 
+typedef void *fs_handle;
+typedef struct filesystem filesystem;
 
-typedef fs_file_handle (*func_open) (const char *);
-typedef void (*func_close) (fs_file_handle);
+typedef fs_handle (*func_open) (filesystem *, const char *);
+typedef void (*func_close) (filesystem *, fs_handle);
+typedef int (*func_read) (filesystem *, fs_handle, char *, size_t);
+typedef int (*func_write) (filesystem *, fs_handle, char *, size_t);
 
+struct filesystem {
 
+   void *device_data;
+
+   func_open fopen;
+   func_close fclose;
+   func_read fread;
+   func_write fwrite;
+
+};
 
 typedef struct {
 
-   char name[16];
+   filesystem *fs;
+   char path[0];
 
-
-
-} filesystem;
-
+} mountpoint;
