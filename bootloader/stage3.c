e43346f13989a1e05a35d7604c261e18661b902f
Vladislav Valtchev <vladi32@gmail.com>
2018-01-08 22:06:19 +0200

    [bootloader] Adding basic term support in the 3rd stage

--- bootloader/stage3.c
+++ bootloader/stage3.c
@@ -1,9 +1,140 @@
 
+#include <common_defs.h>
+#include <arch/generic_x86/x86_utils.h>
+#include <term.h>
+
+#define TERMINAL_VIDEO_ADDR ((volatile u16*)(0 + 0xB8000))
+
+#define TERM_WIDTH  80
+#define TERM_HEIGHT 25
+
+#define RAM_DISK_PADDR (0x8000000U) // +128 M
+
+
+/* ----- Declarations of asm functions ------ */
 
 void jump_to_kernel(void);
+
+/* ------------------------------------------- */
+
+
+void memcpy(volatile void *dest, const volatile void *src, size_t n)
+{
+   for (size_t i = 0; i < n; i++) {
+      ((char*)dest)[i] = ((char*)src)[i];
+   }
+}
+
+
+volatile uint8_t terminal_row = 0;
+volatile uint8_t terminal_column = 0;
+volatile uint8_t terminal_color = 0;
+
+void term_setcolor(uint8_t color) {
+   terminal_color = color;
+}
+
+void term_movecur(int row, int col)
+{
+   uint16_t position = (row * 80) + col;
+
+   // cursor LOW port to vga INDEX register
+   outb(0x3D4, 0x0F);
+   outb(0x3D5, (unsigned char)(position & 0xFF));
+   // cursor HIGH port to vga INDEX register
+   outb(0x3D4, 0x0E);
+   outb(0x3D5, (unsigned char)((position >> 8) & 0xFF));
+}
+
+
+static void term_incr_row()
+{
+   if (terminal_row < TERM_HEIGHT - 1) {
+
+      ++terminal_row;
+      return;
+   }
+
+   // We have to scroll...
+
+   memcpy(TERMINAL_VIDEO_ADDR,
+          TERMINAL_VIDEO_ADDR + 2 * TERM_WIDTH,
+          TERM_WIDTH * TERM_HEIGHT * 2);
+
+   volatile uint16_t *lastRow =
+      (volatile uint16_t *)TERMINAL_VIDEO_ADDR + TERM_WIDTH * (TERM_HEIGHT - 1);
+
+   for (int i = 0; i < TERM_WIDTH; i++) {
+      lastRow[i] = make_vgaentry(' ', terminal_color);
+   }
+
+}
+
+void term_write_char(char c) {
+
+   if (c == '\n') {
+      terminal_column = 0;
+      term_incr_row();
+      term_movecur(terminal_row, terminal_column);
+      return;
+   }
+
+   if (c == '\r') {
+      terminal_column = 0;
+      term_movecur(terminal_row, terminal_column);
+      return;
+   }
+
+   if (c == '\t') {
+      return;
+   }
+
+   volatile uint16_t *video = (volatile uint16_t *)TERMINAL_VIDEO_ADDR;
+
+   const size_t offset = terminal_row * TERM_WIDTH + terminal_column;
+   video[offset] = make_vgaentry(c, terminal_color);
+   ++terminal_column;
+
+   if (terminal_column == TERM_WIDTH) {
+      terminal_column = 0;
+      term_incr_row();
+   }
+
+   term_movecur(terminal_row, terminal_column);
+}
+
+void term_write_string(const char *str)
+{
+   while (*str) {
+      term_write_char(*str++);
+   }
+}
+
+void term_move_ch(int row, int col)
+{
+   terminal_row = row;
+   terminal_column = col;
+   term_movecur(row, col);
+}
+
+void term_init() {
+
+   term_movecur(0, 0);
+
+   term_setcolor(make_color(COLOR_WHITE, COLOR_BLACK));
+   volatile uint16_t *ptr = (volatile uint16_t *)TERMINAL_VIDEO_ADDR;
+
+   for (int i = 0; i < TERM_WIDTH*TERM_HEIGHT; ++i) {
+      *ptr++ = make_vgaentry(' ', terminal_color);
+   }
+}
 
 
 void main()
 {
-   jump_to_kernel();
+   term_init();
+   term_write_string("Hello from the 3rd stage of the bootloader!\n");
+
+   while(1);
+   //jump_to_kernel();
 }
