16661740fa997146dec54f003596e8552f943293
Vladislav Valtchev <vladi32@gmail.com>
2018-01-09 21:37:13 +0200

    [bootloader] Make the stage3's code even simpler

--- bootloader/stage3.c
+++ bootloader/stage3.c
@@ -4,23 +4,16 @@
 #include <term.h>
 #include <fs/fat32.h>
 
-
-/* ----- Declarations of asm functions ------ */
-
-NORETURN void jump_to_kernel(void);
-
-/* ------------------------------------------- */
-
-
-NORETURN void main(void)
+void main(void)
 {
+   /* Necessary in order to PANIC to be able to show something on the screen. */
    term_init();
-   printk("Hello from the 3rd stage of the bootloader!\n");
 
    fat_header *hdr = (fat_header *) RAM_DISK_PADDR;
    fat_entry *e = fat_search_entry(hdr, fat_unknown, "/EFI/BOOT/kernel.bin");
 
    fat_read_whole_file(hdr, e, (char*)KERNEL_PADDR, e->DIR_FileSize);
 
-   jump_to_kernel();
+   /* Jump to the kernel */
+   ((void (*)(void))KERNEL_PADDR)();
 }
