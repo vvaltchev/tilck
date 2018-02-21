e1ca00c5c2ca02ac46063d1332839cac5775edf8
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-02-21 19:26:54 +0200

    [cosmetic]

--- kernel/include/kmalloc.h
+++ kernel/include/kmalloc.h
@@ -5,12 +5,12 @@
 #include <string_util.h>
 
 #if !defined(TESTING) && !defined(KERNEL_TEST)
-#define HEAP_BASE_ADDR (KERNEL_BASE_VA + 0x4000000) // BASE + 64 MB
-#define HEAP_DATA_SIZE (64 * 1024 * 1024)
+#define HEAP_BASE_ADDR (KERNEL_BASE_VA + 64 * MB)
+#define HEAP_DATA_SIZE (64 * MB)
 #else
 extern void *kernel_heap_base;
 #define HEAP_BASE_ADDR ((uptr)kernel_heap_base)
-#define HEAP_DATA_SIZE (64 * 1024 * 1024)
+#define HEAP_DATA_SIZE (64 * MB)
 #endif
 
 #define MIN_BLOCK_SIZE (32)
