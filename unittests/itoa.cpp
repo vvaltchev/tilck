5cf03ff593fb5aab05104a0832191f907791e32f
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-04-13 12:21:12 +0300

    [gtests] Final polishing of the itoa gtest

--- unittests/itoa.cpp
+++ unittests/itoa.cpp
@@ -12,8 +12,18 @@ extern "C" {
    #include <common/string_util.h>
 }
 
+
+// Wrapper defined only for s32, s64, u32, u64.
 template <typename T>
-inline void itoa_wrapper(T val, char *buf) { abort(); }
+inline void itoa_wrapper(T val, char *buf);
+
+// Wrapper defined only for u32, u64.
+template <typename T>
+inline void uitoa_hex_wrapper(T val, char *buf, bool fixed);
+
+// Wrapper defined only for u32, u64.
+template <typename T>
+inline void sprintf_hex_wrapper(T val, char *buf, bool fixed);
 
 template <>
 inline void itoa_wrapper<s32>(s32 val, char *buf) { itoa32(val, buf); }
@@ -27,8 +37,6 @@ inline void itoa_wrapper<u32>(u32 val, char *buf) { uitoa32_dec(val, buf); }
 template <>
 inline void itoa_wrapper<u64>(u64 val, char *buf) { uitoa64_dec(val, buf); }
 
-template <typename T>
-inline void uitoa_hex_wrapper(T val, char *buf, bool fixed) { abort(); }
 
 template<>
 inline void uitoa_hex_wrapper<u32>(u32 val, char *buf, bool fixed)
@@ -47,10 +55,6 @@ inline void uitoa_hex_wrapper<u64>(u64 val, char *buf, bool fixed)
    else
       uitoa64_hex_fixed(val, buf);
 }
-
-
-template <typename T>
-inline void sprintf_hex_wrapper(T val, char *buf, bool fixed) { abort(); }
 
 template <>
 inline void sprintf_hex_wrapper<u32>(u32 val, char *buf, bool fixed)
@@ -72,7 +76,8 @@ inline void sprintf_hex_wrapper<u64>(u64 val, char *buf, bool fixed)
 
 
 template <typename T, bool hex = false, bool fixed = false>
-void check(T val)
+typename enable_if<hex, void>::type
+check(T val)
 {
    char expected[64];
    char got[64];
@@ -80,66 +85,57 @@ void check(T val)
    memset(expected, '*', sizeof(expected));
    memset(got, '*', sizeof(got));
 
-   if (!hex) {
-
-      strcpy(expected, to_string(val).c_str());
-      itoa_wrapper(val, got);
-
-   } else {
-
-      sprintf_hex_wrapper<T>(val, expected, fixed);
-      uitoa_hex_wrapper<T>(val, got, fixed);
-
-   }
+   sprintf_hex_wrapper<T>(val, expected, fixed);
+   uitoa_hex_wrapper<T>(val, got, fixed);
 
    ASSERT_STREQ(got, expected);
 }
 
 template <typename T, bool hex = false, bool fixed = false>
-void check_set()
+typename enable_if<!hex, void>::type
+check(T val)
+{
+   char expected[64];
+   char got[64];
+
+   memset(expected, '*', sizeof(expected));
+   memset(got, '*', sizeof(got));
+
+   strcpy(expected, to_string(val).c_str());
+   itoa_wrapper(val, got);
+
+   ASSERT_STREQ(got, expected);
+}
+
+
+template <typename T, bool hex = false, bool fixed = false>
+void check_basic_set()
 {
    auto check_func = check<T, hex, fixed>;
 
-   check_func(0);
-   check_func(numeric_limits<T>::min());
-   check_func(numeric_limits<T>::max());
-   check_func(numeric_limits<T>::min() + 1);
-   check_func(numeric_limits<T>::max() - 1);
+   ASSERT_NO_FATAL_FAILURE({
+      check_func(numeric_limits<T>::lowest());
+      check_func(numeric_limits<T>::min());
+      check_func(numeric_limits<T>::max());
+      check_func(numeric_limits<T>::min() + 1);
+      check_func(numeric_limits<T>::max() - 1);
+   });
 }
 
-TEST(itoa, u32_hex)
+template <typename T, bool hex = false, bool fixed = false>
+void generic_itoa_test_body()
 {
-   typedef u32 T;
-   auto check_func = check<T, true>;
-   auto check_set_func = check_set<T, true>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
    random_device rdev;
    const auto seed = rdev();
    default_random_engine e(seed);
 
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
+   auto check_func = check<T, hex, fixed>;
+   auto check_basic_set_func = check_basic_set<T, hex, fixed>;
 
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
-}
+   uniform_int_distribution<T> dist(numeric_limits<T>::min(),
+                                    numeric_limits<T>::max());
 
-TEST(itoa, u64_hex)
-{
-   typedef u64 T;
-   auto check_func = check<T, true>;
-   auto check_set_func = check_set<T, true>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
+   ASSERT_NO_FATAL_FAILURE({ check_basic_set_func(); });
 
    for (int i = 0; i < 100000; i++)
       ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
@@ -147,115 +143,48 @@ TEST(itoa, u64_hex)
 
 TEST(itoa, u32_dec)
 {
-   typedef u32 T;
-   auto check_func = check<T>;
-   auto check_set_func = check_set<T>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
+   auto test_func = generic_itoa_test_body<u32>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
 }
 
 TEST(itoa, u64_dec)
 {
-   typedef u64 T;
-   auto check_func = check<T>;
-   auto check_set_func = check_set<T>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
-}
-
-TEST(itoa, u32_hex_fixed)
-{
-   typedef u32 T;
-   auto check_func = check<T, true, true>;
-   auto check_set_func = check_set<T, true, true>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
-}
-
-TEST(itoa, u64_hex_fixed)
-{
-   typedef u64 T;
-   auto check_func = check<T, true, true>;
-   auto check_set_func = check_set<T, true, true>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
+   auto test_func = generic_itoa_test_body<u64>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
 }
 
 TEST(itoa, s32_dec)
 {
-   typedef s32 T;
-   auto check_func = check<T>;
-   auto check_set_func = check_set<T>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
+   auto test_func = generic_itoa_test_body<s32>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
 }
 
 TEST(itoa, s64_dec)
 {
-   typedef s32 T;
-   auto check_func = check<T>;
-   auto check_set_func = check_set<T>;
-
-   ASSERT_NO_FATAL_FAILURE({ check_set_func(); });
-
-   random_device rdev;
-   const auto seed = rdev();
-   default_random_engine e(seed);
-
-   uniform_int_distribution<T>
-      dist(numeric_limits<T>::min(), numeric_limits<T>::max());
-
-   for (int i = 0; i < 100000; i++)
-      ASSERT_NO_FATAL_FAILURE({ check_func(dist(e)); });
+   auto test_func = generic_itoa_test_body<s64>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
 }
 
+TEST(itoa, u32_hex)
+{
+   auto test_func = generic_itoa_test_body<u32, true>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
+}
+
+TEST(itoa, u64_hex)
+{
+   auto test_func = generic_itoa_test_body<u64, true>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
+}
+
+TEST(itoa, u32_hex_fixed)
+{
+   auto test_func = generic_itoa_test_body<u32, true, true>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
+}
+
+TEST(itoa, u64_hex_fixed)
+{
+   auto test_func = generic_itoa_test_body<u64, true, true>;
+   ASSERT_NO_FATAL_FAILURE({ test_func(); });
+}
