51d76b0afe6f38965bd39dd10a9b83babef2d16b
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-04-15 11:05:42 +0300

    [init] Removing unused code and other minor improvements

--- usermode_apps/init.c
+++ usermode_apps/init.c
@@ -1,6 +1,4 @@
 
-/* Usermode application */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -12,89 +10,45 @@
 #include <stdbool.h>
 #include <errno.h>
 
-int bss_variable[32];
-
-void bss_var_test(void)
+void call_exit(int code)
 {
-   for (int i = 0; i < 32; i++) {
-      if (bss_variable[i] != 0) {
-         printf("%s: FAIL\n", __FUNCTION__);
-         exit(1);
-      }
-   }
-
-   printf("%s: OK\n", __FUNCTION__);
+   printf("[init] exit with code: %i\n", code);
+   exit(code);
 }
 
-void args_test(int argc, char ** argv)
+void open_std_handles(void)
 {
-   printf("argc: %i\n", argc);
+   int in = open("/dev/tty", O_RDONLY);
+   int out = open("/dev/tty", O_WRONLY);
+   int err = open("/dev/tty", O_WRONLY);
 
-   for (int i = 0; i < argc; i++) {
-      printf("argv[%i] = '%s'\n", i, argv[i]);
+   if (in != 0) {
+      printf("[init] in: %i, expected: 0\n", in);
+      call_exit(1);
    }
 
-   printf("env[OSTYPE] = '%s'\n", getenv("OSTYPE"));
-
-   if (strcmp(argv[0], "init")) {
-      printf("%s: FAIL\n", __FUNCTION__);
-      exit(1);
+   if (out != 1) {
+      printf("[init] out: %i, expected: 1\n", out);
+      call_exit(1);
    }
 
-   if (strcmp(getenv("OSTYPE"), "linux-gnu")) {
-      printf("%s: FAIL\n", __FUNCTION__);
-      exit(1);
+   if (err != 2) {
+      printf("[init] err: %i, expected: 2\n", err);
+      call_exit(1);
    }
-}
-
-void file_read_test(void)
-{
-   char buf[256];
-   int fd;
-
-   if (getenv("EXOS")) {
-      fd = open("/EFI/BOOT/elf_kernel_stripped", O_RDONLY);
-   } else {
-      fd = open("build/sysroot/EFI/BOOT/elf_kernel_stripped", O_RDONLY);
-   }
-
-   printf("open() = %i\n", fd);
-
-   if (fd < 0) {
-      perror("Open failed");
-      exit(1);
-   }
-
-   int r = read(fd, buf, 256);
-   printf("user: read() returned %i\n", r);
-
-   for (int i = 0; i < 16; i++) {
-      printf("0x%02x ", (unsigned)buf[i]);
-   }
-
-   printf("\n");
-   close(fd);
 }
 
 int main(int argc, char **argv, char **env)
 {
-   if (getenv("EXOS")) {
-      int in_fd = open("/dev/tty", O_RDONLY);
-      int out_fd = open("/dev/tty", O_WRONLY);
-      int err_fd = open("/dev/tty", O_WRONLY);
-      //printf("in: %i, out: %i, err: %i\n", in_fd, out_fd, err_fd);
-      (void)in_fd; (void)out_fd; (void)err_fd;
-   }
+   int shell_pid;
+   int wstatus;
 
-   printf("Hello from init! MY PID IS %i\n", getpid());
+   if (getenv("EXOS"))
+      open_std_handles();
 
-   //args_test(argc, argv);
-   //file_read_test();
-   //test_read_stdin();
+   printf("[init] Hello from init! MY PID IS %i\n", getpid());
 
-   bss_var_test();
-
-   int shell_pid = fork();
+   shell_pid = fork();
 
    if (!shell_pid) {
       printf("[init forked child] running shell\n");
@@ -102,12 +56,9 @@ int main(int argc, char **argv, char **env)
    }
 
    printf("[init] wait for the shell to exit\n");
-   int wstatus;
+
    waitpid(shell_pid, &wstatus, 0);
    printf("[init] the shell exited with status: %d\n", WEXITSTATUS(wstatus));
-
-   //fork_test();
-
-   return 0;
+   call_exit(0);
 }
 
