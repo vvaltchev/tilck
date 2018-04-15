fd0f406fea93d14c53f056bfcae8f6a6a2952b26
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-04-15 15:36:38 +0300

    [gtests] Fix build error (missing mocks)

--- unittests/asm_mocks.c
+++ unittests/asm_mocks.c
@@ -1,6 +1,11 @@
 
+#include <common/basic_defs.h>
+
 // Defining some necessary symbols just to make the linker happy.
 
-void kernel_yield() { }
-void arch_specific_new_task_setup() { }
-void arch_specific_free_task() { }
+void *kernel_initial_stack = NULL;
+
+void kernel_yield() { NOT_REACHED(); }
+void arch_specific_new_task_setup() { NOT_REACHED(); }
+void arch_specific_free_task() { NOT_REACHED(); }
+void switch_to_initial_kernel_stack() { NOT_REACHED(); }
