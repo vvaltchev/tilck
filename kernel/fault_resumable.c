d5bffe26ebf1b5bc17bc6c54f6bfae2d5fba9ad2
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-04-19 20:54:39 +0300

    [fault_resumable] Optimizing fault_resumable_call [draft]

--- kernel/fault_resumable.c
+++ kernel/fault_resumable.c
@@ -19,65 +19,7 @@ void handle_resumable_fault(regs *r)
    context_switch(saved_fault_resumable_regs);
 }
 
-
-typedef struct {
-
-   void *func;
-   uptr args[6];
-
-} call_ctx;
-
-typedef void (*func0)(void);
-typedef void (*func1)(uptr);
-typedef void (*func2)(uptr, uptr);
-typedef void (*func3)(uptr, uptr, uptr);
-typedef void (*func4)(uptr, uptr, uptr, uptr);
-typedef void (*func5)(uptr, uptr, uptr, uptr, uptr);
-typedef void (*func6)(uptr, uptr, uptr, uptr, uptr, uptr);
-
-static void do_call0(call_ctx *ctx)
-{
-   ((func0)ctx->func)();
-}
-
-static void do_call1(call_ctx *ctx)
-{
-   ((func1)ctx->func)(ctx->args[0]);
-}
-
-static void do_call2(call_ctx *ctx)
-{
-   ((func2)ctx->func)(ctx->args[0], ctx->args[1]);
-}
-
-static void do_call3(call_ctx *ctx)
-{
-   ((func3)ctx->func)(ctx->args[0], ctx->args[1], ctx->args[2]);
-}
-
-static void do_call4(call_ctx *ctx)
-{
-   ((func4)ctx->func)(ctx->args[0], ctx->args[1], ctx->args[2], ctx->args[3]);
-}
-
-static void do_call5(call_ctx *ctx)
-{
-   ((func5)ctx->func)(ctx->args[0],
-                      ctx->args[1], ctx->args[2], ctx->args[3], ctx->args[4]);
-}
-
-static void do_call6(call_ctx *ctx)
-{
-   ((func6)ctx->func)(ctx->args[0], ctx->args[1],
-                      ctx->args[2], ctx->args[3], ctx->args[4], ctx->args[5]);
-}
-
-static void (*call_wrappers[7])(call_ctx *) =
-{
-   do_call0, do_call1, do_call2, do_call3, do_call4, do_call5, do_call6
-};
-
-int asm_do_fault_resumable_call(void (*func)(call_ctx *), call_ctx *ctx);
+int asm_do_fault_resumable_call(void *func, u32 *nargs_ref);
 
 int fault_resumable_call(u32 faults_mask, void *func, u32 nargs, ...)
 {
@@ -85,22 +27,11 @@ int fault_resumable_call(u32 faults_mask, void *func, u32 nargs, ...)
    ASSERT(is_preemption_enabled());
    ASSERT(nargs <= 6);
 
-   call_ctx ctx;
-   ctx.func = func;
-
-   va_list args;
-   va_start(args, nargs);
-
-   for (u32 i = 0; i < nargs; i++)
-      ctx.args[i] = va_arg(args, uptr);
-
-   va_end(args);
-
    disable_preemption();
    {
       __fault_resume_mask = faults_mask;
       saved_disable_preemption_count = disable_preemption_count;
-      r = asm_do_fault_resumable_call(call_wrappers[nargs], &ctx);
+      r = asm_do_fault_resumable_call(func, &nargs);
       __fault_resume_mask = 0;
    }
    enable_preemption();
