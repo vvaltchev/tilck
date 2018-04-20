1796f182f4f8789dbf55590904916275c958b57a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-04-20 12:53:27 +0300

    [asm_do_fault_resumable_call] Save fault res. regs and dis preempt. count

--- kernel/fault_resumable.c
+++ kernel/fault_resumable.c
@@ -5,7 +5,6 @@
 #include <exos/process.h>
 
 volatile u32 __fault_resume_mask;
-static u32 saved_disable_preemption_count;
 
 /* This cannot be static since asm_do_fault_resumable_call() writes to it */
 regs *saved_fault_resumable_regs;
@@ -14,7 +13,6 @@ void handle_resumable_fault(regs *r)
 {
    ASSERT(!are_interrupts_enabled());
    pop_nested_interrupt(); // the fault
-   disable_preemption_count = saved_disable_preemption_count;
    set_return_register(saved_fault_resumable_regs, 1 << regs_intnum(r));
    context_switch(saved_fault_resumable_regs);
 }
@@ -30,7 +28,6 @@ int fault_resumable_call(u32 faults_mask, void *func, u32 nargs, ...)
    disable_preemption();
    {
       __fault_resume_mask = faults_mask;
-      saved_disable_preemption_count = disable_preemption_count;
       r = asm_do_fault_resumable_call(func, &nargs);
       __fault_resume_mask = 0;
    }
