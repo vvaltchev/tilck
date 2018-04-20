e3d585e0941d562c743ed7cc767d89bcff700c7d
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-04-20 17:25:18 +0300

    [fault_resumable_call] Add the nested_faulting_code() test

--- kernel/self_tests/testrunners.c
+++ kernel/self_tests/testrunners.c
@@ -225,16 +225,21 @@ void selftest_panic(void)
 
 #ifdef __i386__
 
+static void faulting_code_div0(void)
+{
+   asmVolatile("mov $0, %edx\n\t"
+               "mov $1, %eax\n\t"
+               "mov $0, %ecx\n\t"
+               "div %ecx\n\t");
+}
+
 static void faulting_code(void)
 {
    printk("hello from div by 0 faulting code\n");
 
    disable_preemption();
 
-   asmVolatile("mov $0, %edx\n\t"
-               "mov $1, %eax\n\t"
-               "mov $0, %ecx\n\t"
-               "div %ecx\n\t");
+   faulting_code_div0();
 
    /*
     * Note: because the above asm will trigger a div by 0 fault, we'll never
@@ -252,10 +257,39 @@ static void faulting_code2(void)
    *null_ptr = NULL;
 }
 
+#define NESTED_FAULTING_CODE_MAX_LEVELS 4
+
+static void nested_faulting_code(int level)
+{
+   if (level == NESTED_FAULTING_CODE_MAX_LEVELS) {
+      printk("[level %i]: *** call faulting code ***\n", level);
+      faulting_code2();
+      NOT_REACHED();
+   }
+
+   printk("[level %i]: do recursive nested call\n", level);
+
+   int r = fault_resumable_call((u32)-1, nested_faulting_code, 1, level+1);
+
+   if (r) {
+      if (level == NESTED_FAULTING_CODE_MAX_LEVELS - 1) {
+         printk("[level %i]: the call faulted (r = %u). "
+                "Let's do another faulty call\n", level, r);
+         faulting_code_div0();
+         NOT_REACHED();
+      } else {
+         printk("[level %i]: the call faulted (r = %u)\n", level, r);
+      }
+   } else {
+      printk("[level %i]: the call was OK\n", level);
+   }
+
+   printk("[level %i]: we reached the end\n", level);
+}
+
 void selftest_fault_resumable(void)
 {
    int r;
-   enable_preemption();
 
    printk("fault_resumable with just printk()\n");
    r = fault_resumable_call((u32)-1,
@@ -272,7 +306,14 @@ void selftest_fault_resumable(void)
    printk("fault_resumable with code causing page fault\n");
    r = fault_resumable_call(1 << FAULT_PAGE_FAULT, faulting_code2, 0);
    printk("returned %i\n", r);
-   disable_preemption();
+
+   printk("[level 0]: do recursive nested call\n");
+   r = fault_resumable_call((u32)-1, // all faults
+                            nested_faulting_code,
+                            1,  // nargs
+                            1); // arg1: level
+   printk("[level 0]: call returned %i\n", r);
+
 }
 
 static NO_INLINE void do_nothing(uptr a1, uptr a2, uptr a3,
