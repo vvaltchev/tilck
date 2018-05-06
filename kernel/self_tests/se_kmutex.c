7400e0ef16f2be4e926b404272579efb7eb59472
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-05-06 20:44:02 +0300

    [selftests] Extract the kmutex test in a separate file

---
+++ kernel/self_tests/se_kmutex.c
@@ -0,0 +1,53 @@
+
+#include <common/basic_defs.h>
+#include <common/string_util.h>
+#include <common/utils.h>
+
+#include <exos/process.h>
+#include <exos/sync.h>
+
+void simple_test_kthread(void *arg);
+
+static kmutex test_mutex = { 0 };
+
+void test_kmutex_thread(void *arg)
+{
+   printk("%i) before lock\n", arg);
+
+   kmutex_lock(&test_mutex);
+
+   printk("%i) under lock..\n", arg);
+   for (int i=0; i < 256*MB; i++) { }
+
+   kmutex_unlock(&test_mutex);
+
+   printk("%i) after lock\n", arg);
+}
+
+void test_kmutex_thread_trylock()
+{
+   printk("3) before trylock\n");
+
+   bool locked = kmutex_trylock(&test_mutex);
+
+   if (locked) {
+
+      printk("3) trylock SUCCEEDED: under lock..\n");
+
+      kmutex_unlock(&test_mutex);
+
+      printk("3) after lock\n");
+
+   } else {
+      printk("3) trylock returned FALSE\n");
+   }
+}
+
+void selftest_kmutex()
+{
+   kmutex_init(&test_mutex);
+   kthread_create(&simple_test_kthread, (void*)0xAA1234BB);
+   kthread_create(test_kmutex_thread, (void *)1);
+   kthread_create(test_kmutex_thread, (void *)2);
+   kthread_create(test_kmutex_thread_trylock, NULL);
+}
--- kernel/self_tests/testrunners.c
+++ kernel/self_tests/testrunners.c
@@ -70,50 +70,6 @@ void selftest_kernel_sleep(void)
    kthread_create(sleeping_kthread, (void *)TIMER_HZ);
 }
 
-static kmutex test_mutex = { 0 };
-
-void test_kmutex_thread(void *arg)
-{
-   printk("%i) before lock\n", arg);
-
-   kmutex_lock(&test_mutex);
-
-   printk("%i) under lock..\n", arg);
-   for (int i=0; i < 256*MB; i++) { }
-
-   kmutex_unlock(&test_mutex);
-
-   printk("%i) after lock\n", arg);
-}
-
-void test_kmutex_thread_trylock()
-{
-   printk("3) before trylock\n");
-
-   bool locked = kmutex_trylock(&test_mutex);
-
-   if (locked) {
-
-      printk("3) trylock SUCCEEDED: under lock..\n");
-
-      kmutex_unlock(&test_mutex);
-
-      printk("3) after lock\n");
-
-   } else {
-      printk("3) trylock returned FALSE\n");
-   }
-}
-
-void selftest_kmutex()
-{
-   kmutex_init(&test_mutex);
-   kthread_create(&simple_test_kthread, (void*)0xAA1234BB);
-   kthread_create(test_kmutex_thread, (void *)1);
-   kthread_create(test_kmutex_thread, (void *)2);
-   kthread_create(test_kmutex_thread_trylock, NULL);
-}
-
 
 static kcond cond = { 0 };
 static kmutex cond_mutex = { 0 };
