59a213fe4f23c318f51a8386539da9ada2db15a3
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-05-06 20:40:40 +0300

    [selftests] Extract the tasklet test in a separate file

--- kernel/self_tests/testrunners.c
+++ kernel/self_tests/testrunners.c
@@ -14,46 +14,6 @@
 #include <exos/sync.h>
 #include <exos/fault_resumable.h>
 
-void test_tasklet_func()
-{
-   for (int i = 0; i < 10; i++) {
-      asmVolatile("nop");
-   }
-}
-
-void tasklet_test_thread(void *unused)
-{
-   const int tot_iters = MAX_TASKLETS * 10;
-   printk("[selftest_tasklet] BEGIN\n");
-
-   for (int i = 0; i < tot_iters; i++) {
-
-      ASSERT(is_preemption_enabled());
-
-      bool added;
-
-      do {
-         disable_preemption();
-         {
-            added = enqueue_tasklet0(&test_tasklet_func);
-         }
-         enable_preemption();
-      } while (!added);
-
-      if (!(i % 1000)) {
-         printk("[selftest_tasklet] %i%%\n", (i*100)/tot_iters);
-      }
-   }
-
-   printk("[selftest_tasklet] COMPLETED\n");
-   debug_qemu_turn_off_machine();
-}
-
-void selftest_tasklet(void)
-{
-   kthread_create(tasklet_test_thread, NULL);
-}
-
 void simple_test_kthread(void *arg)
 {
    int i;
