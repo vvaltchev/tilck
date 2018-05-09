c31ae57e38a96edc5327654da3a5390d50e947b2
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-05-09 17:12:31 +0300

    [ringbuf] Making some funcs non-inline by moving them into ringbuf.c

--- kernel/ringbuf.c
+++ kernel/ringbuf.c
@@ -4,12 +4,6 @@
 #include <exos/ringbuf.h>
 #include <exos/kmalloc.h>
 
-extern inline bool ringbuf_is_empty(ringbuf *rb);
-extern bool ringbuf_is_full(ringbuf *rb);
-extern inline bool ringbuf_write_elem1(ringbuf *rb, u8 val);
-extern inline bool ringbuf_read_elem1(ringbuf *rb, void *elem_ptr);
-extern inline bool ringbuf_unwrite_elem(ringbuf *rb);
-
 void ringbuf_init(ringbuf *rb, u16 max_elems, u16 elem_size, void *buf)
 {
    ASSERT(max_elems <= 32768);
@@ -65,6 +59,72 @@ bool ringbuf_read_elem(ringbuf *rb, void *elem_ptr /* out */)
       memcpy(elem_ptr, rb->buf + cs.read_pos * rb->elem_size, rb->elem_size);
 
       ns.read_pos = (ns.read_pos + 1) % rb->max_elems;
+      ns.full = false;
+
+   } while (!BOOL_COMPARE_AND_SWAP(&rb->s.raw, cs.raw, ns.raw));
+
+   return true;
+}
+
+bool ringbuf_write_elem1(ringbuf *rb, u8 val)
+{
+   generic_ringbuf_stat cs, ns;
+   ASSERT(rb->elem_size == 1);
+
+   do {
+
+      cs = rb->s;
+      ns = rb->s;
+
+      if (cs.full)
+         return false;
+
+      ns.write_pos = (ns.write_pos + 1) % rb->max_elems;
+
+      if (ns.write_pos == ns.read_pos)
+         ns.full = true;
+
+   } while (!BOOL_COMPARE_AND_SWAP(&rb->s.raw, cs.raw, ns.raw));
+
+   rb->buf[cs.write_pos] = val;
+   return true;
+}
+
+bool ringbuf_read_elem1(ringbuf *rb, void *elem_ptr)
+{
+   generic_ringbuf_stat cs, ns;
+
+   do {
+
+      cs = rb->s;
+      ns = rb->s;
+
+      if (ringbuf_is_empty(rb))
+         return false;
+
+      *(u8 *)elem_ptr = rb->buf[cs.read_pos];
+
+      ns.read_pos = (ns.read_pos + 1) % rb->max_elems;
+      ns.full = false;
+
+   } while (!BOOL_COMPARE_AND_SWAP(&rb->s.raw, cs.raw, ns.raw));
+
+   return true;
+}
+
+bool ringbuf_unwrite_elem(ringbuf *rb)
+{
+   generic_ringbuf_stat cs, ns;
+
+   do {
+
+      cs = rb->s;
+      ns = rb->s;
+
+      if (ringbuf_is_empty(rb))
+         return false;
+
+      ns.write_pos = (ns.write_pos - 1) % rb->max_elems;
       ns.full = false;
 
    } while (!BOOL_COMPARE_AND_SWAP(&rb->s.raw, cs.raw, ns.raw));
