8a01c362151dc54e01382b943ff6c256404c0727
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-05-11 20:19:43 +0300

    [efi bootloader] Using the graphics output protocol

--- bootloader/efi/efimain.c
+++ bootloader/efi/efimain.c
@@ -18,6 +18,10 @@
 #define BOOT_PADDR           0xC000
 #define KERNEL_FILE      L"\\EFI\\BOOT\\elf_kernel_stripped"
 
+#define DESIRED_RES_X 800
+#define DESIRED_RES_Y 600
+
+
 EFI_STATUS
 LoadFileFromDisk(EFI_BOOT_SERVICES *BS,
                  EFI_FILE_PROTOCOL *fileProt,
@@ -128,6 +132,121 @@ end:
    return status;
 }
 
+void print_mode_info(EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *mode)
+{
+   Print(L"Framebuffer addr: 0x%x\n", mode->FrameBufferBase);
+   Print(L"Framebuffer size: %u\n", mode->FrameBufferSize);
+   Print(L"Resolution: %u x %u\n",
+         mode->Info->HorizontalResolution,
+         mode->Info->VerticalResolution);
+
+   if (mode->Info->PixelFormat == PixelRedGreenBlueReserved8BitPerColor)
+      Print(L"PixelFormat: RGB + reserved\n");
+   else if (mode->Info->PixelFormat == PixelBlueGreenRedReserved8BitPerColor)
+      Print(L"PixelFormat: BGR + reserved\n");
+   else
+      Print(L"PixelFormat: other\n");
+}
+
+EFI_STATUS
+SetupGraphicMode(EFI_BOOT_SERVICES *BS)
+{
+   UINTN status = EFI_SUCCESS;
+
+   EFI_HANDLE handles[32];
+   UINTN handles_buf_size;
+   UINTN handles_count;
+   EFI_GRAPHICS_OUTPUT_PROTOCOL *graphicsProtocol;
+
+   handles_buf_size = sizeof(handles);
+
+   status = uefi_call_wrapper(BS->LocateHandle,
+                              5,
+                              ByProtocol,
+                              &GraphicsOutputProtocol,
+                              NULL,
+                              &handles_buf_size,
+                              handles);
+
+   HANDLE_EFI_ERROR("LocateHandle() failed");
+
+   handles_count = handles_buf_size/sizeof(EFI_HANDLE);
+
+   CHECK(handles_count > 0);
+
+   status = uefi_call_wrapper(BS->HandleProtocol,
+                              3,
+                              handles[0],
+                              &GraphicsOutputProtocol,
+                              &graphicsProtocol);
+
+   HANDLE_EFI_ERROR("HandleProtocol() failed");
+
+   EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *mode = graphicsProtocol->Mode;
+   print_mode_info(mode);
+
+   UINTN wanted_mode = (UINTN)-1;
+   UINTN orig_mode = mode->Mode;
+
+   for (UINTN i = 0; i < mode->MaxMode; i++) {
+
+      EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *mode_info = NULL;
+      UINTN sizeof_info = 0;
+
+      status = uefi_call_wrapper(graphicsProtocol->QueryMode,
+                                 4,
+                                 graphicsProtocol,
+                                 i,
+                                 &sizeof_info,
+                                 &mode_info);
+
+      HANDLE_EFI_ERROR("QueryMode() failed");
+
+      Print(L"Mode [%u]: %u x %u\n",
+            i,
+            mode_info->HorizontalResolution,
+            mode_info->VerticalResolution);
+
+      if (mode_info->HorizontalResolution == DESIRED_RES_X &&
+          mode_info->VerticalResolution == DESIRED_RES_Y) {
+
+         wanted_mode = i;
+      }
+   }
+
+   if (wanted_mode != (UINTN)-1) {
+
+      Print(L"About to switch to mode %u [%u x %u]. Press any key\n",
+            wanted_mode, DESIRED_RES_X, DESIRED_RES_Y);
+      WaitForKeyPress(ST);
+
+      status = uefi_call_wrapper(ST->ConOut->ClearScreen,
+                                 1,
+                                 ST->ConOut);
+
+      HANDLE_EFI_ERROR("ClearScreen() failed");
+
+      status = uefi_call_wrapper(graphicsProtocol->SetMode,
+                                 2,
+                                 graphicsProtocol,
+                                 wanted_mode);
+
+      if (EFI_ERROR(status) && status == EFI_NOT_STARTED) {
+         status = uefi_call_wrapper(graphicsProtocol->SetMode,
+                                    2,
+                                    graphicsProtocol,
+                                    orig_mode);
+
+         Print(L"Restored orig mode after error\n");
+      }
+
+      print_mode_info(mode);
+   }
+
+end:
+   return status;
+}
+
 /**
  * efi_main - The entry point for the EFI application
  * @image: firmware-allocated handle that identifies the image
@@ -144,6 +263,7 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *ST)
    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fileFsProt;
    EFI_FILE_PROTOCOL *fileProt;
    EFI_FILE_PROTOCOL *fileHandle;
+
    UINTN bufSize;
    UINTN crc32;
    UINTN ramdisk_paddr = RAMDISK_PADDR;
@@ -153,6 +273,9 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *ST)
    InitializeLib(image, ST);
 
    Print(L"--- exOS bootloader ---\r\n");
+
+   SetupGraphicMode(BS);
+   HANDLE_EFI_ERROR("SetupGraphicMode() failed");
 
    status = uefi_call_wrapper(BS->OpenProtocol,
                               6,                                  // #args
@@ -203,7 +326,7 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *ST)
                               RAMDISK_SIZE /* buffer size */,
                               ramdisk_paddr);
    HANDLE_EFI_ERROR("ReadDisk");
-   Print(L"Read OK\r\n");
+   //Print(L"Read OK\r\n");
    Print(L"RAMDISK paddr: 0x%lx\r\n", ramdisk_paddr);
 
    crc32 = 0;
