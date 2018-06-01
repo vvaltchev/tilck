aa8cd844a91c1ee64655e302ac4c86e48301c93c
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-06-01 22:14:38 +0300

    [build][cmake] Introduce the CMake function hex2dec()
    Unfortunately, CMake's MATH function does not understand hex numbers,
    while they are often pretty useful, especially in linker scripts.
    Therefore, adding a pure-CMake implementation of hex2dec().

--- gen_config.cmake
+++ gen_config.cmake
@@ -2,12 +2,14 @@
 set(EARLY_BOOT_SCRIPT ${PROJECT_BINARY_DIR}/bootloader/early_boot_script.ld)
 set(STAGE3_SCRIPT ${PROJECT_BINARY_DIR}/bootloader/elf_stage3_script.ld)
 
-set(BL_ST2_DATA_SEG 8192)    # 8192 = 0x2000 => linear address 0x20000
+set(BL_ST2_DATA_SEG 0x2000)
 set(STAGES_1_AND_2_SIZE 4096)
-set(STAGE3_ENTRY_OFFSET 4096)
+set(STAGE3_ENTRY_OFF 4096)
+
+hex2dec(${BL_ST2_DATA_SEG} BL_ST2_DATA_SEG_DEC)
 
 math(EXPR BL_BASE_ADDR
-   "${BL_ST2_DATA_SEG} * 16 + ${STAGES_1_AND_2_SIZE} + ${STAGE3_ENTRY_OFFSET}")
+   "${BL_ST2_DATA_SEG_DEC} * 16 + ${STAGES_1_AND_2_SIZE} + ${STAGE3_ENTRY_OFF}")
 
 configure_file(
    ${PROJECT_SOURCE_DIR}/include/common/generated_config_template.h
