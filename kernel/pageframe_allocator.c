2f0caa661f6ae243e315b65d137ecf79b30c5913
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-06-06 22:10:57 +0300

    Remove the hard-coded value for memsize_in_mb: the bootloader provides it

--- kernel/pageframe_allocator.c
+++ kernel/pageframe_allocator.c
@@ -22,11 +22,7 @@
 
 u32 pageframes_bitfield[8 * (MAX_MEM_SIZE_IN_MB - LINEAR_MAPPING_MB)];
 
-/*
- * HACK: just assume we have 128 MB as memory. In the future, this value has to
- * be somehow passed from the bootloader to the kernel.
- */
-u32 memsize_in_mb = 128;
+u32 memsize_in_mb;
 int pageframes_used;
 static u32 last_index;
 
