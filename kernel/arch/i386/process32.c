90e47cbf32b7004b0115f4008ee602afcbdd0a09
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-06-14 16:34:17 +0300

    [switch_to_task] Clear timer IRQ mask if !KERNEL_TRACK_NESTED_INTERRUPTS
    In commit 9a37d46 that statement has been removed from the function, but
    at that time KERNEL_TRACK_NESTED_INTERRUPTS was enabled by default and in
    that case, it was correct to remove that because the timer IRQ was never
    masked (allowing nested IRQ). After that, KERNEL_TRACK_NESTED_INTERRUPTS
    has been make a configurable define and that introduced the bug currectly
    being fixed, since both the scenarios are supported.

--- kernel/arch/i386/process32.c
+++ kernel/arch/i386/process32.c
@@ -296,6 +296,11 @@ NORETURN void switch_to_task(task_info *ti)
 
    DEBUG_VALIDATE_STACK_PTR();
 
+#if !KERNEL_TRACK_NESTED_INTERRUPTS
+   // We have to be SURE that the timer IRQ is NOT masked!
+   irq_clear_mask(X86_PC_TIMER_IRQ);
+#endif
+
    regs *state = ti->state_regs;
    ASSERT(state->eflags & EFLAGS_IF);
 
