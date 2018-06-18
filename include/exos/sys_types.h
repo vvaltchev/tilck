c50ffce15e94ff5e945be1bcfcafab62de7a3693
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-06-18 22:37:38 +0300

    [datetime] Use system's defines

--- include/exos/sys_types.h
+++ include/exos/sys_types.h
@@ -2,6 +2,7 @@
 #pragma once
 #include <common/basic_defs.h>
 
+#include <time.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/stat.h>
--- kernel/datetime.c
+++ kernel/datetime.c
@@ -125,33 +125,6 @@ sptr sys_gettimeofday(struct timeval *user_tv, struct timezone *user_tz)
    return 0;
 }
 
-/* Defines from /usr/include/bits/time.h */
-
-/* Identifier for system-wide realtime clock.  */
-# define CLOCK_REALTIME                 0
-/* Monotonic system-wide clock.  */
-# define CLOCK_MONOTONIC                1
-/* High-resolution timer from the CPU.  */
-# define CLOCK_PROCESS_CPUTIME_ID       2
-/* Thread-specific CPU-time clock.  */
-# define CLOCK_THREAD_CPUTIME_ID        3
-/* Monotonic system-wide clock, not adjusted for frequency scaling.  */
-# define CLOCK_MONOTONIC_RAW            4
-/* Identifier for system-wide realtime clock, updated only on ticks.  */
-# define CLOCK_REALTIME_COARSE          5
-/* Monotonic system-wide clock, updated only on ticks.  */
-# define CLOCK_MONOTONIC_COARSE         6
-/* Monotonic system-wide clock that includes time spent in suspension.  */
-# define CLOCK_BOOTTIME                 7
-/* Like CLOCK_REALTIME but also wakes suspended system.  */
-# define CLOCK_REALTIME_ALARM           8
-/* Like CLOCK_BOOTTIME but also wakes suspended system.  */
-# define CLOCK_BOOTTIME_ALARM           9
-/* Like CLOCK_REALTIME but in International Atomic Time.  */
-# define CLOCK_TAI                      11
-
-/* End of copy-pasted defines */
-
 sptr sys_clock_gettime(clockid_t clk_id, struct timespec *user_tp)
 {
    if (clk_id == CLOCK_REALTIME) {
