a0337f4e781b3fd219b09ba1ae4eba5f979417aa
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-06-20 18:44:59 +0300

    [fs] Improving sys_chdir() and sys_getcwd()

--- kernel/process.c
+++ kernel/process.c
@@ -171,20 +171,16 @@ sptr sys_chdir(const char *user_path)
    STATIC_ASSERT(ARRAY_SIZE(pi->cwd) == MAX_PATH);
    STATIC_ASSERT((ARGS_COPYBUF_SIZE / 2) >= MAX_PATH);
 
+   rc = copy_str_from_user(orig_path, user_path, MAX_PATH, NULL);
+
+   if (rc < 0)
+      return -EFAULT;
+
+   if (rc > 0)
+      return -ENAMETOOLONG;
+
    disable_preemption();
    {
-      rc = copy_str_from_user(orig_path, user_path, MAX_PATH, NULL);
-
-      if (rc < 0) {
-         rc = -EFAULT;
-         goto out;
-      }
-
-      if (rc > 0) {
-         rc = -ENAMETOOLONG;
-         goto out;
-      }
-
       rc = compute_abs_path(orig_path, pi->cwd, path, MAX_PATH);
       VERIFY(rc == 0); /* orig_path is at most MAX_PATH and cannot get longer */
 
@@ -192,17 +188,20 @@ sptr sys_chdir(const char *user_path)
       memcpy(pi->cwd, path, pl + 1);
 
       if (pl > 1) {
+
+         /* compute_abs_path always returns a path without a trailing '/' */
+         ASSERT(pi->cwd[pl - 1] != '/');
+
+         /* on the other side, pi->cwd has always a trailing '/' */
          pi->cwd[pl] = '/';
-         pi->cwd[pl+1] = 0;
+         pi->cwd[pl + 1] = 0;
       }
    }
-
-out:
    enable_preemption();
-   return rc;
+   return 0;
 }
 
-sptr sys_getcwd(char *buf, size_t buf_size)
+sptr sys_getcwd(char *user_buf, size_t buf_size)
 {
    sptr ret;
    size_t cwd_len;
@@ -210,7 +209,7 @@ sptr sys_getcwd(char *buf, size_t buf_size)
    {
       cwd_len = strlen(get_curr_task()->pi->cwd) + 1;
 
-      if (!buf || !buf_size) {
+      if (!user_buf || !buf_size) {
          ret = -EINVAL;
          goto out;
       }
@@ -220,11 +219,16 @@ sptr sys_getcwd(char *buf, size_t buf_size)
          goto out;
       }
 
-      ret = copy_to_user(buf, get_curr_task()->pi->cwd, cwd_len);
+      ret = copy_to_user(user_buf, get_curr_task()->pi->cwd, cwd_len);
 
       if (ret < 0) {
          ret = -EFAULT;
          goto out;
+      }
+
+      if (cwd_len > 2) { /* NOTE: cwd_len includes the trailing \0 */
+         ASSERT(user_buf[cwd_len - 2] == '/');
+         user_buf[cwd_len - 2] = 0; /* drop the trailing '/' */
       }
 
       ret = cwd_len;
