12ceaeebec1be60c07c7608b7d427141a5ab064e
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-07-12 15:38:01 +0300

    [devshell] Implement command line editing

--- usermode_apps/devshell_readcmd.c
+++ usermode_apps/devshell_readcmd.c
@@ -7,22 +7,27 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <termios.h>
+#include <assert.h>
 
 #define HIST_SIZE 16
 
-#define SEQ_UP    "\033[A\0\0\0\0\0"
-#define SEQ_DOWN  "\033[B\0\0\0\0\0"
-#define SEQ_RIGHT "\033[C\0\0\0\0\0"
-#define SEQ_LEFT  "\033[D\0\0\0\0\0"
-#define SEQ_BS    "\033[D \033[D\0"
+#define SEQ_UP      "\033[A\0\0\0\0\0"
+#define SEQ_DOWN    "\033[B\0\0\0\0\0"
+#define SEQ_RIGHT   "\033[C\0\0\0\0\0"
+#define SEQ_LEFT    "\033[D\0\0\0\0\0"
+#define SEQ_DELETE  "\033[\x7f\0\0\0\0\0"
+#define SEQ_HOME    "\033[H\0\0\0\0\0"
+#define SEQ_END     "\033[F\0\0\0\0\0"
 
 #define SN(s) (*(uint64_t*)(s))
 
-#define KEY_ERASE 0x7f
+#define WRITE_BS      "\033[D \033[D\0"
+#define KEY_BACKSPACE 0x7f
 
 char cmd_history[HIST_SIZE][256];
-unsigned hist_count = 0;
+unsigned hist_count;
 unsigned curr_hist_cmd_to_show;
+int curr_line_pos;
 
 void put_in_history(const char *cmdline)
 {
@@ -39,7 +44,14 @@ const char *get_prev_cmd(unsigned count)
 
 void raw_mode_erase_last(void)
 {
-   write(1, SEQ_BS, 7);
+   write(1, WRITE_BS, 7);
+}
+
+void erase_line_on_screen(int curr_cmd_len)
+{
+   for (; curr_cmd_len > 0; curr_cmd_len--) {
+      raw_mode_erase_last();
+   }
 }
 
 uint64_t read_esc_seq(void)
@@ -73,6 +85,7 @@ uint64_t read_esc_seq(void)
          return 0; /* no more space in our 64-bit int (seq too long) */
    }
 
+   //printf("\n[0x%x]\n", ret);
    return ret;
 }
 
@@ -86,6 +99,66 @@ void handle_esc_seq(char *buf,
 
    if (!seq)
       return;
+
+   if (seq == SN(SEQ_HOME)) {
+
+      for (int i = curr_line_pos - 1; i >= 0; i--)
+         write(1, SEQ_LEFT, 3);
+
+      curr_line_pos = 0;
+      return;
+   }
+
+   if (seq == SN(SEQ_END)) {
+
+      for (int i = curr_line_pos; i <= *curr_cmd_len - 1; i++)
+         write(1, SEQ_RIGHT, 3);
+
+      curr_line_pos = *curr_cmd_len;
+      return;
+   }
+
+   if (seq == SN(SEQ_DELETE)) {
+
+      if (!*curr_cmd_len || curr_line_pos == *curr_cmd_len)
+         return;
+
+      (*curr_cmd_len)--;
+
+      for (int i = curr_line_pos; i < *curr_cmd_len + 1; i++) {
+         buf[i] = buf[i+1];
+      }
+
+      buf[*curr_cmd_len] = ' ';
+      write(1, buf + curr_line_pos, *curr_cmd_len - curr_line_pos + 1);
+
+      for (int i = *curr_cmd_len; i >= curr_line_pos; i--)
+         write(1, SEQ_LEFT, 3);
+
+      return;
+   }
+
+   if (seq == SN(SEQ_LEFT) || seq == SN(SEQ_RIGHT)) {
+
+      if (seq == SN(SEQ_LEFT)) {
+
+         if (!curr_line_pos)
+            return;
+
+         write(1, SEQ_LEFT, 3);
+         curr_line_pos--;
+
+      } else {
+
+         if (curr_line_pos >= *curr_cmd_len)
+            return;
+
+         write(1, SEQ_RIGHT, 3);
+         curr_line_pos++;
+      }
+
+      return;
+   }
 
    if (seq == SN(SEQ_UP) || seq == SN(SEQ_DOWN)) {
 
@@ -118,14 +191,11 @@ void handle_esc_seq(char *buf,
          }
       }
 
-      while (*curr_cmd_len > 0) {
-         raw_mode_erase_last();
-         (*curr_cmd_len)--;
-      }
-
+      erase_line_on_screen(*curr_cmd_len);
       strncpy(buf, cmd, buf_size);
-      *curr_cmd_len = strlen(cmd);
+      *curr_cmd_len = strlen(buf);
       write(1, buf, *curr_cmd_len);
+      curr_line_pos = *curr_cmd_len;
    }
 }
 
@@ -145,8 +215,9 @@ int read_command(char *buf, int buf_size)
    tcsetattr(0, TCSAFLUSH, &t);
 
    curr_hist_cmd_to_show = 0;
+   curr_line_pos = 0;
 
-   while (ret < buf_size) {
+   while (ret < buf_size - 1) {
 
       rc = read(0, &c, 1);
 
@@ -158,11 +229,31 @@ int read_command(char *buf, int buf_size)
          goto out;
       }
 
-      if (c == KEY_ERASE) {
-         if (ret > 0) {
-            ret--;
-            raw_mode_erase_last();
+      //printf("\n[0x%x]\n", c);
+
+      if (c == KEY_BACKSPACE) {
+
+         if (!ret || !curr_line_pos)
+            continue;
+
+         ret--;
+         curr_line_pos--;
+         raw_mode_erase_last();
+
+         if (curr_line_pos == ret)
+            continue;
+
+         /* We have to shift left all the chars after curr_line_pos */
+         for (int i = curr_line_pos; i < ret + 1; i++) {
+            buf[i] = buf[i+1];
          }
+
+         buf[ret] = ' ';
+         write(1, buf + curr_line_pos, ret - curr_line_pos + 1);
+
+         for (int i = ret; i >= curr_line_pos; i--)
+            write(1, SEQ_LEFT, 3);
+
          continue;
       }
 
@@ -184,7 +275,27 @@ int read_command(char *buf, int buf_size)
          if (c == '\n')
             break;
 
-         buf[ret++] = c;
+         if (curr_line_pos == ret) {
+
+            buf[curr_line_pos++] = c;
+
+         } else {
+
+            /* We have to shift right all the chars after curr_line_pos */
+            for (int i = ret; i >= curr_line_pos; i--) {
+               buf[i + 1] = buf[i];
+            }
+
+            buf[curr_line_pos] = c;
+
+            write(1, buf + curr_line_pos + 1, ret - curr_line_pos);
+            curr_line_pos++;
+
+            for (int i = ret; i >= curr_line_pos; i--)
+               write(1, SEQ_LEFT, 3);
+         }
+
+         ret++;
          continue;
       }
 
