04707362bc8e625555e05c5f9ef81df7f7d88d44
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-07-14 14:49:56 +0300

    [x86] Introduce 'custom_flags' in struct regs.

--- include/exos/kernel/arch/i386/arch_utils.h
+++ include/exos/kernel/arch/i386/arch_utils.h
@@ -10,10 +10,10 @@
 
 typedef struct regs regs;
 
-/* This defines what the stack looks like after an ISR ran */
 struct regs {
    u32 kernel_resume_eip;
-   u32 gs, fs, es, ds;      /* pushed the segs last */
+   u32 custom_flags;        /* custom exOS flags */
+   u32 gs, fs, es, ds;
    u32 edi, esi, ebp, esp, ebx, edx, ecx, eax;  /* pushed by 'pusha' */
    u32 int_num, err_code;    /* our 'push byte #' and error codes do this */
    u32 eip, cs, eflags, useresp, ss;   /* pushed by the CPU automatically */
--- include/exos/kernel/arch/i386/asm_defs.h
+++ include/exos/kernel/arch/i386/asm_defs.h
@@ -4,15 +4,17 @@
 #define TI_F_RESUME_RS_OFF 92 /* offset of: fault_resume_regs */
 #define TI_FAULTS_MASK_OFF 96 /* offset of: faults_resume_mask */
 
-#define SIZEOF_REGS 80
-#define REGS_EIP_OFF 60
-#define REGS_USERESP_OFF 72
+#define SIZEOF_REGS 84
+#define REGS_EIP_OFF 64
+#define REGS_USERESP_OFF 76
+
+#define REGS_FL_SYSENTER     1
+#define REGS_FL_FPU_ENABLED  2
 
 #define X86_KERNEL_CODE_SEL 0x08
 #define X86_KERNEL_DATA_SEL 0x10
 #define X86_USER_CODE_SEL   0x1b
 #define X86_USER_DATA_SEL   0x23
-
 
 /* Some useful asm macros */
 #ifdef ASM_FILE
