3032a18088fa3648dff64eb823a1e6e2247a790b
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-08-24 00:18:43 +0300

    [system_mmap][gtests] Cosmetic improvements to the test

--- unittests/mem_regions.cpp
+++ unittests/mem_regions.cpp
@@ -24,6 +24,20 @@ extern "C" {
    void handle_overlapping_regions(void);
 }
 
+class mem_reg2 {
+
+   public:
+
+   u64 start;
+   u64 end;
+   u32 type;
+   u32 extra;
+
+   operator memory_region_t() {
+      return memory_region_t{start, end - start, type, extra};
+   }
+};
+
 void reset_mem_regions(void)
 {
    memset(mem_regions, 0, sizeof(mem_regions));
@@ -34,33 +48,10 @@ TEST(mem_regions, large_mem)
 {
    reset_mem_regions();
 
-   append_mem_region(memory_region_t{
-      0x0,
-      0x9dc00 - 0x0,
-      1,
-      0
-   });
-
-   append_mem_region(memory_region_t{
-      0x9dc00,
-      0xa0000 - 0x9dc00,
-      2,
-      0
-   });
-
-   append_mem_region(memory_region_t{
-      0xe0000,
-      0x100000 - 0xe0000,
-      2,
-      0
-   });
-
-   append_mem_region(memory_region_t{
-      0x100000000,
-      0x47e800000 - 0x100000000,
-      1,
-      0
-   });
+   append_mem_region(mem_reg2{0x00000, 0x9dc00, 1, 0});
+   append_mem_region(mem_reg2{0x9dc00, 0xa0000, 2, 0});
+   append_mem_region(mem_reg2{0xe0000, 0x100000, 2, 0});
+   append_mem_region(mem_reg2{0x100000000ull, 0x47e800000ull, 1, 0});
 
    dump_memory_map("Original memory map", mem_regions, mem_regions_count);
 
