4230f1c1ad29d84e35c6fc7c68f2e70e5e28a820
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-08-24 00:30:54 +0300

    [system_mmap] Fix a nasty bug (32 vs 64 bit)
    In align_mem_regions_to_page_boundary() the 64-bit start addr was AND'd
    with PAGE_MASK, but that truncated addr in case addr was >= 2^32.
    The right solution was to page mask created with 64-bit integers.

--- kernel/mm/system_mmap.c
+++ kernel/mm/system_mmap.c
@@ -113,7 +113,8 @@ STATIC void align_mem_regions_to_page_boundary(void)
        * we have to forcibly approximate the regions at page-boundaries.
        */
       const u64 ma_end = round_up_at64(ma->addr + ma->len, PAGE_SIZE);
-      ma->addr &= PAGE_MASK;
+      ma->addr &= ~((u64)PAGE_SIZE - 1); /* Don't use PAGE_MASK here: it causes
+                                            truncation on 32-bit systems */
       ma->len = ma_end - ma->addr;
    }
 }
