2be54d91a22f676324763cb0d36f169b5f1499d0
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-10-14 01:56:06 +0300

    [build] Introduce set_cross_compiler() and set_cross_compiler_glibc()

--- other/utils.cmake
+++ other/utils.cmake
@@ -15,3 +15,45 @@ function(PREPEND var prefix)
    endforeach(f)
    set(${var} "${listVar}" PARENT_SCOPE)
 endfunction(PREPEND)
+
+macro(set_cross_compiler)
+
+   if (${ARCH} STREQUAL "i386")
+
+      set(CMAKE_C_COMPILER ${GCC_TOOLCHAIN}/i686-linux-gcc)
+      set(CMAKE_CXX_COMPILER ${GCC_TOOLCHAIN}/i686-linux-g++)
+      set(CMAKE_ASM_COMPILER ${GCC_TOOLCHAIN}/i686-linux-gcc)
+      set(OBJCOPY ${GCC_TOOLCHAIN}/i686-linux-objcopy)
+      set(STRIP ${GCC_TOOLCHAIN}/i686-linux-strip)
+
+   else()
+
+      message(FATAL_ERROR "Architecture '${ARCH}' not supported.")
+
+   endif()
+
+endmacro()
+
+macro(set_cross_compiler_glibc)
+
+   if (${ARCH} STREQUAL "i386")
+
+      set(CMAKE_C_COMPILER ${GCC_TOOLCHAIN_GLIBC}/i686-linux-gcc)
+      set(CMAKE_CXX_COMPILER ${GCC_TOOLCHAIN_GLIBC}/i686-linux-g++)
+      set(CMAKE_ASM_COMPILER ${GCC_TOOLCHAIN_GLIBC}/i686-linux-gcc)
+      set(OBJCOPY ${GCC_TOOLCHAIN_GLIBC}/i686-linux-objcopy)
+      set(STRIP ${GCC_TOOLCHAIN_GLIBC}/i686-linux-strip)
+
+   else()
+
+      message(FATAL_ERROR "Architecture '${ARCH}' not supported.")
+
+   endif()
+
+endmacro()
+
+#message("CMAKE_ASM_COMPILE_OBJECT: ${CMAKE_ASM_COMPILE_OBJECT}")
+#message("CMAKE_C_LINK_EXECUTABLE: ${CMAKE_C_LINK_EXECUTABLE}")
+#message("CMAKE_C_COMPILE_OBJECT: ${CMAKE_C_COMPILE_OBJECT}")
+#message("CMAKE_C_LINK_FLAGS: ${CMAKE_C_LINK_FLAGS}")
+#message("CMAKE_CXX_COMPILE_OBJECT: ${CMAKE_CXX_COMPILE_OBJECT}")
