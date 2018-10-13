3c1c3de4d930d0c37465ba827cca94e221442fe6
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2018-10-14 01:13:13 +0300

    [build] Move two util functions in a separate cmake file

---
+++ other/utils.cmake
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+cmake_minimum_required(VERSION 3.2)
+
+set(SPACE " ")
+
+function(JOIN VALUES GLUE OUTPUT)
+  string (REPLACE ";" "${GLUE}" _TMP_STR "${VALUES}")
+  set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+endfunction()
+
+function(PREPEND var prefix)
+   set(listVar "")
+   foreach(f ${ARGN})
+      list(APPEND listVar "${prefix}${f}")
+   endforeach(f)
+   set(${var} "${listVar}" PARENT_SCOPE)
+endfunction(PREPEND)
