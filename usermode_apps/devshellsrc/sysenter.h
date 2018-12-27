95d1ceebd15aeb8385d0cfb74a3bfc3d27bb6d78
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2018-12-27 15:53:44 +0200

    [devshell] Add sysenter_call2() in sysenter.h

--- usermode_apps/devshellsrc/sysenter.h
+++ usermode_apps/devshellsrc/sysenter.h
@@ -41,6 +41,25 @@ do_sysenter_call1(int syscall, void *arg1)
 }
 
 static inline int
+do_sysenter_call2(int syscall, void *arg1, void *arg2)
+{
+   int ret;
+
+   __asm__ volatile ("pushl $1f\n\t"
+                     "pushl %%ecx\n\t"
+                     "pushl %%edx\n\t"
+                     "pushl %%ebp\n\t"
+                     "movl %%esp, %%ebp\n\t"
+                     "sysenter\n\t"
+                     "1:\n\t"
+                     : "=a" (ret)
+                     : "a" (syscall), "b" (arg1), "c" (arg2)
+                     : "memory", "cc");
+
+   return ret;
+}
+
+static inline int
 do_sysenter_call3(int syscall, void *arg1, void *arg2, void *arg3)
 {
    int ret;
@@ -64,6 +83,9 @@ do_sysenter_call3(int syscall, void *arg1, void *arg2, void *arg3)
 
 #define sysenter_call1(n, a1) \
    do_sysenter_call1((n), (void*)(a1))
+
+#define sysenter_call2(n, a1, a2) \
+   do_sysenter_call2((n), (void*)(a1), (void*)(a2))
 
 #define sysenter_call3(n, a1, a2, a3) \
    do_sysenter_call3((n), (void*)(a1), (void*)(a2), (void*)(a3))
