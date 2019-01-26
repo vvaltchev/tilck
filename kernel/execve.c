88aabe1328c61febe99037f08deb6c85f02e63bf
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-01-26 19:18:31 +0200

    [elf] Fix a subtle error-case bug in load_elf_program()
    In the code allocating the user stack pages the `rc` of the variable was
    shadowed by a local `rc` variable. As a result of that, in the case the
    loop ended because the kmalloc() or map_page() failed, the outer `rc`
    would remain untouched. Of course that lead the user program to crash just
    a little later.

--- kernel/execve.c
+++ kernel/execve.c
@@ -119,7 +119,7 @@ sptr sys_execve(const char *user_filename,
 
    rc = load_elf_program(abs_path, &pdir, &entry_point, &stack_addr, &brk);
 
-   if (rc < 0)
+   if (rc != 0)
       goto errend;
 
    char *const default_argv[] = { abs_path, NULL };
