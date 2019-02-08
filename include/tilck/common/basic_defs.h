246808d5dc1ffa32ba7f185e82d6f2da6df7283b
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-02-08 16:22:08 +0200

    [common] Remove the #ifdef _MSC_VER case from basic_defs.h

--- include/tilck/common/basic_defs.h
+++ include/tilck/common/basic_defs.h
@@ -58,62 +58,30 @@
 
 #endif
 
-#ifdef _MSC_VER
-
-   #define inline __inline
-   #define __i386__
-
-   // Make the Microsoft Intellisense happy: this does not have to work
-   // since we use GCC. Just, we'd like to avoid to confuse intellisense.
-   #define asmVolatile(...)
-   #define __attribute__(...)
-
-   #define asm(...)
-
-   #define ALWAYS_INLINE inline
-   #define NO_INLINE
-   #define typeof(x) void *
-
-   #define STATIC_ASSERT(s, err)
-
-   #define PURE
-   #define CONSTEXPR
-   #define NORETURN
-   #define WEAK
-   #define NODISCARD
-   #define OFFSET_OF(st, m)
-   #define FASTCALL
-   #define ASSUME_WITHOUT_CHECK(x)
-   #define ALIGNED_AT(x)
-
+#ifndef TESTING
+   #define NORETURN _Noreturn /* C11 standard no return attribute. */
 #else
+   #define NORETURN
+#endif
 
-   #ifndef TESTING
-      #define NORETURN _Noreturn /* C11 standard no return attribute. */
-   #else
-      #define NORETURN
-   #endif
+#define OFFSET_OF(st, m) __builtin_offsetof(st, m)
+#define ALWAYS_INLINE __attribute__((always_inline)) inline
+#define NO_INLINE __attribute__((noinline))
+#define asmVolatile __asm__ volatile
+#define asm __asm__
+#define typeof(x) __typeof__(x)
+#define PURE __attribute__((pure))
+#define CONSTEXPR __attribute__((const))
+#define WEAK __attribute__((weak))
+#define PACKED __attribute__((packed))
+#define NODISCARD __attribute__((warn_unused_result))
+#define ASSUME_WITHOUT_CHECK(x) if (!(x)) __builtin_unreachable();
+#define ALIGNED_AT(x) __attribute__ ((aligned(x)))
 
-   #define OFFSET_OF(st, m) __builtin_offsetof(st, m)
-   #define ALWAYS_INLINE __attribute__((always_inline)) inline
-   #define NO_INLINE __attribute__((noinline))
-   #define asmVolatile __asm__ volatile
-   #define asm __asm__
-   #define typeof(x) __typeof__(x)
-   #define PURE __attribute__((pure))
-   #define CONSTEXPR __attribute__((const))
-   #define WEAK __attribute__((weak))
-   #define PACKED __attribute__((packed))
-   #define NODISCARD __attribute__((warn_unused_result))
-   #define ASSUME_WITHOUT_CHECK(x) if (!(x)) __builtin_unreachable();
-   #define ALIGNED_AT(x) __attribute__ ((aligned(x)))
-
-   #ifdef BITS32
-      #define FASTCALL __attribute__((fastcall))
-   #else
-      #define FASTCALL
-   #endif
-
+#ifdef BITS32
+   #define FASTCALL __attribute__((fastcall))
+#else
+   #define FASTCALL
 #endif
 
 typedef int8_t s8;
