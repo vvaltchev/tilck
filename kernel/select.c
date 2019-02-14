8d07db4ae56403af45ccb5c2760ab504d6356ea2
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-02-15 01:49:36 +0200

    [select] Refactoring sys_select() [part 1/6]

--- kernel/select.c
+++ kernel/select.c
@@ -126,33 +126,89 @@ select_set_ready(u32 nfds, fd_set *set, func_rwe_ready is_ready)
    return tot;
 }
 
-sptr sys_select(int nfds, fd_set *user_rfds, fd_set *user_wfds,
+static const func_get_rwe_cond gcf[3] = {
+   &vfs_get_rready_cond,
+   &vfs_get_wready_cond,
+   &vfs_get_except_cond
+};
+
+static const func_rwe_ready grf[3] = {
+   &vfs_read_ready,
+   &vfs_write_ready,
+   &vfs_except_ready
+};
+
+static int
+select_wait_on_cond(u32 nfds,
+                    fd_set *sets[3],
+                    struct timeval *tv,
+                    u32 cond_cnt,
+                    u32 timeout_ticks)
+{
+   task_info *curr = get_curr_task();
+   multi_obj_waiter *waiter = NULL;
+   u32 idx = 0;
+   int rc = 0;
+
+   /*
+    * NOTE: it is not that difficult cond_cnt to be 0: it's enough the
+    * specified files to NOT have r/w/e get kcond functions. Also, all the
+    * sets might be NULL (see the comment below).
+    */
+
+   if (!(waiter = allocate_mobj_waiter(cond_cnt)))
+      return -ENOMEM;
+
+   for (int i = 0; i < 3; i++) {
+      if ((rc = select_set_kcond((u32)nfds, waiter, &idx, sets[i], gcf[i])))
+         goto out;
+   }
+
+   if (tv) {
+      ASSERT(timeout_ticks > 0);
+      task_set_wakeup_timer(get_curr_task(), (u32)timeout_ticks);
+   }
+
+   kernel_sleep_on_waiter(waiter);
+
+   if (tv) {
+
+      if (curr->wobj.type) {
+
+         /* we woke-up because of the timeout */
+         wait_obj_reset(&curr->wobj);
+         tv->tv_sec = 0;
+         tv->tv_usec = 0;
+
+      } else {
+
+         /* we woke-up because of a kcond was signaled */
+         u32 rem = task_cancel_wakeup_timer(curr);
+         tv->tv_sec = rem / TIMER_HZ;
+         tv->tv_usec = (rem % TIMER_HZ) * (1000000 / TIMER_HZ);
+      }
+   }
+
+out:
+   free_mobj_waiter(waiter);
+   return rc;
+}
+
+sptr sys_select(int user_nfds, fd_set *user_rfds, fd_set *user_wfds,
                 fd_set *user_efds, struct timeval *user_tv)
 {
-   static const func_get_rwe_cond gcf[3] = {
-      &vfs_get_rready_cond,
-      &vfs_get_wready_cond,
-      &vfs_get_except_cond
-   };
-
-   static const func_rwe_ready grf[3] = {
-      &vfs_read_ready,
-      &vfs_write_ready,
-      &vfs_except_ready
-   };
-
    fd_set *u_sets[3] = { user_rfds, user_wfds, user_efds };
 
    task_info *curr = get_curr_task();
-   multi_obj_waiter *waiter = NULL;
    int total_ready_count = 0;
    struct timeval *tv = NULL;
+   u32 nfds = (u32)user_nfds;
    fd_set *sets[3] = {0};
    u32 cond_cnt = 0;
-   u64 timeout_ticks = 0;
+   u32 timeout_ticks = 0;
    int rc;
 
-   if (nfds < 0 || nfds > MAX_HANDLES)
+   if (user_nfds < 0 || user_nfds > MAX_HANDLES)
       return -EINVAL;
 
    for (int i = 0; i < 3; i++) {
@@ -173,69 +229,27 @@ sptr sys_select(int nfds, fd_set *user_rfds, fd_set *user_wfds,
       if (copy_from_user(tv, user_tv, sizeof(struct timeval)))
          return -EFAULT;
 
-      timeout_ticks += (u64)tv->tv_sec * TIMER_HZ;
-      timeout_ticks += (u64)tv->tv_usec / (1000000ull / TIMER_HZ);
+      u64 tmp = 0;
+      tmp += (u64)tv->tv_sec * TIMER_HZ;
+      tmp += (u64)tv->tv_usec / (1000000ull / TIMER_HZ);
 
       /* NOTE: select() can't sleep for more than UINT32_MAX ticks */
-      timeout_ticks = MIN(timeout_ticks, UINT32_MAX);
+      timeout_ticks = (u32) MIN(tmp, UINT32_MAX);
    }
 
    //debug_dump_select_args(nfds, sets[0], sets[1], sets[2], tv);
 
    if (!tv || timeout_ticks > 0) {
       for (int i = 0; i < 3; i++) {
-         if ((rc = select_count_kcond((u32)nfds, sets[i], &cond_cnt, gcf[i])))
+         if ((rc = select_count_kcond(nfds, sets[i], &cond_cnt, gcf[i])))
             return rc;
       }
    }
 
    if (cond_cnt > 0) {
 
-      u32 idx = 0;
-
-      /*
-       * NOTE: it is not that difficult cond_cnt to be 0: it's enough the
-       * specified files to NOT have r/w/e get kcond functions.
-       */
-
-      if (!(waiter = allocate_mobj_waiter(cond_cnt)))
-         return -ENOMEM;
-
-      for (int i = 0; i < 3; i++) {
-         if ((rc = select_set_kcond((u32)nfds, waiter, &idx, sets[i], gcf[i])))
-         {
-            free_mobj_waiter(waiter);
-            return rc;
-         }
-      }
-
-      if (tv) {
-         ASSERT(timeout_ticks > 0);
-         task_set_wakeup_timer(get_curr_task(), (u32)timeout_ticks);
-      }
-
-      kernel_sleep_on_waiter(waiter);
-
-      if (tv) {
-
-         if (curr->wobj.type) {
-
-            /* we woke-up because of the timeout */
-            wait_obj_reset(&curr->wobj);
-            tv->tv_sec = 0;
-            tv->tv_usec = 0;
-
-         } else {
-
-            /* we woke-up because of a kcond was signaled */
-            u32 rem = task_cancel_wakeup_timer(curr);
-            tv->tv_sec = rem / TIMER_HZ;
-            tv->tv_usec = (rem % TIMER_HZ) * (1000000 / TIMER_HZ);
-         }
-      }
-
-      /* OK, we woke-up: it does not matter which kcond was signaled */
-      free_mobj_waiter(waiter);
+      if ((rc = select_wait_on_cond(nfds, sets, tv, cond_cnt, timeout_ticks)))
+         return rc;
 
    } else {
 
