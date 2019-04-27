832c9fef6e1333f57e460483a32824c605b99e5a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-04-28 00:11:25 +0300

    [vfs] Cosmetic improvements + comments

--- kernel/fs/vfs/vfs_stat.c.h
+++ kernel/fs/vfs/vfs_stat.c.h
@@ -66,21 +66,44 @@ static int vfs_getdents_cb(vfs_dent64 *vde, void *arg)
    vfs_getdents_ctx *ctx = arg;
 
    if (ctx->curr_index < ctx->h->pos) {
+
+      /*
+       * NOTE: this way of resuming the previous position kinda works, but it's
+       * pretty inefficient, in particular if the directory contains many
+       * entries. Also: what if a file is created between two getdents() calls?
+       * In ramfs, the only non-readonly FS for the moment, the entries are
+       * returned in lexicographical order. What if the new file gets a position
+       * ahead of the current pos? An entry already returned might be returned
+       * twice.
+       *
+       * TODO: think about a better way to implement getdents().
+       * An idea might be to keep the entries also in a linked list and store
+       * in `pos` a "weak" pointer to the next node to read. In case between
+       * getdents() calls the next entry gets unlinked, the "weak" pointer has
+       * to be invalided or just moved forward. The linked list approach
+       * guarantees that an entry will be returned just once because:
+       *
+       *    - new entries are always appended to the tail
+       *    - removing already returned entries is fine
+       *    - removing the next entry can be handled specially as mentioned.
+       */
+
       ctx->curr_index++;
       return 0; // continue
    }
 
    const u16 fl = (u16)strlen(vde->name);
    const u16 entry_size = sizeof(struct linux_dirent64) + fl + 1;
+   struct linux_dirent64 *user_ent;
 
    if (ctx->offset + entry_size > ctx->buf_size) {
 
       if (!ctx->offset) {
 
          /*
-         * We haven't "returned" any entries yet and the buffer is too small
-         * for our first entry.
-         */
+          * We haven't "returned" any entries yet and the buffer is too small
+          * for our first entry.
+          */
 
          return -EINVAL;
       }
@@ -89,12 +112,12 @@ static int vfs_getdents_cb(vfs_dent64 *vde, void *arg)
       return (int) ctx->offset;
    }
 
-   ctx->ent.d_ino = vde->ino;
-   ctx->ent.d_off = ctx->offset + entry_size;
+   ctx->ent.d_ino    = vde->ino;
+   ctx->ent.d_off    = ctx->offset + entry_size;
    ctx->ent.d_reclen = entry_size;
-   ctx->ent.d_type = vfs_type_to_linux_dirent_type(vde->type);
+   ctx->ent.d_type   = vfs_type_to_linux_dirent_type(vde->type);
 
-   struct linux_dirent64 *user_ent = (void *)((char *)ctx->dirp + ctx->offset);
+   user_ent = (void *)((char *)ctx->dirp + ctx->offset);
 
    if (copy_to_user(user_ent, &ctx->ent, sizeof(ctx->ent)) < 0)
       return -EFAULT;
@@ -118,12 +141,12 @@ int vfs_getdents64(fs_handle h, struct linux_dirent64 *user_dirp, u32 buf_size)
    ASSERT(hb->fs->fsops->getdents);
 
    vfs_getdents_ctx ctx = {
-      .h = hb,
-      .dirp = user_dirp,
-      .buf_size = buf_size,
-      .offset = 0,
-      .curr_index = 0,
-      .ent = { 0 },
+      .h             = hb,
+      .dirp          = user_dirp,
+      .buf_size      = buf_size,
+      .offset        = 0,
+      .curr_index    = 0,
+      .ent           = { 0 },
    };
 
    /* See the comment in vfs.h about the "fs-locks" */
