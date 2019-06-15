a7c47211f278bde231f54d9a5844d966b850a06a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-06-16 00:17:48 +0300

    [bintree] Extract bintree_remove_internal_aux() from the remove func

--- kernel/avl_bintree.c
+++ kernel/avl_bintree.c
@@ -214,37 +214,12 @@ bintree_find_internal(void *root_obj,
    return NULL;
 }
 
-void *
-bintree_remove_internal(void **root_obj_ref,
-                        void *value_ptr,
-                        cmpfun_ptr objval_cmpfun, // cmp(root_obj, value_ptr)
-                        ptrdiff_t bintree_offset)
+static void
+bintree_remove_internal_aux(void **root_obj_ref,
+                            void ***stack,
+                            int stack_size,
+                            ptrdiff_t bintree_offset)
 {
-   void **stack[MAX_TREE_HEIGHT] = {0};
-   int stack_size = 0;
-
-   ASSERT(root_obj_ref != NULL);
-   STACK_PUSH(root_obj_ref);
-
-   while (true) {
-
-      root_obj_ref = STACK_TOP();
-
-      if (!*root_obj_ref)
-         return NULL; // we did not find the object.
-
-      sptr c = objval_cmpfun(*root_obj_ref, value_ptr);
-
-      if (!c)
-         break;
-
-      // *root_obj_ref is smaller then val => val is bigger => go right.
-      STACK_PUSH(c < 0 ? &RIGHT_OF(*root_obj_ref) : &LEFT_OF(*root_obj_ref));
-   }
-
-
-   void *deleted_obj = *root_obj_ref;
-
    if (LEFT_OF(*root_obj_ref) && RIGHT_OF(*root_obj_ref)) {
 
       // not-leaf node
@@ -294,7 +269,38 @@ bintree_remove_internal(void **root_obj_ref,
 
    while (stack_size > 0)
       BALANCE(STACK_POP());
+}
 
+void *
+bintree_remove_internal(void **root_obj_ref,
+                        void *value_ptr,
+                        cmpfun_ptr objval_cmpfun, // cmp(root_obj, value_ptr)
+                        ptrdiff_t bintree_offset)
+{
+   void **stack[MAX_TREE_HEIGHT] = {0};
+   int stack_size = 0;
+
+   ASSERT(root_obj_ref != NULL);
+   STACK_PUSH(root_obj_ref);
+
+   while (true) {
+
+      root_obj_ref = STACK_TOP();
+
+      if (!*root_obj_ref)
+         return NULL; // we did not find the object.
+
+      sptr c = objval_cmpfun(*root_obj_ref, value_ptr);
+
+      if (!c)
+         break;
+
+      // *root_obj_ref is smaller then val => val is bigger => go right.
+      STACK_PUSH(c < 0 ? &RIGHT_OF(*root_obj_ref) : &LEFT_OF(*root_obj_ref));
+   }
+
+   void *deleted_obj = *root_obj_ref;
+   bintree_remove_internal_aux(root_obj_ref, stack, stack_size, bintree_offset);
    return deleted_obj;
 }
 
