398b3e926508fa7e8497be0fbd827c561c986f2a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-06-16 15:45:32 +0300

    [norec] Introduce SIMULATE_YIELD() and other minor features

--- include/tilck/common/norec.h
+++ include/tilck/common/norec.h
@@ -1,6 +1,5 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 
-#pragma once
 #include <tilck/common/basic_defs.h>
 
 #ifndef STACK_VAR
@@ -11,8 +10,12 @@
    #define STACK_SIZE_VAR stack_size
 #endif
 
-struct explicit_stack_elem1 { void *ret_addr, *arg1; };
-struct explicit_stack_elem2 { void *ret_addr, *arg1, *arg2; };
+#ifndef NO_REC_INCLUDED
+   /* Make sure we define this structures only once */
+   struct explicit_stack_elem1 { void *ret_addr, *arg1; };
+   struct explicit_stack_elem2 { void *ret_addr, *arg1, *arg2; };
+   #define NO_REC_INCLUDED
+#endif
 
 #define NOREC_LOOP_BEGIN   while (STACK_SIZE_VAR) {
 #define NOREC_LOOP_END     loop_end:; }
@@ -20,16 +23,30 @@ struct explicit_stack_elem2 { void *ret_addr, *arg1, *arg2; };
 #define LOAD_ARG_FROM_STACK(n, t)                                      \
    ((t)(uptr)STACK_VAR[STACK_SIZE_VAR-1].arg##n)
 
-#define CREATE_SHADOW_STACK(size, nargs)                               \
+#define DECLARE_SHADOW_STACK(size, nargs)                              \
    struct explicit_stack_elem##nargs STACK_VAR[size];                  \
-   int STACK_SIZE_VAR = 0;
+   int STACK_SIZE_VAR;
 
+#define INIT_SHADOW_STACK()                                            \
+   STACK_SIZE_VAR = 0;
+
+#define CREATE_SHADOW_STACK(size, nargs)                               \
+   DECLARE_SHADOW_STACK(size, nargs)                                   \
+   INIT_SHADOW_STACK()
 
 #define SIMULATE_RETURN_NULL()                                         \
    {                                                                   \
       STACK_SIZE_VAR--;                                                \
       ASSERT(STACK_VAR[STACK_SIZE_VAR].ret_addr || !STACK_SIZE_VAR);   \
       continue;                                                        \
+   }
+
+#define SIMULATE_YIELD(val)                                            \
+   {                                                                   \
+      STACK_VAR[STACK_SIZE_VAR].ret_addr =                             \
+         &&CONCAT(after_, __LINE__);                                   \
+      return val;                                                      \
+      CONCAT(after_, __LINE__):;                                       \
    }
 
 #define HANDLE_SIMULATED_RETURN()                                      \
