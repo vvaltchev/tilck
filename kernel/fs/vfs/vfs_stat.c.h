e77dae5785b76bb2a727f8b7fb19eb13199ddb61
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2019-06-30 19:53:12 +0300

    [vfs] Use vfs_resolve_new() in vfs_stat()

--- kernel/fs/vfs/vfs_stat.c.h
+++ kernel/fs/vfs/vfs_stat.c.h
@@ -20,29 +20,11 @@ int vfs_fstat64(fs_handle h, struct stat64 *statbuf)
 
 int vfs_stat64(const char *path, struct stat64 *statbuf, bool res_last_sl)
 {
-   const char *fs_path;
-   filesystem *fs;
-   vfs_path p;
-   int rc;
+   VFS_FS_PATH_FUNCS_COMMON_HEADER(path, false, res_last_sl)
 
-   NO_TEST_ASSERT(is_preemption_enabled());
-   ASSERT(path != NULL);
-   ASSERT(*path == '/'); /* VFS works only with absolute paths */
+   rc = p.fs_path.inode
+      ? fs->fsops->stat(fs, p.fs_path.inode, statbuf)
+      : -ENOENT;
 
-   if (!(fs = get_retained_fs_at(path, &fs_path)))
-      return -ENOENT;
-
-   /* See the comment in vfs.h about the "fs-lock" funcs */
-   vfs_fs_exlock(fs);
-   {
-      rc = vfs_resolve(fs, fs_path, &p, res_last_sl);
-
-      if (!rc)
-         rc = p.fs_path.inode
-            ? fs->fsops->stat(fs, p.fs_path.inode, statbuf)
-            : -ENOENT;
-   }
-   vfs_fs_exunlock(fs);
-   release_obj(fs);     /* it was retained by get_retained_fs_at() */
-   return rc;
+   VFS_FS_PATH_FUNCS_COMMON_FOOTER(path, false, res_last_sl)
 }
