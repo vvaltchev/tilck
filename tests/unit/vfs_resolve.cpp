66e8fb1955595cbe4e5c392e18cbbafb60f5561a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-07-29 14:18:54 +0300

    [gtests] Intr. the vfs_test_fs.{h,cpp} files

--- tests/unit/vfs_resolve.cpp
+++ tests/unit/vfs_resolve.cpp
@@ -2,35 +2,7 @@
 
 #include <iostream>
 #include <map>
-#include "vfs_test.h"
-using namespace std;
-
-struct tfs_entry {
-
-   const char *name;
-   vfs_entry_type type;
-   const char *symlink;
-   tfs_entry *parent;
-   int ref_count;
-   map<string, tfs_entry *> children;
-
-   tfs_entry *set_parents() {
-
-      for (auto &p : children) {
-         p.second->parent = this;
-         p.second->set_parents();
-      }
-
-      return this;
-   }
-};
-
-
-#define _NODE(n, t, s, ...) new tfs_entry{n, t, s, 0, 0, {__VA_ARGS__}}
-#define N_FILE(name) make_pair(name, _NODE(name, VFS_FILE, 0))
-#define N_SYM(name, s) make_pair(name, _NODE(name, VFS_FILE, s))
-#define N_DIR(name, ...) make_pair(name, _NODE(name, VFS_DIR, 0, __VA_ARGS__))
-#define ROOT_NODE(...) (_NODE("", VFS_DIR, 0, __VA_ARGS__))->set_parents()
+#include "vfs_test_fs.h"
 
 static tfs_entry *fs1 =
    ROOT_NODE(
@@ -47,7 +19,9 @@ static tfs_entry *fs1 =
             N_DIR(".hdir"),
          )
       ),
-      N_DIR("dev")
+      N_DIR("dev"),
+      N_SYM("abs_s1", "/a/b/c/f1"),
+      N_SYM("rel_s1", "a/b/c/f1"),
    );
 
 static tfs_entry *fs2 =
@@ -76,55 +50,11 @@ static tfs_entry *fs3 =
       N_FILE("fd2")
    );
 
-static map<tfs_entry *, tfs_entry *> test_mps;
-
-static void test_fs_register_mp(tfs_entry *where, tfs_entry *target)
-{
-   auto it = test_mps.find(where);
-   ASSERT_TRUE(it == test_mps.end());
-
-   test_mps[where] = target;
-}
-
-static void test_fs_clear_mps()
-{
-   test_mps.clear();
-}
-
-static bool test_fs_is_mountpoint(tfs_entry *e)
-{
-   return test_mps.find(e) != test_mps.end();
-}
-
-static void test_fs_reset_refcounts(tfs_entry *node)
-{
-   for (auto &p : node->children) {
-      p.second->ref_count = 0;
-      test_fs_reset_refcounts(p.second);
-   }
-}
-
 static void reset_all_fs_refcounts()
 {
-   test_fs_reset_refcounts(fs1);
-   test_fs_reset_refcounts(fs2);
-   test_fs_reset_refcounts(fs3);
-}
-
-static void test_fs_check_refcounts(tfs_entry *node)
-{
-   for (auto &p : node->children) {
-
-      tfs_entry *e = p.second;
-
-      if (test_fs_is_mountpoint(e)) {
-         ASSERT_EQ(e->ref_count, 1) << "[Info] mp node: " << p.first;
-      } else {
-         ASSERT_EQ(e->ref_count, 0) << "[Info] node: " << p.first;
-      }
-
-      test_fs_check_refcounts(e);
-   }
+   fs1->reset_refcounts();
+   fs2->reset_refcounts();
+   fs3->reset_refcounts();
 }
 
 static void check_all_fs_refcounts()
@@ -134,140 +64,9 @@ static void check_all_fs_refcounts()
    test_fs_check_refcounts(fs3);
 }
 
-static void
-testfs_get_entry(filesystem *fs,
-                 void *dir_inode,
-                 const char *name,
-                 ssize_t name_len,
-                 fs_path_struct *fs_path)
-{
-   if (!dir_inode && !name) {
-      fs_path->type = VFS_DIR;
-      fs_path->inode = fs->device_data;
-      fs_path->dir_inode = fs->device_data;
-      fs_path->dir_entry = nullptr;
-      return;
-   }
-
-   string s{name, (size_t)name_len};
-   tfs_entry *e = (tfs_entry *)dir_inode;
-   //cout << "get_entry: '" << s << "'" << endl;
-
-   if (s == "." || s == "..") {
-
-      if (s == "..")
-         e = e->parent;
-
-      fs_path->inode = (void *)e;
-      fs_path->type = e->type;
-      fs_path->dir_inode = e->parent;
-      return;
-   }
-
-
-   auto it = e->children.find(s);
-
-   if (it != e->children.end()) {
-      fs_path->inode = it->second;
-      fs_path->type = it->second->type;
-   } else {
-      fs_path->inode = nullptr;
-      fs_path->type = VFS_NONE;
-   }
-
-   fs_path->dir_inode = e;
-}
-
-static void vfs_test_fs_exlock(filesystem *fs)
-{
-   //printf("EXLOCK: %s\n", fs->fs_type_name);
-}
-
-static void vfs_test_fs_exunlock(filesystem *fs)
-{
-   //printf("EXUNLOCK: %s\n", fs->fs_type_name);
-}
-
-static void vfs_test_fs_shlock(filesystem *fs)
-{
-   //printf("SHLOCK: %s\n", fs->fs_type_name);
-}
-
-static void vfs_test_fs_shunlock(filesystem *fs)
-{
-   //printf("SHUNLOCK: %s\n", fs->fs_type_name);
-}
-
-static int vfs_test_retain_inode(filesystem *fs, vfs_inode_ptr_t i)
-{
-   tfs_entry *e = (tfs_entry *)i;
-   return ++e->ref_count;
-}
-
-static int vfs_test_release_inode(filesystem *fs, vfs_inode_ptr_t i)
-{
-   tfs_entry *e = (tfs_entry *)i;
-   assert(e->ref_count > 0);
-   return --e->ref_count;
-}
-
-/*
- * Unfortunately, in C++ non-trivial designated initializers are fully not
- * supported, so we have to explicitly initialize all the members, in order!
- */
-static const fs_ops static_fsops_testfs = {
-
-   .get_entry           = testfs_get_entry,
-   .get_inode           = nullptr,
-   .open                = nullptr,
-   .close               = nullptr,
-   .dup                 = nullptr,
-   .getdents            = nullptr,
-   .unlink              = nullptr,
-   .stat                = nullptr,
-   .mkdir               = nullptr,
-   .rmdir               = nullptr,
-   .symlink             = nullptr,
-   .readlink            = nullptr,
-   .truncate            = nullptr,
-   .retain_inode        = vfs_test_retain_inode,
-   .release_inode       = vfs_test_release_inode,
-   .fs_exlock           = vfs_test_fs_exlock,
-   .fs_exunlock         = vfs_test_fs_exunlock,
-   .fs_shlock           = vfs_test_fs_shlock,
-   .fs_shunlock         = vfs_test_fs_shunlock,
-};
-
-static filesystem testfs1 = {
-
-   1,                        /* ref-count */
-   "testfs1",                /* fs type name */
-   0,                        /* device_id */
-   0,                        /* flags */
-   fs1,                      /* device_data */
-   &static_fsops_testfs,     /* fsops */
-};
-
-static filesystem testfs2 = {
-
-   1,                        /* ref-count */
-   "testfs2",                /* fs type name */
-   0,                        /* device_id */
-   0,                        /* flags */
-   fs2,                      /* device_data */
-   &static_fsops_testfs,     /* fsops */
-};
-
-static filesystem testfs3 = {
-
-   1,                        /* ref-count */
-   "testfs3",                /* fs type name */
-   0,                        /* device_id */
-   0,                        /* flags */
-   fs3,                      /* device_data */
-   &static_fsops_testfs,     /* fsops */
-};
-
+static filesystem testfs1 = create_test_fs("testfs1", fs1);
+static filesystem testfs2 = create_test_fs("testfs2", fs2);
+static filesystem testfs3 = create_test_fs("testfs3", fs3);
 
 static int resolve(const char *path, vfs_path *p, bool res_last_sl)
 {
@@ -280,23 +79,6 @@ static int resolve(const char *path, vfs_path *p, bool res_last_sl)
    release_obj(p->fs);
    return rc;
 }
-
-static tfs_entry *
-path(tfs_entry *e, initializer_list<const char *> comps)
-{
-   for (auto comp_name : comps) {
-
-      auto it = e->children.find(comp_name);
-
-      if (it == e->children.end())
-         return nullptr;
-
-      e = it->second;
-   }
-
-   return e;
-}
-
 
 class vfs_resolve_test : public vfs_test_base {
 
@@ -745,5 +527,14 @@ TEST_F(vfs_resolve_multi_fs, rel_paths)
 
 TEST_F(vfs_resolve_symlinks, basic_tests)
 {
+   // int rc;
+   // vfs_path p;
 
+   // rc = resolve("/abs_s1", &p, true);
+   // ASSERT_EQ(rc, 0);
+   // ASSERT_TRUE(p.fs_path.inode != nullptr);
+   // ASSERT_TRUE(p.fs == &testfs1);
+   // ASSERT_TRUE(p.fs_path.inode == path(fs1, {"a", "b", "c", "f1"}));
+   // ASSERT_STREQ(p.last_comp, "abs_s1");
+   // ASSERT_NO_FATAL_FAILURE({ check_all_fs_refcounts(); });
 }
