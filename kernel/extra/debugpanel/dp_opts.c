00e8cf06a32c671729d624e71ce2d8cb782658d2
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-08-20 21:14:27 +0300

    [debugpanel] Working on the options screen

--- kernel/extra/debugpanel/dp_opts.c
+++ kernel/extra/debugpanel/dp_opts.c
@@ -13,13 +13,17 @@
 #endif
 
 
-#define SHOW_INT(name, val)  dp_printkln(DP_COLOR "%-32s: %d", name, val)
-#define DUMP_STR_OPT(opt)    dp_printkln(DP_COLOR "%-32s: %s", #opt, opt)
-#define DUMP_INT_OPT(opt)    dp_printkln(DP_COLOR "%-32s: %d", #opt, opt)
-#define DUMP_BOOL_OPT(opt)   dp_printkln(DP_COLOR "%-32s: %u", #opt, opt)
+#define SHOW_INT(name, val)  dp_printk(DP_COLOR "%-20s: %d", name, val)
+#define DUMP_STR_OPT(opt)    dp_printkln(DP_COLOR "%-30s: %s", #opt, opt)
+#define DUMP_INT_OPT(opt)    dp_printkln(DP_COLOR "%-30s: %d", #opt, opt)
+#define DUMP_BOOL_OPT(opt)   dp_printkln(DP_COLOR "%-30s: %u", #opt, opt)
 
 void dp_show_opts(void)
 {
+   const int row = term_get_curr_row(get_curr_term()) + 1;
+
+   dp_printkln("[         Build-time options         ]");
+
    DUMP_INT_OPT(IS_RELEASE_BUILD);
    DUMP_STR_OPT(BUILDTYPE_STR);
 
@@ -43,21 +47,33 @@ void dp_show_opts(void)
    DUMP_BOOL_OPT(KMALLOC_SUPPORT_LEAK_DETECTOR);
    DUMP_BOOL_OPT(BOOTLOADER_POISON_MEMORY);
 
-   dp_printk(DP_COLOR "\n");
+   dp_move_cursor(row, 50);
+   dp_printk("[ Boot-time options ]");
 
-// #ifndef UNIT_TEST_ENVIRONMENT
+   dp_move_cursor(row + 1, 50);
+   SHOW_INT("TERM_ROWS", term_get_rows(get_curr_term()));
 
-//    printk(NO_PREFIX "------------------- RUNTIME ------------------\n");
-//    SHOW_INT("TERM_ROWS", term_get_rows(get_curr_term()));
-//    SHOW_INT("TERM_COLS", term_get_cols(get_curr_term()));
-//    SHOW_INT("USE_FRAMEBUFFER", use_framebuffer());
-//    SHOW_INT("FB_OPT_FUNCS", fb_is_using_opt_funcs());
-//    SHOW_INT("FB_RES_X", fb_get_res_x());
-//    SHOW_INT("FB_RES_Y", fb_get_res_y());
-//    SHOW_INT("FB_BBP", fb_get_bbp());
-//    SHOW_INT("FB_FONT_W", fb_get_font_w());
-//    SHOW_INT("FB_FONT_H", fb_get_font_h());
-//    printk(NO_PREFIX "\n");
+   dp_move_cursor(row + 2, 50);
+   SHOW_INT("TERM_COLS", term_get_cols(get_curr_term()));
 
-// #endif
+   dp_move_cursor(row + 3, 50);
+   SHOW_INT("USE_FRAMEBUFFER", use_framebuffer());
+
+   dp_move_cursor(row + 4, 50);
+   SHOW_INT("FB_OPT_FUNCS", fb_is_using_opt_funcs());
+
+   dp_move_cursor(row + 5, 50);
+   SHOW_INT("FB_RES_X", fb_get_res_x());
+
+   dp_move_cursor(row + 6, 50);
+   SHOW_INT("FB_RES_Y", fb_get_res_y());
+
+   dp_move_cursor(row + 7, 50);
+   SHOW_INT("FB_BBP", fb_get_bbp());
+
+   dp_move_cursor(row + 8, 50);
+   SHOW_INT("FB_FONT_W", fb_get_font_w());
+
+   dp_move_cursor(row + 9, 50);
+   SHOW_INT("FB_FONT_H", fb_get_font_h());
 }
