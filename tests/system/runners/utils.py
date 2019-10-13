0f4b97f755fdecca0707b5b1e4872a008c6b3815
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-10-13 19:10:44 +0300

    [system_tests] Move constants and other stuff in utils.py

--- tests/system/runners/utils.py
+++ tests/system/runners/utils.py
@@ -1,6 +1,31 @@
 # SPDX-License-Identifier: BSD-2-Clause
 import sys
 
+# Constants
+
+# Fail codes:
+FAIL_SUCCESS = 0
+FAIL_INVALID_ARGS = 1
+FAIL_REBOOT = 2
+FAIL_TIMEOUT = 3
+FAIL_PANIC = 4
+FAIL_SHELL_NO_ZERO_EXIT = 5
+FAIL_GCOV_ERROR = 6
+
+error_codes_strings = {
+   FAIL_SUCCESS: 'success',
+   FAIL_INVALID_ARGS: 'invalid_arguments',
+   FAIL_REBOOT: 'reboot',
+   FAIL_TIMEOUT: 'timeout',
+   FAIL_PANIC: 'panic',
+   FAIL_SHELL_NO_ZERO_EXIT: 'shell_no_zero_exit',
+   FAIL_GCOV_ERROR: 'gcov_issue'
+}
+
+# Constants coming from CMake (this file gets pre-processed by CMake)
+KERNEL_FILE = r'@KERNEL_FILE@'
+BUILD_DIR = r'@CMAKE_BINARY_DIR@'
+
 def raw_print(msg):
    sys.stdout.buffer.write(msg.encode('utf-8'))
    sys.stdout.buffer.write('\n'.encode('utf-8'))
