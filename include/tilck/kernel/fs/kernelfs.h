274bc9219b2be2311d6afc8620ee41e66643fd50
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-10-25 20:17:37 +0300

    [kernelfs] Implementing kernelfs

--- include/tilck/kernel/fs/kernelfs.h
+++ include/tilck/kernel/fs/kernelfs.h
@@ -3,4 +3,20 @@
 #pragma once
 #include <tilck/kernel/fs/vfs.h>
 
+#define KOBJ_BASE_FIELDS                    \
+   REF_COUNTED_OBJECT;                      \
+   void (*destory)(struct kobj_base *);
+
+struct kobj_base {
+
+   KOBJ_BASE_FIELDS
+};
+
+struct kernel_fs_handle {
+
+   FS_HANDLE_BASE_FIELDS
+   struct kobj_base *kobj;
+};
+
 void init_kernelfs(void);
+struct kernel_fs_handle *kfs_create_new_handle(void);
--- kernel/fs/kernelfs.c
+++ kernel/fs/kernelfs.c
@@ -3,6 +3,8 @@
 #include <tilck/common/basic_defs.h>
 #include <tilck/common/string_util.h>
 
+#include <tilck/kernel/errno.h>
+#include <tilck/kernel/fs/kernelfs.h>
 #include <tilck/kernel/kmalloc.h>
 #include <tilck/kernel/fs/vfs.h>
 #include <tilck/kernel/sys_types.h>
@@ -14,13 +16,16 @@ static void no_lock(struct fs *fs) { }
 static vfs_inode_ptr_t
 kernelfs_get_inode(fs_handle h)
 {
-   NOT_IMPLEMENTED();
+   return ((struct kernel_fs_handle *)h)->kobj;
 }
 
 static void
 kernelfs_close(fs_handle h)
 {
-   NOT_IMPLEMENTED();
+   struct kernel_fs_handle *kh = h;
+
+   if (release_obj(kh->kobj) == 0)
+      kh->kobj->destory(kh->kobj);
 }
 
 int
@@ -30,32 +35,55 @@ kernelfs_stat(struct fs *fs, vfs_inode_ptr_t i, struct stat64 *statbuf)
 }
 
 static int
-kernelfs_dup(fs_handle fsh, fs_handle *dup_h)
-{
-   NOT_IMPLEMENTED();
-}
-
-static int
 kernelfs_retain_inode(struct fs *fs, vfs_inode_ptr_t inode)
 {
-   NOT_IMPLEMENTED();
+   return retain_obj((struct kobj_base *)inode);
 }
 
 static int kernelfs_release_inode(struct fs *fs, vfs_inode_ptr_t inode)
 {
-   NOT_IMPLEMENTED();
+   return release_obj((struct kobj_base *)inode);
 }
 
+static int
+kernelfs_dup(fs_handle fsh, fs_handle *dup_h)
+{
+   struct kernel_fs_handle *n;
+
+   if (!(n = (void *)kmalloc(sizeof(struct kernel_fs_handle))))
+      return -ENOMEM;
+
+   memcpy(n, fsh, sizeof(struct kernel_fs_handle));
+   retain_obj(n->kobj);
+   *dup_h = n;
+
+   return 0;
+}
+
+struct kernel_fs_handle *
+kfs_create_new_handle(void)
+{
+   struct kernel_fs_handle *h;
+
+   if (!(h = (void *)kzmalloc(sizeof(struct kernel_fs_handle))))
+      return NULL;
+
+   h->fs = kernelfs;
+   return h;
+}
 
 static const struct fs_ops static_fsops_kernelfs =
 {
-   .get_inode = kernelfs_get_inode,
-   .close = kernelfs_close,
+   /* Implemented by the kernel object (e.g. pipe) */
    .stat = kernelfs_stat,
-   .dup = kernelfs_dup,
-
    .retain_inode = kernelfs_retain_inode,
    .release_inode = kernelfs_release_inode,
+
+   /* Implemented here */
+   .close = kernelfs_close,
+   .dup = kernelfs_dup,
+   .get_inode = kernelfs_get_inode,
+
    .fs_exlock = no_lock,
    .fs_exunlock = no_lock,
    .fs_shlock = no_lock,
