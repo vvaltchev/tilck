cb558a531d527e52f2b341edc824e4775a188e37
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-10-27 01:56:36 +0300

    [fat] Rename struct `fat_handle` to `fatfs_handle`

--- kernel/fs/fat32.c
+++ kernel/fs/fat32.c
@@ -27,14 +27,14 @@ fat_get_first_cluster_generic(struct fat_fs_device_data *d, struct fat_entry *e)
 STATIC void
 fat_close(fs_handle handle)
 {
-   struct fat_handle *h = (struct fat_handle *)handle;
-   kfree2(h, sizeof(struct fat_handle));
+   struct fatfs_handle *h = (struct fatfs_handle *)handle;
+   kfree2(h, sizeof(struct fatfs_handle));
 }
 
 STATIC ssize_t
 fat_read(fs_handle handle, char *buf, size_t bufsize)
 {
-   struct fat_handle *h = (struct fat_handle *) handle;
+   struct fatfs_handle *h = (struct fatfs_handle *) handle;
    struct fat_fs_device_data *d = h->fs->device_data;
    offt fsize = (offt)h->e->DIR_FileSize;
    offt written_to_buf = 0;
@@ -96,7 +96,7 @@ fat_read(fs_handle handle, char *buf, size_t bufsize)
 STATIC int
 fat_rewind(fs_handle handle)
 {
-   struct fat_handle *h = (struct fat_handle *) handle;
+   struct fatfs_handle *h = (struct fatfs_handle *) handle;
    h->pos = 0;
    h->curr_cluster = fat_get_first_cluster(h->e);
    return 0;
@@ -105,7 +105,7 @@ fat_rewind(fs_handle handle)
 STATIC offt
 fat_seek_forward(fs_handle handle, offt dist)
 {
-   struct fat_handle *h = (struct fat_handle *) handle;
+   struct fatfs_handle *h = (struct fatfs_handle *) handle;
    struct fat_fs_device_data *d = h->fs->device_data;
    offt fsize = (offt)h->e->DIR_FileSize;
    offt moved_distance = 0;
@@ -190,7 +190,7 @@ STATIC offt fat_count_dirents(struct fat_fs_device_data *d, struct fat_entry *e)
    return rc ? rc : count;
 }
 
-static offt fat_seek_dir(struct fat_handle *fh, offt off)
+static offt fat_seek_dir(struct fatfs_handle *fh, offt off)
 {
    if (off < 0)
       return -EINVAL;
@@ -205,7 +205,7 @@ static offt fat_seek_dir(struct fat_handle *fh, offt off)
 STATIC offt
 fat_seek(fs_handle handle, offt off, int whence)
 {
-   struct fat_handle *fh = handle;
+   struct fatfs_handle *fh = handle;
 
    if (fh->e->directory) {
 
@@ -232,7 +232,7 @@ fat_seek(fs_handle handle, offt off, int whence)
          if (off >= 0)
             break;
 
-         struct fat_handle *h = (struct fat_handle *) handle;
+         struct fatfs_handle *h = (struct fatfs_handle *) handle;
          off = (offt) h->e->DIR_FileSize + off;
 
          if (off < 0)
@@ -330,7 +330,7 @@ STATIC int fat_stat(struct fs *fs, vfs_inode_ptr_t i, struct stat64 *statbuf)
 
 struct fat_getdents_ctx {
 
-   struct fat_handle *fh;
+   struct fatfs_handle *fh;
    get_dents_func_cb vfs_cb;
    void *vfs_ctx;
    int rc;
@@ -362,7 +362,7 @@ fat_getdents_cb(struct fat_hdr *hdr,
 
 static int fat_getdents(fs_handle h, get_dents_func_cb cb, void *arg)
 {
-   struct fat_handle *fh = h;
+   struct fatfs_handle *fh = h;
    struct fat_fs_device_data *d = fh->fs->device_data;
    struct fat_walk_dir_ctx walk_ctx = {0};
    int rc;
@@ -459,7 +459,7 @@ static const struct file_ops static_ops_fat =
 STATIC int
 fat_open(struct vfs_path *p, fs_handle *out, int fl, mode_t mode)
 {
-   struct fat_handle *h;
+   struct fatfs_handle *h;
    struct fs *fs = p->fs;
    struct fat_fs_path *fp = (struct fat_fs_path *)&p->fs_path;
    struct fat_entry *e = fp->entry;
@@ -480,7 +480,7 @@ fat_open(struct vfs_path *p, fs_handle *out, int fl, mode_t mode)
       if (fl & (O_WRONLY | O_RDWR))
          return -EROFS;
 
-   if (!(h = kzmalloc(sizeof(struct fat_handle))))
+   if (!(h = kzmalloc(sizeof(struct fatfs_handle))))
       return -ENOMEM;
 
    h->fs = fs;
@@ -496,12 +496,12 @@ fat_open(struct vfs_path *p, fs_handle *out, int fl, mode_t mode)
 
 STATIC int fat_dup(fs_handle h, fs_handle *dup_h)
 {
-   struct fat_handle *new_h = kmalloc(sizeof(struct fat_handle));
+   struct fatfs_handle *new_h = kmalloc(sizeof(struct fatfs_handle));
 
    if (!new_h)
       return -ENOMEM;
 
-   memcpy(new_h, h, sizeof(struct fat_handle));
+   memcpy(new_h, h, sizeof(struct fatfs_handle));
    *dup_h = new_h;
    return 0;
 }
@@ -573,7 +573,7 @@ fat_get_entry(struct fs *fs,
 
 static vfs_inode_ptr_t fat_get_inode(fs_handle h)
 {
-   return ((struct fat_handle *)h)->e;
+   return ((struct fatfs_handle *)h)->e;
 }
 
 static int fat_retain_inode(struct fs *fs, vfs_inode_ptr_t inode)
