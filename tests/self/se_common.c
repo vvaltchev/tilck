0dc7e7a77bd2cfc6d6a7881b56169d2a043bb4ea
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-11-11 16:46:27 +0200

    [selftests] Support stop request with Ctrl+C

--- tests/self/se_common.c
+++ tests/self/se_common.c
@@ -16,6 +16,68 @@
 #include <tilck/kernel/timer.h>
 #include <tilck/kernel/self_tests.h>
 #include <tilck/kernel/cmdline.h>
+#include <tilck/kernel/kb.h>
+
+static volatile bool se_stop_requested;
+static void (*se_running_func)(void);
+
+bool se_is_stop_requested(void)
+{
+   return se_stop_requested;
+}
+
+void se_internal_run(void (*se_func)(void))
+{
+   /* Common self test setup code */
+   disable_preemption();
+   {
+      se_stop_requested = false;
+      se_running_func = se_func;
+   }
+   enable_preemption();
+
+   /* Run the actual self test */
+   se_func();
+
+   /* Common self test tear down code */
+   disable_preemption();
+   {
+      se_stop_requested = false;
+      se_running_func = NULL;
+   }
+   enable_preemption();
+}
+
+static int se_keypress_handler(struct key_event ke)
+{
+   int ret = KB_HANDLER_NAK;
+
+   if (!se_running_func)
+      return ret;
+
+   disable_preemption();
+
+   if (se_running_func) {
+      if (ke.print_char == 'c' && kb_is_ctrl_pressed()) {
+         se_stop_requested = true;
+         ret = KB_HANDLER_OK_AND_STOP;
+      }
+   }
+
+   enable_preemption();
+   return ret;
+}
+
+static struct keypress_handler_elem se_handler =
+{
+   .handler = &se_keypress_handler
+};
+
+__attribute__((constructor))
+static void init_self_tests_infrastructure()
+{
+   kb_register_keypress_handler(&se_handler);
+}
 
 void regular_self_test_end(void)
 {
