dec3f0fe92ed68cfc372a5083a2c4626d99f55b1
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-11-12 15:56:23 +0200

    [se_tasklet] Use a kmutex with kcond to avoid a race condition
    BONUS: making the test more verbose about its state.

--- tests/self/se_tasklet.c
+++ tests/self/se_tasklet.c
@@ -17,31 +17,42 @@ static void test_tasklet_func()
    counter++;
 }
 
-static void end_test(void *arg)
+static void end_test(void *arg1, void *arg2)
 {
-   struct kcond *c = arg;
+   struct kcond *c = arg1;
+   struct kmutex *m = arg2;
 
    const u32 max_tasklets = get_tasklet_runner_limit(0);
    const u32 tot_iters = max_tasklets * 10;
 
    u64 elapsed = RDTSC() - cycles_begin;
    VERIFY(counter == tot_iters);
-   printk("[selftest_tasklet] Avg cycles per tasklet "
+   printk("[se_tasklet] Avg cycles per tasklet "
           "(enqueue + execute): %llu\n", elapsed/counter);
 
-   kcond_signal_one(c);
+   kmutex_lock(m);
+   {
+      printk("[se_tasklet] end_test() holding the lock and signalling cond\n");
+      kcond_signal_one(c);
+   }
+   kmutex_unlock(m);
+   printk("[se_tasklet] end_test() func completed\n");
 }
 
 void selftest_tasklet_short(void)
 {
    const u32 max_tasklets = get_tasklet_runner_limit(0);
    const u32 tot_iters = max_tasklets * 10;
-
+   struct kcond c;
+   struct kmutex m;
    bool added;
+
+   kcond_init(&c);
+   kmutex_init(&m, 0);
    counter = 0;
 
    ASSERT(is_preemption_enabled());
-   printk("[selftest_tasklet] BEGIN\n");
+   printk("[se_tasklet] BEGIN\n");
 
    cycles_begin = RDTSC();
 
@@ -50,19 +61,25 @@ void selftest_tasklet_short(void)
       do {
          added = enqueue_tasklet0(0, &test_tasklet_func);
       } while (!added);
-
    }
 
-   struct kcond c;
-   kcond_init(&c);
+   printk("[se_tasklet] Main test done, now enqueue end_test()\n");
+   kmutex_lock(&m);
+   {
+      printk("[se_tasklet] Under lock, before enqueue\n");
 
-   do {
-      added = enqueue_tasklet1(0, &end_test, &c);
-   } while (!added);
+      do {
+         added = enqueue_tasklet2(0, &end_test, &c, &m);
+      } while (!added);
 
-   kcond_wait(&c, NULL, KCOND_WAIT_FOREVER);
+      printk("[se_tasklet] Under lock, AFTER enqueue\n");
+      printk("[se_tasklet] Now, wait on cond\n");
+      kcond_wait(&c, &m, KCOND_WAIT_FOREVER);
+   }
+   kmutex_unlock(&m);
    kcond_destory(&c);
-   printk("[selftest_tasklet] END\n");
+   kmutex_destroy(&m);
+   printk("[se_tasklet] END\n");
    regular_self_test_end();
 }
 
