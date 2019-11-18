fb14297b58651196862b7bdfe21f11b23170909f
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-11-18 18:33:27 +0200

    [utils] Introduce the function(s) round_down_at[64]()

--- include/tilck/common/utils.h
+++ include/tilck/common/utils.h
@@ -122,3 +122,13 @@ CONSTEXPR static ALWAYS_INLINE u64 round_up_at64(u64 n, u64 unit)
 {
    return ((n + unit - 1) / unit) * unit;
 }
+
+CONSTEXPR static ALWAYS_INLINE uptr round_down_at(uptr n, uptr unit)
+{
+   return (n / unit) * unit;
+}
+
+CONSTEXPR static ALWAYS_INLINE u64 round_down_at64(u64 n, u64 unit)
+{
+   return (n / unit) * unit;
+}
