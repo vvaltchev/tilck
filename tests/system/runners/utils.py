fc303e6195250cf07ce279782a8d9bc9d5f4db29
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-11-23 15:07:03 +0200

    [system_tests] Introduce the variable in_any_ci

--- tests/system/runners/utils.py
+++ tests/system/runners/utils.py
@@ -40,9 +40,14 @@ def msg_print(msg):
 # Environment flags
 in_travis = os.environ.get('TRAVIS', False)
 in_circleci = os.environ.get('CIRCLECI', False)
+in_azure = os.environ.get('AZURE_HTTP_USER_AGENT', False)
 dump_coverage = os.environ.get('DUMP_COV', False)
 report_coverage = os.environ.get('REPORT_COV', False)
 verbose = os.environ.get('VERBOSE', False)
+in_any_ci = False
+
+if in_travis or in_circleci or in_azure:
+   in_any_ci = True
 
 kvm_installed = False
 qemu_kvm_version = None
@@ -85,7 +90,7 @@ def detect_kvm():
          raw_print(r)
 
    except:
-      if not in_circleci and not in_travis:
+      if not in_any_ci:
          raw_print(
             "\n"
             "*** WARNING: qemu-kvm not found on the system ***\n"
