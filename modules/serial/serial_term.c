c242861ee13a459af942baddad3c0a1b4c3100de
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2019-12-10 13:33:47 +0200

    [serial_term] Serialize writes as the video term does

--- modules/serial/serial_term.c
+++ modules/serial/serial_term.c
@@ -2,12 +2,25 @@
 
 #include <tilck/kernel/term.h>
 #include <tilck/kernel/kmalloc.h>
+#include <tilck/kernel/safe_ringbuf.h>
 #include <tilck/mods/serial.h>
+
+struct term_action {
+
+   /* Only one action is supported: write */
+   const char *buf;
+   size_t len;
+};
+
+STATIC_ASSERT(sizeof(struct term_action) == (2 * sizeof(uptr)));
 
 struct term {
 
    bool initialized;
    u16 serial_port_fwd;
+
+   struct safe_ringbuf ringb;
+   struct term_action actions_buf[32];
 };
 
 static struct term first_instance;
@@ -34,7 +47,7 @@ sterm_get_params(struct term *t, struct term_params *out)
 }
 
 static void
-sterm_write(struct term *t, const char *buf, size_t len, u8 color)
+sterm_action_write(struct term *t, const char *buf, size_t len)
 {
    for (u32 i = 0; i < len; i++) {
       serial_write(t->serial_port_fwd, buf[i]);
@@ -42,25 +55,37 @@ sterm_write(struct term *t, const char *buf, size_t len, u8 color)
 }
 
 static void
-sterm_scroll_up(struct term *t, u32 lines) { }
+serial_term_execute_or_enqueue_action(struct term *t, struct term_action a)
+{
+   bool written;
+   bool was_empty;
+
+   written = safe_ringbuf_write_elem_ex(&t->ringb, &a, &was_empty);
+
+   /*
+    * written would be false only if the ringbuf was full. In order that to
+    * happen, we'll need ARRAY_SIZE(actions_buf) nested interrupts and
+    * all of them need to issue a term_* call. Virtually "impossible".
+    */
+   VERIFY(written);
+
+   if (was_empty) {
+
+      while (safe_ringbuf_read_elem(&t->ringb, &a))
+         sterm_action_write(t, a.buf, a.len);
+   }
+}
 
 static void
-sterm_scroll_down(struct term *t, u32 lines) { }
+sterm_write(struct term *t, const char *buf, size_t len, u8 color)
+{
+   struct term_action a = {
+      .buf = buf,
+      .len = len,
+   };
 
-static void
-sterm_set_col_offset(struct term *t, int off) { }
-
-static void
-sterm_pause_video_output(struct term *t) { }
-
-static void
-sterm_restart_video_output(struct term *t) { }
-
-static void
-sterm_set_cursor_enabled(struct term *t, bool value) { }
-
-static void
-sterm_set_filter(struct term *t, term_filter func, void *ctx) { }
+   serial_term_execute_or_enqueue_action(t, a);
+}
 
 static struct term *sterm_get_first_inst(void)
 {
@@ -91,6 +116,11 @@ sterm_init(struct term *t, u16 serial_port_fwd)
 {
    t->serial_port_fwd = serial_port_fwd;
    t->initialized = true;
+
+   safe_ringbuf_init(&t->ringb,
+                     ARRAY_SIZE(t->actions_buf),
+                     sizeof(struct term_action),
+                     t->actions_buf);
    return 0;
 }
 
@@ -101,13 +131,13 @@ static const struct term_interface intf = {
    .get_params = sterm_get_params,
 
    .write = sterm_write,
-   .scroll_up = sterm_scroll_up,
-   .scroll_down = sterm_scroll_up,
-   .set_col_offset = sterm_set_col_offset,
-   .pause_video_output = sterm_pause_video_output,
-   .restart_video_output = sterm_restart_video_output,
-   .set_cursor_enabled = sterm_set_cursor_enabled,
-   .set_filter = sterm_set_filter,
+   .scroll_up = NULL,
+   .scroll_down = NULL,
+   .set_col_offset = NULL,
+   .pause_video_output = NULL,
+   .restart_video_output = NULL,
+   .set_cursor_enabled = NULL,
+   .set_filter = NULL,
 
    .get_first_term = sterm_get_first_inst,
    .video_term_init = NULL,
