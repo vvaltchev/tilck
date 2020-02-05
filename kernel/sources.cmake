733b642fea032b358ebae0fa874ffbc717a1ed83
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-02-05 18:46:54 +0200

    [cmake] Move the build of kernel_noarch_for_test in unit tests
    Reason: it was not possible to build the kernel with clang while having
    ARCH_GTESTS=1 because the cross-compiler was set at the top of kernel's CMake
    file in case of ARCH_GTESTS=1.
    
    Side effects: it was necessary to make ${KERNEL_NOARCH_SOURCES} globally
    available in all CMake files.

---
+++ kernel/sources.cmake
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: BSD-2-Clause
+cmake_minimum_required(VERSION 3.2)
+
+set(
+   KERNEL_NOARCH_SOURCES_GLOB
+
+   "${CMAKE_SOURCE_DIR}/kernel/*.c"
+   "${CMAKE_SOURCE_DIR}/kernel/*/*.c"
+   "${CMAKE_SOURCE_DIR}/kernel/fs/*/*.c"
+   "${CMAKE_SOURCE_DIR}/common/*.c"
+   "${CMAKE_SOURCE_DIR}/common/3rd_party/datetime.c"
+)
+
+if (KERNEL_SELFTESTS)
+   list(
+      APPEND
+      KERNEL_NOARCH_SOURCES_GLOB
+      "${CMAKE_SOURCE_DIR}/tests/self/*.c"
+   )
+endif()
+
+file(GLOB KERNEL_NOARCH_SOURCES ${KERNEL_NOARCH_SOURCES_GLOB})
