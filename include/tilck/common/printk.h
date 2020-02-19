35fcdeadf50254858aa7ad2bc389dd41b1b2a4f4
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-02-20 00:10:52 +0200

    [printk] Re-define printk -> printf #if defined(USERMODE_APP)

--- include/tilck/common/printk.h
+++ include/tilck/common/printk.h
@@ -3,6 +3,8 @@
 #pragma once
 #include <tilck/common/basic_defs.h>
 
+#ifndef USERMODE_APP
+
 void vprintk(const char *fmt, va_list args);
 void printk(const char *fmt, ...);
 
@@ -20,4 +22,25 @@ void printk(const char *fmt, ...);
    #define NO_PREFIX          "\x01\x01\x00\x00"
 #else
    #define NO_PREFIX          ""
+#endif
+
+#else
+
+/*
+ * Code in `common/` such as fat32_base.c is used also in user apps like
+ * fat_get_used_bytes.c and it will be great to be able to printk there
+ * as well instead of using dirty #ifdef USERMODE_APP switches to decide
+ * whether to use printk or printf.
+ */
+
+#include <stdio.h>
+#define printk printf
+#define vprintk vprintf
+
+/*
+ * NOTE: not including snprintk() because it's available only for the kernel
+ * itself, not in the `common` code. See basic_printk.c, the one used for the
+ * `common` code.
+ */
+
 #endif
