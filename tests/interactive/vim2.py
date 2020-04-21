9f3d7e379919633f78ff0b279552f8e6669ee4de
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-04-21 20:45:56 +0200

    [interactive] Intr. the vim2 interactive test

---
+++ tests/interactive/vim2.py
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# pylint: skip-file
+#
+# NOTE: this file, as all the others in this directory, run in the same global
+# context as their runner (run_interactive_test).
+
+do_interactive_actions(
+   "vim2",
+   [
+      r"cd /usr/lib/vim/samples{ret}"
+      r"vim perl.pl{ret}",
+      r"{esc}:open python.py{ret}",
+      r"{esc}:open ruby.rb{ret}",
+      r"{esc}:open shell.sh{ret}",
+      r"{esc}:q{ret}cd /{ret}",
+   ]
+)
