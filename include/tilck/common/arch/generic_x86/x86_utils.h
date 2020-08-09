ba53a08cdbfe808d10f1aaeb958b9e0470c64a4a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-08-09 03:33:18 +0300

    [x86] Improve inb/outb

--- include/tilck/common/arch/generic_x86/x86_utils.h
+++ include/tilck/common/arch/generic_x86/x86_utils.h
@@ -93,24 +93,22 @@
 
 static ALWAYS_INLINE void outb(u16 port, u8 val)
 {
-   /*
-    * There's an outb %al, $imm8  encoding, for compile-time constant port
-    * numbers that fit in 8b.  (N constraint). Wider immediate constants
-    * would be truncated at assemble-time (e.g. "i" constraint).
-    * The  outb  %al, %dx  encoding is the only option for all other cases.
-    * %1 expands to %dx because  port  is a u16.  %w1 could be used if we had
-    * the port number a wider C type.
-    */
-   asmVolatile("outb %0, %1" : : "a"(val), "Nd"(port));
+   asmVolatile(
+      "outb %[value], %[port]"
+      : /* no output */
+      : [value] "a"(val), [port] "Nd"(port)
+   );
 }
 
 static ALWAYS_INLINE u8 inb(u16 port)
 {
-   u8 ret_val;
-   asmVolatile("inb %[port], %[result]"
-      : [result] "=a"(ret_val)   // using symbolic operand names
-      : [port] "Nd"(port));
-   return ret_val;
+   u8 ret;
+   asmVolatile(
+      "inb %[port], %[result]"
+      : [result] "=a"(ret)
+      : [port] "Nd"(port)
+   );
+   return ret;
 }
 
 static ALWAYS_INLINE void halt(void)
