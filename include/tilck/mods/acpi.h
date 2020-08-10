cf37b6a1fb37026feab631560606151b305cc0c3
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-08-11 02:13:25 +0300

    [acpi] Intr. acpi_module.c and the mods/acpi.h header

---
+++ include/tilck/mods/acpi.h
@@ -0,0 +1,6 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#pragma once
+#include <tilck/common/basic_defs.h>
+
+void early_init_acpi_module(void);
---
+++ modules/acpi/acpi_module.c
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#include <tilck/common/basic_defs.h>
+#include <tilck/common/printk.h>
+
+#include <tilck/kernel/sched.h>
+
+#include "osl.h"
+#include <3rd_party/acpi/acpi.h>
+#include <3rd_party/acpi/acexcep.h>
+
+static ACPI_TABLE_DESC acpi_tables[16];
+
+void
+early_init_acpi_module(void)
+{
+   ACPI_STATUS rc;
+
+   rc = AcpiInitializeTables(acpi_tables, 16, false);
+
+   if (rc != AE_OK)
+      panic("AcpiInitializeTables() failed with: %d", rc);
+}
