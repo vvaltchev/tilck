15ee02ac8b4109dcb3ff88d8ee40bf5edae2053e
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-08-27 13:15:37 +0300

    [acpi] Intr. ACPICA files (ver. 20200717) in the source tree

---
+++ include/3rd_party/acpi/platform/actilck.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *
+ * Name: actilck.h - OS specific defines, etc. for Tilck
+ *
+ *****************************************************************************/
+
+#pragma once
+#include <tilck/common/basic_defs.h>
+#include <tilck/common/string_util.h>
+
+#define ACPI_USE_SYSTEM_CLIBRARY
+#define ACPI_USE_NATIVE_MATH64
+#define ACPI_USE_NATIVE_DIVIDE
+#define ACPI_USE_LOCAL_CACHE
+
+#define ACPI_MACHINE_WIDTH          NBITS
