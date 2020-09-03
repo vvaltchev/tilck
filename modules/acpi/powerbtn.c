ea7556fe1a4a3ef20fbeb3ea1ce946762f43830a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-09-03 21:56:17 +0300

    [acpi] Register notify handler for control-method PWBTN

--- modules/acpi/powerbtn.c
+++ modules/acpi/powerbtn.c
@@ -8,7 +8,7 @@
 static ACPI_STATUS
 acpi_fix_power_button_handler(void *ctx)
 {
-   printk("ACPI: power button fix event\n");
+   printk("ACPI: power button fixed event\n");
    return 0; /* MUST return 0 in ANY case. Other values are reserved */
 }
 
@@ -29,6 +29,36 @@ powerbtn_reg_fix_handlers(void *__ctx)
    return AE_OK;
 }
 
+static void
+powerbtn_notify_handler(ACPI_HANDLE Device,
+                        UINT32 Value,
+                        void *Context)
+{
+   printk("ACPI: power button notify event, device: %p, val: %#x\n",
+          Device, Value);
+}
+
+static ACPI_STATUS
+powerbtn_reg_notify_cb(void *obj_handle,
+                       void *device_info,
+                       void *ctx)
+{
+   ACPI_STATUS rc;
+   printk("ACPI: register notify handler for power button\n");
+
+   rc = AcpiInstallNotifyHandler(obj_handle,
+                                 ACPI_ALL_NOTIFY,
+                                 &powerbtn_notify_handler,
+                                 NULL);
+
+   if (ACPI_FAILURE(rc)) {
+      print_acpi_failure("AcpiInstallNotifyHandler", NULL, rc);
+      return rc;
+   }
+
+   return AE_OK;
+}
+
 __attribute__((constructor))
 static void __reg_callbacks(void)
 {
@@ -37,6 +67,17 @@ static void __reg_callbacks(void)
       .ctx = NULL
    };
 
+   static struct acpi_reg_per_object_cb_node notf = {
+      .cb = &powerbtn_reg_notify_cb,
+      .ctx = NULL,
+      .hid = "PNP0C0C",
+      .uid = NULL,
+      .cls = NULL,
+   };
+
    list_node_init(&fixh.node);
    acpi_reg_on_subsys_enabled_cb(&fixh);
+
+   list_node_init(&notf.node);
+   acpi_reg_per_object_cb(&notf);
 }
