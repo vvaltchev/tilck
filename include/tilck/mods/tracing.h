099c8c1a20c1afa0d07b4f61ebe249c667ebf13a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2021-06-24 20:11:51 +0300

    [tracing] Make trace_printk() work also w/o args

--- include/tilck/mods/tracing.h
+++ include/tilck/mods/tracing.h
@@ -328,7 +328,7 @@ tracing_set_printk_lvl(int lvl)
 
 #define trace_printk(lvl, fmt, ...)                                            \
    if (MOD_tracing && UNLIKELY(tracing_is_enabled())) {                        \
-      trace_printk_int((lvl), fmt, __VA_ARGS__);                               \
+      trace_printk_int((lvl), fmt, ##__VA_ARGS__);                             \
    }
 
 #define trace_signal_delivered(target_tid, signum)                             \
