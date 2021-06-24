01b04c7aebcd128b8e482b96ac89eb70c5eea15e
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2021-06-24 22:51:56 +0300

    [utils] Move get_first_{zero,set}_bit_index[64] in utils.h
    Despite those functions were left in bithacks.h, they are not really
    special bit tricks taken from:
    
    	http://graphics.stanford.edu/~seander/bithacks.html
    
    Originally, there was just one utils.h header. After renaming
    utils.h -> bithacks.h and introducing (again) utils.h, those functions
    have been left in bithacks.h as a mistake.

--- include/tilck/common/utils.h
+++ include/tilck/common/utils.h
@@ -47,3 +47,58 @@ make_bitmask(ulong width)
    ulong w = (sizeof(ulong) * 8) - width;
    return (((ulong)-1) << w) >> w;
 }
+
+
+CONSTEXPR static inline u32
+get_first_zero_bit_index(u32 num)
+{
+   u32 i;
+
+   ASSERT(num != ~0U);
+
+   for (i = 0; i < 32; i++) {
+      if ((num & (1U << i)) == 0) break;
+   }
+
+   return i;
+}
+
+CONSTEXPR static inline u32
+get_first_set_bit_index(u32 num)
+{
+   u32 i;
+   ASSERT(num != 0);
+
+   for (i = 0; i < 32; i++)
+      if (num & (1U << i))
+         break;
+
+   return i;
+}
+
+CONSTEXPR static inline u32
+get_first_zero_bit_index64(u64 num)
+{
+   u32 i;
+
+   ASSERT(num != ~0U);
+
+   for (i = 0; i < 64; i++) {
+      if ((num & (1ull << i)) == 0) break;
+   }
+
+   return i;
+}
+
+CONSTEXPR static inline u32
+get_first_set_bit_index64(u64 num)
+{
+   u32 i;
+   ASSERT(num != 0);
+
+   for (i = 0; i < 64; i++)
+      if (num & (1ull << i))
+         break;
+
+   return i;
+}
