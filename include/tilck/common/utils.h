1360140ef38530fef00b16767220f669bf23f37a
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2021-06-24 23:01:47 +0300

    [utils] Intr. get_first_{zero,set}_bit_index_l()

--- include/tilck/common/utils.h
+++ include/tilck/common/utils.h
@@ -53,12 +53,11 @@ CONSTEXPR static inline u32
 get_first_zero_bit_index32(u32 num)
 {
    u32 i;
-
    ASSERT(num != ~0U);
 
-   for (i = 0; i < 32; i++) {
-      if ((num & (1U << i)) == 0) break;
-   }
+   for (i = 0; i < 32; i++)
+      if ((num & (1U << i)) == 0)
+         break;
 
    return i;
 }
@@ -83,9 +82,9 @@ get_first_zero_bit_index64(u64 num)
 
    ASSERT(num != ~0U);
 
-   for (i = 0; i < 64; i++) {
-      if ((num & (1ull << i)) == 0) break;
-   }
+   for (i = 0; i < 64; i++)
+      if ((num & (1ull << i)) == 0)
+         break;
 
    return i;
 }
@@ -98,6 +97,32 @@ get_first_set_bit_index64(u64 num)
 
    for (i = 0; i < 64; i++)
       if (num & (1ull << i))
+         break;
+
+   return i;
+}
+
+CONSTEXPR static inline u32
+get_first_zero_bit_index_l(ulong num)
+{
+   u32 i;
+   ASSERT(num != ~0U);
+
+   for (i = 0; i < NBITS; i++)
+      if ((num & (1UL << i)) == 0)
+         break;
+
+   return i;
+}
+
+CONSTEXPR static inline u32
+get_first_set_bit_index_l(ulong num)
+{
+   u32 i;
+   ASSERT(num != 0);
+
+   for (i = 0; i < NBITS; i++)
+      if (num & (1UL << i))
          break;
 
    return i;
