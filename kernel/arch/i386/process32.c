4cd35785f4b11b78b22174a59210e80a093f82fb
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2021-06-26 00:46:38 +0300

    [process32] Extr. nested interrupts adjust logic from switch_to_task()

--- kernel/arch/i386/process32.c
+++ kernel/arch/i386/process32.c
@@ -413,7 +413,8 @@ void save_current_task_state(regs_t *r)
  * Sched functions that are here because of arch-specific statements.
  */
 
-void set_current_task_in_user_mode(void)
+void
+set_current_task_in_user_mode(void)
 {
    ASSERT(!is_preemption_enabled());
    struct task *curr = get_curr_task();
@@ -424,13 +425,15 @@ void set_current_task_in_user_mode(void)
    set_kernel_stack((u32)curr->state_regs);
 }
 
-static inline bool is_fpu_enabled_for_task(struct task *ti)
+static inline bool
+is_fpu_enabled_for_task(struct task *ti)
 {
    return get_task_arch_fields(ti)->aligned_fpu_regs &&
           (ti->state_regs->custom_flags & REGS_FL_FPU_ENABLED);
 }
 
-static inline void save_curr_fpu_ctx_if_enabled(void)
+static inline void
+save_curr_fpu_ctx_if_enabled(void)
 {
    if (is_fpu_enabled_for_task(get_curr_task())) {
       hw_fpu_enable();
@@ -452,7 +455,43 @@ switch_to_task_pop_nested_interrupts(void)
    }
 }
 
-NORETURN void switch_to_task(struct task *ti)
+static inline void
+adjust_nested_interrupts_for_task_in_kernel(struct task *ti)
+{
+   /*
+    * The new task was running in kernel when it was preempted.
+    *
+    * In theory, there's nothing we have to do here, and that's exactly
+    * what happens when KRN_TRACK_NESTED_INTERR is 0. But, our nice
+    * debug feature for nested interrupts tracking requires a little work:
+    * because of its assumptions (hard-coded in ASSERTS) are that when the
+    * kernel is running, it's always inside some kind of interrupt handler
+    * (fault, int 0x80 [syscall], IRQ) before resuming the next task, we have
+    * to resume the state of the nested_interrupts in one case: the one when
+    * we're resuming a USER task that was running in KERNEL MODE (the kernel
+    * was running on behalf of the task). In that case, when for the first
+    * time the user task got to the kernel, we had a nice 0x80 added in our
+    * nested_interrupts array [even in the case of sysenter] by the function
+    * soft_interrupt_entry(). The kernel started to work on behalf of the
+    * user process but, for some reason (typically kernel preemption or
+    * wait on condition) the task was scheduled out. When that happened,
+    * because of the function switch_to_task_pop_nested_interrupts() called
+    * above, the 0x80 value was dropped from `nested_interrupts`. Now that
+    * we have to resume the execution of the user task (but in kernel mode),
+    * we MUST push back that 0x80 in order to compensate the pop that will
+    * occur in kernel's soft_interrupt_entry() just before returning back
+    * to the user. That's because the nested_interrupts array is global and
+    * not specific to any given task. Like the registers, it has to be saved
+    * and restored in a consistent way.
+    */
+
+   if (!is_kernel_thread(ti)) {
+      push_nested_interrupt(SYSCALL_SOFT_INTERRUPT);
+   }
+}
+
+NORETURN void
+switch_to_task(struct task *ti)
 {
    /* Save the value of ti->state_regs as it will be reset below */
    regs_t *state = ti->state_regs;
@@ -498,50 +537,12 @@ NORETURN void switch_to_task(struct task *ti)
    disable_interrupts_forced();
    switch_to_task_pop_nested_interrupts();
    enable_preemption_nosched();
-
-   /*
-    * Make sure in NO WAY we'll switch to a user task while keeping the
-    * preemption disabled. That would be pretty bad.
-    */
    ASSERT(is_preemption_enabled());
 
-   if (!ti->running_in_kernel) {
-
+   if (!ti->running_in_kernel)
       task_info_reset_kernel_stack(ti);
-
-   } else {
-
-      /*
-       * The new task was running in kernel when it was preempted.
-       *
-       * In theory, there's nothing we have to do here, and that's exactly
-       * what happens when KRN_TRACK_NESTED_INTERR is 0. But, our nice
-       * debug feature for nested interrupts tracking requires a little work:
-       * because of its assumptions (hard-coded in ASSERTS) are that when the
-       * kernel is running, it's always inside some kind of interrupt handler
-       * (fault, int 0x80 [syscall], IRQ) before resuming the next task, we have
-       * to resume the state of the nested_interrupts in one case: the one when
-       * we're resuming a USER task that was running in KERNEL MODE (the kernel
-       * was running on behalf of the task). In that case, when for the first
-       * time the user task got to the kernel, we had a nice 0x80 added in our
-       * nested_interrupts array [even in the case of sysenter] by the function
-       * soft_interrupt_entry(). The kernel started to work on behalf of the
-       * user process but, for some reason (typically kernel preemption or
-       * wait on condition) the task was scheduled out. When that happened,
-       * because of the function switch_to_task_pop_nested_interrupts() called
-       * above, the 0x80 value was dropped from `nested_interrupts`. Now that
-       * we have to resume the execution of the user task (but in kernel mode),
-       * we MUST push back that 0x80 in order to compensate the pop that will
-       * occur in kernel's soft_interrupt_entry() just before returning back
-       * to the user. That's because the nested_interrupts array is global and
-       * not specific to any given task. Like the registers, it has to be saved
-       * and restored in a consistent way.
-       */
-
-      if (!is_kernel_thread(ti)) {
-         push_nested_interrupt(SYSCALL_SOFT_INTERRUPT);
-      }
-   }
+   else
+      adjust_nested_interrupts_for_task_in_kernel(ti);
 
    set_curr_task(ti);
    ti->timer_ready = false;
@@ -549,7 +550,8 @@ NORETURN void switch_to_task(struct task *ti)
    context_switch(state);
 }
 
-int sys_set_tid_address(int *tidptr)
+int
+sys_set_tid_address(int *tidptr)
 {
    /*
     * NOTE: this syscall must always succeed. In case the user pointer
@@ -614,7 +616,8 @@ arch_specific_new_task_setup(struct task *ti, struct task *parent)
    return true;
 }
 
-void arch_specific_free_task(struct task *ti)
+void
+arch_specific_free_task(struct task *ti)
 {
    arch_task_members_t *arch = get_task_arch_fields(ti);
    aligned_kfree2(arch->aligned_fpu_regs, arch->fpu_regs_size);
@@ -642,7 +645,8 @@ arch_specific_new_proc_setup(struct process *pi, struct process *parent)
    pi->set_child_tid = NULL;
 }
 
-void arch_specific_free_proc(struct process *pi)
+void
+arch_specific_free_proc(struct process *pi)
 {
    arch_proc_members_t *arch = get_proc_arch_fields(pi);
 
