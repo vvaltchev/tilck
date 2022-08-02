589a37df1289d57f50d574cd05c22cbbabe70f3d
Vladislav Valtchev <valtchevv@vmware.com>
2022-08-02 19:42:37 +0300

    [gtests] Make all the wrapped funcs to use the mocking singleton

---
+++ tests/unit/mocking.h
@@ -0,0 +1,91 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+#pragma once
+
+#define DECL_FUNCS_0(name, ret)                     \
+   ret __real_##name();                             \
+   ret __wrap_##name();
+
+#define DECL_FUNCS_1(name, ret, t1)                 \
+   ret __real_##name(t1);                           \
+   ret __wrap_##name(t1);
+
+#define DECL_FUNCS_2(name, ret, t1, t2)             \
+   ret __real_##name(t1, t2);                       \
+   ret __wrap_##name(t1, t2);
+
+#define DECL_FUNCS_3(name, ret, t1, t2, t3)         \
+   ret __real_##name(t1, t2, t3);                   \
+   ret __wrap_##name(t1, t2, t3);
+
+
+extern "C" {
+
+#include <tilck/common/basic_defs.h>
+#include <tilck/kernel/datetime.h>
+#include <tilck/kernel/sync.h>
+#include <tilck/kernel/sched.h>
+
+#define DEF_0(x, n, ret) DECL_FUNCS_0(n, ret)
+#define DEF_1(x, n, ret, t1) DECL_FUNCS_1(n, ret, t1)
+#define DEF_2(x, n, ret, t1, t2) DECL_FUNCS_2(n, ret, t1, t2)
+#define DEF_3(x, n, ret, t1, t2, t3) DECL_FUNCS_3(n, ret, t1, t2, t3)
+
+#include "mocked_funcs.h"
+
+#undef DEF_0
+#undef DEF_1
+#undef DEF_2
+#undef DEF_3
+}
+
+#define DEF_0(what, name, ret)                        \
+   virtual ret name() {                               \
+      return __##what##_##name();                     \
+   }
+
+#define DEF_1(what, name, ret, t1)                    \
+   virtual ret name(t1 a1) {                          \
+      return __##what##_##name(a1);                   \
+   }
+
+#define DEF_2(what, name, ret, t1, t2)                \
+   virtual ret name(t1 a1, t2 a2) {                   \
+      return __##what##_##name(a1, a2);               \
+   }
+
+#define DEF_3(what, name, ret, t1, t2, t3)            \
+   virtual ret name(t1 a1, t2 a2, t3 a3) {            \
+      return __##what##_##name(a1, a2, a3);           \
+   }
+
+
+class KernelSingleton {
+public:
+
+   KernelSingleton() {
+      prevInstance = instance;
+      instance = this;
+   }
+
+   virtual ~KernelSingleton() {
+      instance = prevInstance;
+   }
+
+   #include "mocked_funcs.h"
+
+   static KernelSingleton *get() { return instance; }
+
+protected:
+   static KernelSingleton *instance;
+   KernelSingleton *prevInstance;
+};
+
+#undef DECL_FUNCS_0
+#undef DECL_FUNCS_1
+#undef DECL_FUNCS_2
+#undef DECL_FUNCS_3
+
+#undef DEF_0
+#undef DEF_1
+#undef DEF_2
+#undef DEF_3
