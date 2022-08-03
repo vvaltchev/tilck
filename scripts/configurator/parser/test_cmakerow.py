5e62d2f5f0098e83786a45346b317fa7530e4a0d
michelececcacci <michelececcacci1@gmail.com>
2022-08-03 21:45:05 +0200

    [cmake-configurator]  global refactoring

---
+++ scripts/configurator/parser/test_cmakerow.py
@@ -0,0 +1,59 @@
+from multiprocessing.sharedctypes import Value
+from unittest import TestCase
+from cmake_row import cmake_row, configurator_type,  row_type
+
+class test_cmake_cache_variable(TestCase):
+   commentline = ""
+   filepath_var_valid = "CMAKE_ADDR2LINE:FILEPATH=/usr/bin/addr2line"
+   noname_var = ":FILEPATH=/usr/bin/addr2line"
+   notype_var = "CMAKE_ADDR2LINE:=/usr/bin/addr2line"
+   bool_var = "CMAKE_ADDR2LINE:BOOL=ON"
+
+   def test_row_creation(self):
+      var_name = "CMAKE_ADDR2LINE"
+      row = cmake_row(self.filepath_var_valid, 0)
+      with self.subTest():
+         self.assertEqual(row.name, var_name)
+      with self.subTest():
+         self.assertEqual(row.configurator_type, configurator_type.FILEPATH)
+      with self.subTest():
+         self.assertEqual(row.row_type, row_type.VARIABLE)
+      with self.subTest():
+         self.assertEqual(row.val,  "/usr/bin/addr2line")
+      with self.subTest():
+         self.assertEqual(row.row_number, 0)
+
+   def test_slash_comment_row(self):
+      row = cmake_row("//" + self.filepath_var_valid, 0)
+      with self.subTest():
+         self.assertEqual(row.row_type, row_type.SLASH_COMMENT)
+      with self.subTest():
+         self.assertEqual(row.serialize(), "//" + self.filepath_var_valid)
+
+   def test_pound_comment_row(self):
+      row = cmake_row("#" + self.filepath_var_valid, 0)
+      with self.subTest():
+         self.assertEqual(row.row_type, row_type.POUND_COMMENT)
+      with self.subTest():
+         self.assertEqual(row.serialize(), "#" + self.filepath_var_valid)
+
+   def test_noname_row(self):
+      self.assertRaises(ValueError,  cmake_row, self.noname_var, 0)
+
+   def test_empty_line(self):
+      row = cmake_row("", 0)
+      self.assertEqual(row.row_type, row_type.EMPTY)
+
+   def test_notype_row(self):
+      self.assertRaises(ValueError, cmake_row, self.notype_var, 0)
+
+   def test_bool_row(self):
+      row = cmake_row(self.bool_var, 0)
+      with self.subTest():
+         self.assertTrue(row.val)
+      with self.subTest():
+         self.assertEqual(row.serialize(), self.bool_var)
+
+   def test_emtpy_row(self):
+      row = cmake_row("", 0)
+      self.assertEqual("", row.serialize())
