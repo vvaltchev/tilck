51b58979e92ec208cd1803799b493e1c5cbe48f9
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2023-10-21 15:31:42 -0700

    [acpica] Upgrade ACPICA to version 20221020

--- modules/acpi/acpica/tables/tbutils.c
+++ modules/acpi/acpica/tables/tbutils.c
@@ -8,7 +8,7 @@
  *
  * 1. Copyright Notice
  *
- * Some or all of this work - Copyright (c) 1999 - 2021, Intel Corp.
+ * Some or all of this work - Copyright (c) 1999 - 2022, Intel Corp.
  * All rights reserved.
  *
  * 2. License
@@ -471,7 +471,7 @@ AcpiTbParseRootTable (
 
     /* Validate the root table checksum */
 
-    Status = AcpiTbVerifyChecksum (Table, Length);
+    Status = AcpiUtVerifyChecksum (Table, Length);
     if (ACPI_FAILURE (Status))
     {
         AcpiOsUnmapMemory (Table, Length);
