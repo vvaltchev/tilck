593b8a6e1f17fdfa7946230065a84ce348df7b73
Matheus Garcias <matheus.dasilva.garcias@gmail.com>
2023-10-25 10:27:02 -0300

    fix kernel modules priority headers

--- include/tilck/kernel/modules.h
+++ include/tilck/kernel/modules.h
@@ -34,5 +34,5 @@ void register_module(struct module *m);
 #define MOD_fbdev_prio                       300
 #define MOD_serial_prio                      400
 #define MOD_sb16_prio                        410
-#define MOD_dp_prio                         1000
-#define MOD_systests_prio                   1010  /* last */
+#define MOD_systests_prio                    990
+#define MOD_dp_prio                         1000 /* last */
