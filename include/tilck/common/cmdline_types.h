aedff6293ded384e5e49b44c446a427d73052f46
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2024-03-09 16:56:19 -0800

    [boot] Add 'h' option to show the all the kopts

---
+++ include/tilck/common/cmdline_types.h
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+
+#pragma once
+#include <tilck/common/basic_defs.h>
+
+enum kopt_type {
+   KOPT_TYPE_bool,
+   KOPT_TYPE_long,
+   KOPT_TYPE_ulong,
+   KOPT_TYPE_wordstr,
+};
+
+/* Used by cmdline.c in the kernel */
+struct kopt {
+   const char *name;
+   const char *alias;
+   enum kopt_type type;
+   void *data;
+};
+
+/* Used by the bootloader UI for the help message */
+struct kopt_help {
+   const char *name;
+   const char *alias;
+   enum kopt_type type;
+};
+
+typedef const char *wordstr;
