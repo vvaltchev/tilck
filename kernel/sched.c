5c2ec19255ecc5036111fa4817078cde9c553ae8
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2024-04-14 20:15:44 -0700

    [hack] add trace logging in the scheduler

--- kernel/sched.c
+++ kernel/sched.c
@@ -11,6 +11,7 @@
 #include <tilck/kernel/worker_thread.h>
 #include <tilck/kernel/timer.h>
 #include <tilck/kernel/errno.h>
+#include <tilck/mods/tracing.h>
 
 /* Shared global variables */
 struct task *__current;
@@ -746,9 +747,13 @@ void do_schedule(void)
    /* Essential: clear the `__need_resched` flag */
    sched_clear_need_resched();
 
+   trace_printk(1, "do_schedule");
+
    /* Handle special corner cases */
-   if (sched_should_return_immediately(curr, curr_state))
+   if (sched_should_return_immediately(curr, curr_state)) {
+      trace_printk(1, "sched -> return immediately");
       return;
+   }
 
    /* Check for worker threads ready to run */
    selected = wth_get_runnable_thread();
@@ -764,6 +769,7 @@ void do_schedule(void)
 
    // XXX
    debug_sched_next_task = selected;
+   trace_printk(1, "sched -> selected: %d", selected->tid);
 
    if (selected != curr) {
 
@@ -792,6 +798,7 @@ void do_schedule(void)
    }
 
    ASSERT(curr_state != TASK_STATE_ZOMBIE);
+   trace_printk(1, "sched -> return");
 }
 
 struct task *get_task(int tid)
