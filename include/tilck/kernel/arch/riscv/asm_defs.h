1ea30703118a3d55a851f3fe0dc5f9c6b9304963
hctang <aenrbesaen@126.com>
2024-07-22 18:10:35 +0800

    [riscv64] Implement exception handling and panic

--- include/tilck/kernel/arch/riscv/asm_defs.h
+++ include/tilck/kernel/arch/riscv/asm_defs.h
@@ -52,5 +52,163 @@
 #define FUNC(x) .type x, @function; x
 #define END_FUNC(x) .size x, .-(x)
 
+.macro save_fp_ra
+   addi sp, sp, -(RISCV_SZPTR * 4)
+   REG_S ra, 3 * RISCV_SZPTR(sp)
+   REG_S s0, 2 * RISCV_SZPTR(sp)
+   addi s0, sp, (RISCV_SZPTR * 4)
+.endm
+
+.macro restore_fp_ra
+   REG_L ra, 3 * RISCV_SZPTR(sp)
+   REG_L s0, 2 * RISCV_SZPTR(sp)
+   addi  sp, sp, (RISCV_SZPTR * 4)
+.endm
+
+.macro save_callee_regs
+
+   mv t0, sp
+   addi sp, sp, -SIZEOF_REGS
+
+   REG_S ra,   1 * RISCV_SZPTR(sp)
+   REG_S t0,   2 * RISCV_SZPTR(sp)
+   REG_S s0,   8 * RISCV_SZPTR(sp)
+   REG_S s1,   9 * RISCV_SZPTR(sp)
+   REG_S s2,  18 * RISCV_SZPTR(sp)
+   REG_S s3,  19 * RISCV_SZPTR(sp)
+   REG_S s4,  20 * RISCV_SZPTR(sp)
+   REG_S s5,  21 * RISCV_SZPTR(sp)
+   REG_S s6,  22 * RISCV_SZPTR(sp)
+   REG_S s7,  23 * RISCV_SZPTR(sp)
+   REG_S s8,  24 * RISCV_SZPTR(sp)
+   REG_S s9,  25 * RISCV_SZPTR(sp)
+   REG_S s10, 26 * RISCV_SZPTR(sp)
+   REG_S s11, 27 * RISCV_SZPTR(sp)
+
+   csrr s1, sstatus
+   REG_S s1, 33 * RISCV_SZPTR(sp)
+
+.endm
+
+.macro resume_callee_regs
+
+   REG_L s1, 33 * RISCV_SZPTR(sp)
+   csrw sstatus, s1
+
+   REG_L ra,   1 * RISCV_SZPTR(sp)
+   REG_L t0,   2 * RISCV_SZPTR(sp)
+   REG_L s0,   8 * RISCV_SZPTR(sp)
+   REG_L s1,   9 * RISCV_SZPTR(sp)
+   REG_L s2,  18 * RISCV_SZPTR(sp)
+   REG_L s3,  19 * RISCV_SZPTR(sp)
+   REG_L s4,  20 * RISCV_SZPTR(sp)
+   REG_L s5,  21 * RISCV_SZPTR(sp)
+   REG_L s6,  22 * RISCV_SZPTR(sp)
+   REG_L s7,  23 * RISCV_SZPTR(sp)
+   REG_L s8,  24 * RISCV_SZPTR(sp)
+   REG_L s9,  25 * RISCV_SZPTR(sp)
+   REG_L s10, 26 * RISCV_SZPTR(sp)
+   REG_L s11, 27 * RISCV_SZPTR(sp)
+
+   REG_L sp,   2 * RISCV_SZPTR(sp)
+
+.endm
+
+.macro save_all_regs
+
+   addi sp, sp, -SIZEOF_REGS
+
+   REG_S x1,   1 * RISCV_SZPTR(sp)
+
+   REG_S x3,   3 * RISCV_SZPTR(sp)
+   REG_S x4,   4 * RISCV_SZPTR(sp)
+   REG_S x5,   5 * RISCV_SZPTR(sp)
+   REG_S x6,   6 * RISCV_SZPTR(sp)
+   REG_S x7,   7 * RISCV_SZPTR(sp)
+   REG_S x8,   8 * RISCV_SZPTR(sp)
+   REG_S x9,   9 * RISCV_SZPTR(sp)
+   REG_S x10, 10 * RISCV_SZPTR(sp)
+   REG_S x11, 11 * RISCV_SZPTR(sp)
+   REG_S x12, 12 * RISCV_SZPTR(sp)
+   REG_S x13, 13 * RISCV_SZPTR(sp)
+   REG_S x14, 14 * RISCV_SZPTR(sp)
+   REG_S x15, 15 * RISCV_SZPTR(sp)
+   REG_S x16, 16 * RISCV_SZPTR(sp)
+   REG_S x17, 17 * RISCV_SZPTR(sp)
+   REG_S x18, 18 * RISCV_SZPTR(sp)
+   REG_S x19, 19 * RISCV_SZPTR(sp)
+   REG_S x20, 20 * RISCV_SZPTR(sp)
+   REG_S x21, 21 * RISCV_SZPTR(sp)
+   REG_S x22, 22 * RISCV_SZPTR(sp)
+   REG_S x23, 23 * RISCV_SZPTR(sp)
+   REG_S x24, 24 * RISCV_SZPTR(sp)
+   REG_S x25, 25 * RISCV_SZPTR(sp)
+   REG_S x26, 26 * RISCV_SZPTR(sp)
+   REG_S x27, 27 * RISCV_SZPTR(sp)
+   REG_S x28, 28 * RISCV_SZPTR(sp)
+   REG_S x29, 29 * RISCV_SZPTR(sp)
+   REG_S x30, 30 * RISCV_SZPTR(sp)
+   REG_S x31, 31 * RISCV_SZPTR(sp)
+
+   csrr s0, sepc
+   csrr s1, sstatus
+   csrr s2, stval
+   csrr s3, scause
+   csrr s4, sscratch
+
+   REG_S s0, 32 * RISCV_SZPTR(sp)
+   REG_S s1, 33 * RISCV_SZPTR(sp)
+   REG_S s2, 34 * RISCV_SZPTR(sp)
+   REG_S s3, 35 * RISCV_SZPTR(sp)
+   REG_S s4, 38 * RISCV_SZPTR(sp)
+
+   addi s0, sp, SIZEOF_REGS
+   REG_S s0,  2 * RISCV_SZPTR(sp)
+
+.endm
+
+.macro resume_all_regs
+
+   REG_L s0, 32 * RISCV_SZPTR(sp)
+   REG_L s1, 33 * RISCV_SZPTR(sp)
+   andi s1, s1, ~SR_SIE //The interrupt must remain closed until sret
+   csrw sepc, s0
+   csrw sstatus, s1
+
+   REG_L x1,   1 * RISCV_SZPTR(sp)
+
+   REG_L x3,   3 * RISCV_SZPTR(sp)
+   REG_L x4,   4 * RISCV_SZPTR(sp)
+   REG_L x5,   5 * RISCV_SZPTR(sp)
+   REG_L x6,   6 * RISCV_SZPTR(sp)
+   REG_L x7,   7 * RISCV_SZPTR(sp)
+   REG_L x8,   8 * RISCV_SZPTR(sp)
+   REG_L x9,   9 * RISCV_SZPTR(sp)
+   REG_L x10, 10 * RISCV_SZPTR(sp)
+   REG_L x11, 11 * RISCV_SZPTR(sp)
+   REG_L x12, 12 * RISCV_SZPTR(sp)
+   REG_L x13, 13 * RISCV_SZPTR(sp)
+   REG_L x14, 14 * RISCV_SZPTR(sp)
+   REG_L x15, 15 * RISCV_SZPTR(sp)
+   REG_L x16, 16 * RISCV_SZPTR(sp)
+   REG_L x17, 17 * RISCV_SZPTR(sp)
+   REG_L x18, 18 * RISCV_SZPTR(sp)
+   REG_L x19, 19 * RISCV_SZPTR(sp)
+   REG_L x20, 20 * RISCV_SZPTR(sp)
+   REG_L x21, 21 * RISCV_SZPTR(sp)
+   REG_L x22, 22 * RISCV_SZPTR(sp)
+   REG_L x23, 23 * RISCV_SZPTR(sp)
+   REG_L x24, 24 * RISCV_SZPTR(sp)
+   REG_L x25, 25 * RISCV_SZPTR(sp)
+   REG_L x26, 26 * RISCV_SZPTR(sp)
+   REG_L x27, 27 * RISCV_SZPTR(sp)
+   REG_L x28, 28 * RISCV_SZPTR(sp)
+   REG_L x29, 29 * RISCV_SZPTR(sp)
+   REG_L x30, 30 * RISCV_SZPTR(sp)
+   REG_L x31, 31 * RISCV_SZPTR(sp)
+
+   REG_L x2, 2 * RISCV_SZPTR(sp)
+.endm
+
 #endif
 
--- kernel/arch/riscv/fault.c
+++ kernel/arch/riscv/fault.c
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
+
 #include <tilck/common/basic_defs.h>
 #include <tilck/common/printk.h>
-
 #include <tilck/kernel/hal.h>
 #include <tilck/kernel/interrupts.h>
 #include <tilck/kernel/fault_resumable.h>
@@ -34,33 +34,129 @@ const char *riscv_exception_names[32] =
    "Store(or AMO) page fault",
 };
 
+static void handle_generic_fault(regs_t *r)
+{
+   const int int_num = r->int_num;
+   handle_generic_fault_int(r, riscv_exception_names[int_num]);
+}
+
+static void handle_inst_illegal_fault(regs_t *r)
+{
+   const int int_num = r->int_num;
+   handle_inst_illegal_fault_int(r, riscv_exception_names[int_num]);
+}
+
+static void handle_bus_fault(regs_t *r)
+{
+   const int int_num = r->int_num;
+   handle_bus_fault_int(r, riscv_exception_names[int_num]);
+}
+
+static void handle_breakpoint(regs_t *r)
+{
+   /*
+    * Do nothing, literally. The purpose of this way of handling `int 3` is to
+    * allow during development to easily put a breakpoint in user-space code and
+    * from GDB (using remote debugging) to put a HW breakpoint here.
+    *
+    * On Linux, the behavior of `int 3` is quite different, but that's fine.
+    * This interrupt is anyway used only for debugging purposes. In the future,
+    * it is absolutely possible that Tilck will handle it in a different way.
+    * For the moment, the kernel offers no debugging features of userspace
+    * programs (= no such thing as ptrace).
+    */
+   asmVolatile("nop");
+}
+
 void set_fault_handler(int ex_num, void *ptr)
 {
-   NOT_IMPLEMENTED();
+   fault_handlers[ex_num] = (soft_int_handler_t) ptr;
 }
 
 void init_cpu_exception_handling(void)
 {
-   NOT_IMPLEMENTED();
+   csr_write(CSR_STVEC, &asm_trap_entry);
+
+   set_fault_handler(EXC_INST_MISALIGNED, handle_bus_fault);
+   set_fault_handler(EXC_INST_ACCESS, handle_generic_fault);
+   set_fault_handler(EXC_INST_ILLEGAL, handle_inst_illegal_fault);
+   set_fault_handler(EXC_BREAKPOINT, handle_breakpoint);
+   set_fault_handler(EXC_LOAD_MISALIGNED, handle_bus_fault);
+   set_fault_handler(EXC_LOAD_ACCESS, handle_generic_fault);
+   set_fault_handler(EXC_STORE_MISALIGNED, handle_bus_fault);
+   set_fault_handler(EXC_STORE_ACCESS, handle_generic_fault);
 }
 
 void handle_resumable_fault(regs_t *r)
 {
-   NOT_IMPLEMENTED();
+   struct task *curr = get_curr_task();
+
+   pop_nested_interrupt(); // the fault
+   disable_interrupts_forced();
+   set_return_register(curr->fault_resume_regs, 1u << regs_intnum(r));
+   context_switch(curr->fault_resume_regs);
 }
 
 static void fault_in_panic(regs_t *r)
 {
-   NOT_IMPLEMENTED();
+   const int int_num = r->int_num;
+
+   if (is_fault_resumable(int_num))
+      return handle_resumable_fault(r);
+
+   /*
+    * We might be so unlucky that printk() causes some fault(s) too: therefore,
+    * not even trying to print something on the screen is safe. In order to
+    * avoid generating an endless sequence of page faults in the worst case,
+    * just call printk() in SAFE way here.
+    */
+   fault_resumable_call(
+      ALL_FAULTS_MASK, printk, 5,
+      "FATAL: %s [%d] while in panic state [EIP: %p]\n",
+      riscv_exception_names[int_num], int_num, regs_get_ip(r));
+
+   /* Halt the CPU forever */
+   while (true) { halt(); }
 }
 
 void handle_fault(regs_t *r)
 {
-   NOT_IMPLEMENTED();
+   const int int_num = r->int_num;
+   bool cow = false;
+
+   ASSERT(is_fault(int_num));
+
+   if (UNLIKELY(in_panic()))
+      return fault_in_panic(r);
+
+   if (LIKELY((int_num == EXC_INST_PAGE_FAULT) ||
+              (int_num == EXC_LOAD_PAGE_FAULT) ||
+              (int_num == EXC_STORE_PAGE_FAULT))) {
+
+      cow = handle_potential_cow(r);
+   }
+
+   if (!cow) {
+
+      if (is_fault_resumable(int_num))
+         return handle_resumable_fault(r);
+
+      if (LIKELY(fault_handlers[int_num] != NULL)) {
+
+         fault_handlers[int_num](r);
+
+      } else {
+
+         panic("Unhandled fault #%i: %s EIP: %p",
+               int_num,
+               riscv_exception_names[int_num],
+               regs_get_ip(r));
+      }
+   }
 }
 
 void on_first_pdir_update(void)
 {
-   return;
+   /* do nothing */
 }
 
