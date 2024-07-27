a0ab4742ff26845633e07b6486398d20da4cbb5c
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2024-07-27 16:01:30 -0700

    [process] Move kthread_exit() to the portable code in process.c

--- include/tilck/kernel/process_int.h
+++ include/tilck/kernel/process_int.h
@@ -10,6 +10,10 @@ extern char *kernel_initial_stack[KERNEL_STACK_SIZE];
 void switch_to_initial_kernel_stack(void);
 void free_common_task_allocs(struct task *ti);
 void process_free_mappings_info(struct process *pi);
+void task_info_reset_kernel_stack(struct task *ti);
+int setup_first_process(pdir_t *pdir, struct task **ti_ref);
+void finalize_usermode_task_setup(struct task *ti, regs_t *user_regs);
+void setup_usermode_task_regs(regs_t *r, void *entry, void *stack_addr);
 
 static ALWAYS_INLINE void set_curr_task(struct task *ti)
 {
