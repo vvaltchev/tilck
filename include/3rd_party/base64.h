eec08a1a9f9f111ea3fc715e0dfb9b9e36ad142e
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2025-11-28 21:43:47 -0800

    [LSP] Make clangd understand include/3rd_party/base64.h

--- include/3rd_party/base64.h
+++ include/3rd_party/base64.h
@@ -4,14 +4,20 @@
  *
  * This software may be distributed under the terms of the BSD license.
  * See README for more details.
+ *
+ * MINOR adaptation for Tilck.
  */
 
-#ifndef BASE64_H
-#define BASE64_H
+#pragma once
+#include <tilck/common/basic_defs.h>
 
-unsigned char * base64_encode(const unsigned char *src, size_t len,
-			      size_t *out_len);
-unsigned char * base64_decode(const unsigned char *src, size_t len,
-			      size_t *out_len);
+unsigned char *
+base64_encode(const unsigned char *src,
+              size_t len,
+              size_t *out_len);
 
-#endif /* BASE64_H */
+unsigned char *
+base64_decode(const unsigned char *src,
+              size_t len,
+              size_t *out_len);
+
