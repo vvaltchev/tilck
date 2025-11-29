0d9f953ebca5b71a4a46267786009e3b9eddd010
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2025-11-29 11:50:26 -0800

    [LSP] Fix common/boot.h for clangd

--- include/tilck/common/boot.h
+++ include/tilck/common/boot.h
@@ -5,6 +5,14 @@
  * See EfiToMultibootMemType() and
  * setup_uefi_runtime_services() for more details.
  */
+#pragma once
+#include <multiboot.h> // system header
+
+#ifdef CLANGD
+   #define uint32_t unsigned int
+#endif
+
+
 #define TILCK_BOOT_EFI_RUNTIME_RO (MULTIBOOT_MEMORY_RESERVED + 1)
 #define TILCK_BOOT_EFI_RUNTIME_RW (MULTIBOOT_MEMORY_RESERVED + 2)
 
@@ -13,3 +21,7 @@ struct tilck_extra_boot_info
   uint32_t RSDP;
   uint32_t RT;
 };
+
+#ifdef CLANGD
+   #undef uint32_t
+#endif
