5bf90b77869020b61a097127691f74226e897e14
cozis <francesco.cozzuto@gmail.com>
2026-04-06 18:12:02 +0200

    Move mmio address calculation outside of mmio_read32/mmio_write32 and add compiler barriers to load and store

--- include/tilck/kernel/arch/generic_x86/mmio.h
+++ include/tilck/kernel/arch/generic_x86/mmio.h
@@ -3,14 +3,20 @@
 #pragma once
 #include <tilck/common/basic_defs.h>
 
-static ALWAYS_INLINE u32 mmio_read32(volatile u32 *io_addr, u32 off)
+static ALWAYS_INLINE u32
+mmio_read32(const volatile void *io_addr)
 {
-    ASSERT(off % 4 == 0);
-    return *(io_addr + off/4);
+    COMPILER_BARRIER();
+    u32 val = *(volatile u32*) io_addr;
+    COMPILER_BARRIER();
+
+    return val;
 }
 
-static ALWAYS_INLINE void mmio_write32(volatile u32 *io_addr, u32 off, u32 val)
+static ALWAYS_INLINE void
+mmio_write32(u32 val, volatile void *io_addr)
 {
-    ASSERT(off % 4 == 0);
-    *(io_addr + off/4) = val;
+    COMPILER_BARRIER();
+    *(volatile u32*) io_addr = val;
+    COMPILER_BARRIER();
 }
