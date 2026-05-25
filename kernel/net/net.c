ad30b60dbfb2cad0a29abbf22154634aadc7628d
Vladislav K. Valtchev <vladislav.valtchev@gmail.com>
2026-05-24 22:38:35 -0700

    [net] Remove unnecessary (void) casts on unused parameters
    -Wno-unused-parameter is on project-wide; the casts are redundant.

--- kernel/net/net.c
+++ kernel/net/net.c
@@ -8,9 +8,7 @@ struct net_driver_funcs net_driver_funcs;
 void
 net_process_packet(void *src, size_t len)
 {
-    (void) src;
-    (void) len;
-    /*
-     * Packets not being processed yet
-     */
+   /*
+    * Packets not being processed yet
+    */
 }
